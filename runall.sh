c3020454b137c77c11062dbdb2c82a7f881f96c9
piskvorky <piskvorky@92d0401f-a546-4972-9173-107b360ed7e5>
2009-09-08 12:24:15 +0000

    #2 pridani logu pro tvorbu databazi

--- runall.sh
+++ runall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-./build_database.py
+./build_database.py 2>&1 | tee ~/xrehurek/results/build_database.log
 
 for lang in cze ita eng fre rus ger;
 do
-	./build_tfidf.py $lang 2>&1 | tee ~/xrehurek/results/build_$lang.log
-	./gensim.py lsi $lang 2>&1 | tee ~/xrehurek/results/gensim_$lang_lsi.log
-	./gensim.py tfidf $lang 2>&1 | tee ~/xrehurek/results/gensim_$lang_tfidf.log
-	./gensim.py rp $lang 2>&1 | tee ~/xrehurek/results/gensim_$lang_rp.log
+	./build_tfidf.py $lang 2>&1 | tee ~/xrehurek/results/build_${lang}.log
+	./gensim.py lsi $lang 2>&1 | tee ~/xrehurek/results/gensim_${lang}_lsi.log
+	./gensim.py tfidf $lang 2>&1 | tee ~/xrehurek/results/gensim_${lang}_tfidf.log
+	./gensim.py rp $lang 2>&1 | tee ~/xrehurek/results/gensim_${lang}_rp.log
 done
