27f1b95e1bf929b6534abf9d216ace42d7c91f19
piskvorky <radimrehurek@seznam.cz>
2010-01-14 10:07:01 +0000

    #6 slova v dokumentech v poradi; moznost inicializace seeded/random; refactor
    git-svn-id: https://my-svn.assembla.com/svn/gensim/trunk@19 92d0401f-a546-4972-9173-107b360ed7e5

--- lda.py
+++ lda.py
@@ -10,7 +10,7 @@ How to estimate (model will be written to datafile.model):
 ./lda_estimate.py k datafile
 example: ./lda_estimate.py 200 ~/ldadata/trndocs.dat
 
-How to do inference (document likelihoods and gammas will be written to datafile.infer):
+How to do inference (document likelihoods and gammas will be written to datafile.lda_inferred):
 ./lda_infer.py modelfile datafile
 example: ./lda_infer.py ~/ldadata/trndocs.dat.model ~/ldadata/newdocs.dat
 """
@@ -46,24 +46,28 @@ class CorpusLow(object):
 
     in which all [wordij] (i=1..M, j=1..Ni) are text strings and they are separated by the blank character.
     """
-    def __init__(self, fname, line2words = lambda line: line.strip().split(' ')):
+    def __init__(self, fname, id2word = None, line2words = lambda line: line.strip().split(' ')):
         logging.info("loading corpus from %s" % fname)
         
         self.fname = fname # input file, see class doc for format
         self.line2words = line2words # how to translate lines into words (simply split on space by default)
         self.numDocs = int(open(fname).readline()) # the first line in input data is the number of documents (integer). throws exception on bad input.
-        self.useWordIds = False # return documents as (word, wordCount) 2-tuples
         
-        # build a list of all word types in the corpus (distinct words)
-        logging.info("extracting vocabulary from the corpus")
-        allTerms = set()
-        for doc in self:
-            allTerms.update(word for word, wordCnt in doc)
-        allTerms = sorted(allTerms) # sort the list of all words; rank in that list = word's integer id
-        self.word2id = dict(zip(allTerms, xrange(len(allTerms)))) # build a mapping of word (string) -> its id (int)
-        self.id2word = dict((v, k) for k, v in self.word2id.iteritems())
-        self.useWordIds = True # return documents as (wordIndex, wordCount) 2-tuples
+        if not id2word:
+            # build a list of all word types in the corpus (distinct words)
+            logging.info("extracting vocabulary from the corpus")
+            allTerms = set()
+            self.useWordIds = False # return documents as (word, wordCount) 2-tuples
+            for doc in self:
+                allTerms.update(word for word, wordCnt in doc)
+            allTerms = sorted(allTerms) # sort the list of all words; rank in that list = word's integer id
+            self.id2word = dict(zip(xrange(len(allTerms)), allTerms)) # build a mapping of word id(int) -> word (string)
+        else:
+            logging.info("using provided word mapping (%i ids)" % len(id2word))
+            self.id2word = id2word
+        self.word2id = dict((v, k) for k, v in self.id2word.iteritems())
         self.numTerms = len(self.word2id)
+        self.useWordIds = True # return documents as (wordIndex, wordCount) 2-tuples
         
         logging.info("loaded corpus with %i documents and %i terms from %s" % 
                      (self.numDocs, self.numTerms, fname))
@@ -73,15 +77,26 @@ class CorpusLow(object):
     
     def __iter__(self):
         for lineNo, line in enumerate(open(self.fname)):
-            if lineNo > 0: # ignore the first line
+            if lineNo > 0: # ignore the first line = number of documents
                 # convert document line to words, using the function supplied in constructor
                 words = self.line2words(line)
-                uniqWords = set(words) # all distinct terms in this document
                 
                 if self.useWordIds:
+                    uniqWords = set(words).intersection(self.word2id.iterkeys()) # all distinct terms in this document, ignore unknown words
+                    # the following creates a unique list of words *in the same order*
+                    # as they were in the input. when iterating over the documents,
+                    # the (word, count) pairs will appear in the same order as they
+                    # were in the input (bar duplicates), which looks better. 
+                    # if this was not needed, we might as well have used useWords = set(words)
+                    useWords, marker = [], set()
+                    for word in words:
+                        if (word in uniqWords) and (word not in marker):
+                            useWords.append(word)
+                            marker.add(word)
                     # construct a list of (wordIndex, wordFrequency) 2-tuples
-                    doc = zip(map(self.word2id.get, uniqWords), map(words.count, uniqWords)) # suboptimal but that's irrelevant at this point
+                    doc = zip(map(self.word2id.get, useWords), map(words.count, useWords)) # suboptimal but speed of this whole function is irrelevant
                 else:
+                    uniqWords = set(words)
                     # construct a list of (word, wordFrequency) 2-tuples
                     doc = zip(uniqWords, map(words.count, uniqWords)) # suboptimal but that's irrelevant at this point
                 
@@ -112,6 +127,7 @@ class CorpusLow(object):
 #endclass CorpusLow
 
 
+# ============= main entry point ================
 if __name__ == "__main__":
     logging.basicConfig(level = logging.DEBUG)
     logging.info("running %s" % " ".join(sys.argv))
@@ -135,7 +151,7 @@ if __name__ == "__main__":
         model.save(datafile + '.model')
         if PRINT_TOPICS:
             logging.info("printing topics (top %i words)" % PRINT_TOPICS)
-            model.printTopics(corpus.id2word, numWords = PRINT_TOPICS)
+            model.printTopics(numWords = PRINT_TOPICS)
             print '=' * 40
     elif 'infer' in program:
         # make sure we have enough cmd line parameters
@@ -148,9 +164,9 @@ if __name__ == "__main__":
         datafile = sys.argv[2]
         
         # load model and perform inference
+        corpus = CorpusLow(datafile)
         model = LdaModel.load(modelfile)
-        
-        # TODO inference
+        model.infer(corpus) # save inference to datafile.lda_inferred
     else:
         print globals()["__doc__"]
         sys.exit(1)
