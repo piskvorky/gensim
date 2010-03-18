28974b2adab8bd82817a01752aa5432f0ed22aa5
piskvorky <piskvorky@92d0401f-a546-4972-9173-107b360ed7e5>
2010-03-18 17:50:19 +0000

    #13 fixing test to enable `python setup.py test`

--- setup.py
+++ setup.py
@@ -6,15 +6,19 @@
 
 """
 Run with:
-python2.5 ./setup.py develop
+python ./setup.py install
 """
 
 import os
+import sys
+
+if sys.version_info[:2] < (2, 5):
+    raise Exception('This version of gensim needs Python 2.5 or later. ')
 
 import ez_setup
 ez_setup.use_setuptools()
 
-from setuptools import setup, find_packages, Extension, Feature
+from setuptools import setup, find_packages
 
 
 
@@ -51,7 +55,7 @@ def read(fname):
 
 setup(
     name = 'gensim',
-    version = '0.2.1',
+    version = '0.4',
     description = 'Python Framework for Topic Modeling',
     long_description = long_desc,
     
@@ -66,16 +70,19 @@ setup(
     download_url = 'http://pypi.python.org/pypi/gensim',
     license = 'LGPL',
     zip_safe = False,
-    classifiers = [
+    
+    classifiers = [ # from http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'Topic :: Scientific/Engineering :: Information Analysis'
     ],
+    
+    test_suite = "gensim.test",
 
     install_requires=[
         'numpy >= 1.2.0',
