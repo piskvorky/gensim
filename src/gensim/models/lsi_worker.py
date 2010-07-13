aaedbfbd7f14d475a04b7477c09b068d0f65afb6
piskvorky <radimrehurek@seznam.cz>
2010-07-13 17:45:01 +0000

    #22 integrated LSI into the distributed framework. LSI now automatically chooses between serial/distributed, depending on active workers found.
    git-svn-id: https://my-svn.assembla.com/svn/gensim/trunk@161 92d0401f-a546-4972-9173-107b360ed7e5

--- src/gensim/models/lsi_worker.py
+++ src/gensim/models/lsi_worker.py
@@ -5,6 +5,14 @@
 # Licensed under the GNU LGPL v2.1 - http://www.gnu.org/licenses/lgpl.html
 
 """
+USAGE: %(program)s
+
+    Worker ("slave") process used in computing distributed LSI. Run this script \
+on every node in your cluster. If you wish, you may even run it multiple times \
+on a single machine, to make better use of multiple cores (just beware that \
+memory footprint increases accordingly).
+
+Example: python lsi_worker.py
 """
 
 
@@ -14,10 +22,11 @@ from Queue import Queue
 import threading
 
 import Pyro
+
 from gensim.utils import synchronous
+from gensim.models import LsiModel
 
-
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s')
+logging.basicConfig(format = '%(asctime)s : %(levelname)s : %(message)s')
 logger = logging.getLogger('lsi_worker')
 logger.setLevel(logging.DEBUG)
 
@@ -25,36 +34,40 @@ logger.setLevel(logging.DEBUG)
 
 class Worker(object):
     def __init__(self):
-        self.updates = Queue() # infinite size queue for keeping updates
-        self.initialized = False
-        self.name = None # will be set to pyro NS name, after registering the object
+        self.model = None
 
     
-    def initialize(self, my_id, callback):
+    def initialize(self, my_id, dispatcher, **model_params):
+        self.lock_update = threading.Lock()
+        self.updates = Queue() # infinite size queue for keeping updates
         self.state = 0 # state of this worker (=number of updates applied so far)
         self.jobs_done = 0 # how many jobs has this worker completed itself
         self.my_id = my_id # id of this worker in the dispatcher; just a convenience var for easy access TODO remove?
-        self.callback = callback # dispatcher
-        self.initialized = True
+        self.dispatcher = dispatcher
+        logger.info("initializing worker #%s" % my_id)
+        self.model = LsiModel(**model_params)
     
 
     def get_result(self):
-        logger.debug("worker %s finished job from state %s" % (self.my_id, self.state))
+        logger.info("worker #%s finished job from state %s" % (self.my_id, self.state))
         result, self.result = self.result, None
         return result
 
 
+    @synchronous('lock_update')
     def add_update(self, update):
-        logger.debug("queueing update #%i, len(queue)=%i items" % 
+        logger.debug("enqueueing update #%i, len(queue)=%i items" % 
                      (update[0], 1 + self.updates.qsize()))
         self.updates.put(update)
 
 
     def apply_update(self, update):
-        logger.debug("applying update %s" % update)
+        logger.debug("worker #%s applying update %i" % (self.my_id, self.state))
+        self.model.apply_update(update)
         self.state += 1
-        
-
+    
+    
+    @synchronous('lock_update')
     def process_updates(self):
         """
         Make sure all pending updates (from other workers) have been applied.
@@ -72,7 +85,10 @@ class Worker(object):
                 raise ValueError("received state update #%i, expected #%i" %
                                  (update_id, self.state))
             self.apply_update(update)
-        return old_state != self.state
+        result = old_state != self.state
+        if result:
+            logger.debug("worker #%s now at state %s" % (self.my_id, self.state))
+        return result
 
 
     def request_job(self):
@@ -82,36 +98,37 @@ class Worker(object):
         Once the job is finished, the dispatcher is notified that it should collect
         the results, broadcast them to other workers etc., and eventually tell
         this worker to request another job by calling this function again.
-        
-        Note that callback.collect_result is also called asynchronously ("one-way").
         """
-        if not self.initialized:
-            raise RuntimeError("worker %s must be initialized before receiving jobs" % self.name)
+        if self.model is None:
+            raise RuntimeError("worker must be initialized before receiving jobs")
         self.process_updates()
-        job = self.callback.get_job(self.my_id) # blocks until a new job is available from the dispatcher
-        self.process_updates() # call process_updates again, to stay as up-to-date as possible
-        logger.debug("worker %s received a new job" % self.my_id)
-        result = job # FIXME
-        import time, random
-        time.sleep(random.random() * 3.0)
-        self.result = result
+        job = self.dispatcher.get_job(self.my_id) # blocks until a new job is available from the dispatcher
+        self.process_updates() # call process_updates again, to stay as up-to-date as possible, in case the get_job() call blocked for a long period of time
+        logger.debug("worker #%s received a new job" % self.my_id)
+        self.result = self.model.compute_update(job)
         self.jobs_done += 1
-        self.callback.collect_result(self.my_id)
+        self.dispatcher.collect_result(self.my_id) # must be asynchronous (one-way)
+
+    
+    def get_state(self):
+        self.process_updates() # make sure all pending updates have been applied
+        logger.info("worker #%i returning its state after %s updates" % 
+                    (self.my_id, self.state))
+        self.model.projection_on(True)
+        return self.model.projection
 #endclass Worker
 
 
 def main():
-    ns = Pyro.naming.locateNS()
-    with Pyro.core.Daemon() as daemon:
-        worker = Worker()
-        uri = daemon.register(worker)
-        name = 'gensim.worker.' + str(uri)
-        worker.name = name
-        ns.remove(name)
-        ns.register(name, uri)
-        logger.info("worker is ready at URI %s" % uri)
-        daemon.requestLoop()
-    
+    with Pyro.naming.locateNS() as ns:
+        with Pyro.core.Daemon() as daemon:
+            worker = Worker()
+            uri = daemon.register(worker)
+            name = 'gensim.worker.' + str(uri)
+            ns.remove(name)
+            ns.register(name, uri)
+            logger.info("worker is ready at URI %s" % uri)
+            daemon.requestLoop()
 
 
 
