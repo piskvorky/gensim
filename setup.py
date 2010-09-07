942e762046f3a15e052e0e0a1f167f2baa5e3985
piskvorky <radimrehurek@seznam.cz>
2010-09-07 11:08:07 +0000

    preparing for new release
    git-svn-id: https://my-svn.assembla.com/svn/gensim/trunk@224 92d0401f-a546-4972-9173-107b360ed7e5

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def read(fname):
 
 setup(
     name = 'gensim',
-    version = '0.7.2',
+    version = '0.7.3',
     description = 'Python framework for fast Vector Space Modelling',
     long_description = read('README.txt'),
     
