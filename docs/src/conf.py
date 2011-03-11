74c3aa1d52ac15b3dea18b84fe820789f83086e5
piskvorky <radimrehurek@seznam.cz>
2011-03-11 23:27:15 +0700

    documentation update

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -121,7 +121,7 @@ html_theme = 'default'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = "gensim documentation"
+html_title = "gensim"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
@@ -149,7 +149,7 @@ html_last_updated_fmt = '%b %d, %Y'
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-html_sidebars = {'index': ['globaltoc.html', 'indexsidebar.html', 'searchbox.html']}
+html_sidebars = {'index': ['globaltoc.html', 'searchbox.html', 'indexsidebar.html']}
 #html_sidebars = {'index': ['globaltoc.html', 'searchbox.html']}
 
 # Additional templates that should be rendered to pages, maps page names to
