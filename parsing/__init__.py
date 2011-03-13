8f24848e0ec1d89436f0c39dc75f291d93bdd30c
piskvorky <radimrehurek@seznam.cz>
2011-03-14 02:30:27 +0700

    removed (duplicated?) "parsing" dir

--- parsing/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-This package contains functions to preprocess raw text
-"""
-
-# bring model classes directly into package namespace, to save some typing
-from porter import PorterStemmer
-from tfidf import tfidf
-from preprocessing import *
