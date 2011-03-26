d6d802f78f2ef5e9f7c66b274cfff3dce47c7ebb
David Nemeskey <nemeskey.david@sztaki.hu>
2011-03-26 15:49:41 +0100

    NullHandler added to the top gensim logger.

--- src/gensim/__init__.py
+++ src/gensim/__init__.py
@@ -5,4 +5,15 @@ similarities within a corpus of documents.
 
 # for IPython tab-completion
 import utils, matutils, interfaces, corpora, models, similarities
+import logging
 
+# TODO: if version_info < 2.7?
+class NullHandler(logging.Handler):
+    """For python versions <= 2.6; same as `logging.NullHandler` in 2.7."""
+    def emit(self, record):
+        pass
+ 
+logger = logging.getLogger('gensim')
+logger.setLevel(logging.INFO)
+if len(logger.handlers) == 0:	# To ensure reload() doesn't add another one
+    logger.addHandler(NullHandler())
