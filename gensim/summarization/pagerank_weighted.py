fb5eaae0f460f984cb5a8cfff0892dd8a25d73e4
Federico Barrios <fbarrios@fi.uba.ar>
2015-05-03 16:15:03 -0300

    Removing unused function.

--- gensim/summarization/pagerank_weighted.py
+++ gensim/summarization/pagerank_weighted.py
@@ -15,37 +15,8 @@ try:
 except ImportError:
     pass
 
-CONVERGENCE_THRESHOLD = 0.0001
 
-
-def pagerank_weighted(graph, initial_value=None, damping=0.85):
-    """Calculates PageRank for an undirected graph"""
-    if initial_value is None:
-        initial_value = 1.0 / len(graph.nodes())
-    scores = dict.fromkeys(graph.nodes(), initial_value)
-
-    iteration_quantity = 0
-    for iteration_number in xrange(100):
-        iteration_quantity += 1
-        convergence_achieved = 0
-        for i in graph.nodes():
-            rank = 1 - damping
-            for j in graph.neighbors(i):
-                neighbors_sum = sum(graph.edge_weight((j, k)) for k in graph.neighbors(j))
-                rank += damping * scores[j] * graph.edge_weight((j, i)) / neighbors_sum
-
-            if abs(scores[i] - rank) <= CONVERGENCE_THRESHOLD:
-                convergence_achieved += 1
-
-            scores[i] = rank
-
-        if convergence_achieved == len(graph.nodes()):
-            break
-
-    return scores
-
-
-def pagerank_weighted_scipy(graph, damping=0.85):
+def pagerank_weighted(graph, damping=0.85):
     adjacency_matrix = build_adjacency_matrix(graph)
     probability_matrix = build_probability_matrix(graph)
 
