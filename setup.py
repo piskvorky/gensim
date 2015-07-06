142a59903bf23412266d3daef83a4fde037270fa
Radim Řehůřek <radimrehurek@seznam.cz>
2015-07-06 17:56:04 +0200

    up version: 0.12.0

--- setup.py
+++ setup.py
@@ -104,7 +104,7 @@ model_dir = os.path.join(os.path.dirname(__file__), 'gensim', 'models')
 
 setup(
     name='gensim',
-    version='0.12.0rc1',
+    version='0.12.0',
     description='Python framework for fast Vector Space Modelling',
     long_description=readfile('README.rst'),
 
