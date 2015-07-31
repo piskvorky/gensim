f49cc83b3077e2655579a283679d32543b99f35c
Matti Lyra <m.lyra@sussex.ac.uk>
2015-07-31 12:36:03 +0200

    Added support for fancy indexing in an IndexedCorpus - self.index is now ndarray which will break code that relies on it being a python list 'if self.index: do_something()'

--- gensim/corpora/indexedcorpus.py
+++ gensim/corpora/indexedcorpus.py
@@ -20,7 +20,7 @@ of each document.
 import logging
 import shelve
 
-import numpy as np
+import numpy
 
 from gensim import interfaces, utils
 
@@ -48,6 +48,11 @@ class IndexedCorpus(interfaces.CorpusABC):
             if index_fname is None:
                 index_fname = utils.smart_extension(fname, '.index')
             self.index = utils.unpickle(index_fname)
+            # change self.index into a numpy.ndarray
+            # this will break code like
+            #     if corpus.index:
+            #         do stuff
+            self.index = numpy.asarray(self.index)
             logger.info("loaded corpus index from %s" % index_fname)
         except:
             self.index = None
@@ -96,6 +101,10 @@ class IndexedCorpus(interfaces.CorpusABC):
                 serializer.__name__)
 
         # store offsets persistently, using pickle
+        # we shouldn't have to worry about self.index being a numpy.ndarray as the serializer will return
+        # the offsets that are actually stored on disk - we're not storing self.index in any case, the
+        # load just needs to turn whatever is loaded from disk back into a ndarray - this should also be
+        # backwards compatible
         logger.info("saving %s index to %s" % (serializer.__name__, index_fname))
         utils.pickle(offsets, index_fname)
 
@@ -115,7 +124,7 @@ class IndexedCorpus(interfaces.CorpusABC):
         if self.index is None:
             raise RuntimeError("cannot call corpus[docid] without an index")
 
-        if isinstance(docno, slice) or isinstance(docno, np.ndarray):
+        if isinstance(docno, (slice, numpy.ndarray)):
             return utils.SlicedCorpus(self, docno)
 
         return self.docbyoffset(self.index[docno])
