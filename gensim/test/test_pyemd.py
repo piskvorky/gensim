2ef305131dc1fa4f754d597309ae7465dae5e962
Ólavur Mortensen <olavurmortensen@gmail.com>
2015-11-12 20:54:13 +0100

    Changed the pyemd tests to use unittest.

--- gensim/test/test_pyemd.py
+++ gensim/test/test_pyemd.py
@@ -2,28 +2,33 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import unittest
+import logging
+
 from pyemd import emd
 
+class TestPyemd(unittest.TestCase):
+    def test_case_1(self):
+        first_signature = np.array([0.0, 1.0])
+        second_signature = np.array([5.0, 3.0])
+        distance_matrix = np.array([[0.0, 0.5],
+                                    [0.5, 0.0]])
+        self.assertEqual(3.5, emd(first_signature, second_signature, distance_matrix))
 
-def test_case_1():
-    first_signature = np.array([0.0, 1.0])
-    second_signature = np.array([5.0, 3.0])
-    distance_matrix = np.array([[0.0, 0.5],
-                                [0.5, 0.0]])
-    assert 3.5 == emd(first_signature, second_signature, distance_matrix)
+    def test_case_2(self):
+        first_signature = np.array([1.0, 1.0])
+        second_signature = np.array([1.0, 1.0])
+        distance_matrix = np.array([[0.0, 1.0],
+                                    [1.0, 0.0]])
+        self.assertEqual(0.0, emd(first_signature, second_signature, distance_matrix))
 
+    def test_case_3(self):
+        first_signature = np.array([6.0, 1.0])
+        second_signature = np.array([1.0, 7.0])
+        distance_matrix = np.array([[0.0, 0.0],
+                                    [0.0, 0.0]])
+        self.assertEqual(0.0, emd(first_signature, second_signature, distance_matrix))
 
-def test_case_2():
-    first_signature = np.array([1.0, 1.0])
-    second_signature = np.array([1.0, 1.0])
-    distance_matrix = np.array([[0.0, 1.0],
-                                [1.0, 0.0]])
-    assert 0.0 == emd(first_signature, second_signature, distance_matrix)
-
-
-def test_case_3():
-    first_signature = np.array([6.0, 1.0])
-    second_signature = np.array([1.0, 7.0])
-    distance_matrix = np.array([[0.0, 0.0],
-                                [0.0, 0.0]])
-    assert 0.0 == emd(first_signature, second_signature, distance_matrix)
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.WARNING)
+    unittest.main()
