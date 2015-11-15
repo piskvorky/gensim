8a0d9c9cc91b2e575b5a274519b9d88d0e29cd06
Ólavur Mortensen <olavurmortensen@gmail.com>
2015-11-15 11:52:02 +0100

    Changed pyemd test case names.

--- gensim/test/test_pyemd.py
+++ gensim/test/test_pyemd.py
@@ -8,21 +8,21 @@ import logging
 from gensim.pyemd import emd
 
 class TestPyemd(unittest.TestCase):
-    def test_case_1(self):
+    def test_case_nonzero_dist(self):
         first_signature = np.array([0.0, 1.0])
         second_signature = np.array([5.0, 3.0])
         distance_matrix = np.array([[0.0, 0.5],
                                     [0.5, 0.0]])
         self.assertEqual(3.5, emd(first_signature, second_signature, distance_matrix))
 
-    def test_case_2(self):
+    def test_case_identical_vecs(self):
         first_signature = np.array([1.0, 1.0])
         second_signature = np.array([1.0, 1.0])
         distance_matrix = np.array([[0.0, 1.0],
                                     [1.0, 0.0]])
         self.assertEqual(0.0, emd(first_signature, second_signature, distance_matrix))
 
-    def test_case_3(self):
+    def test_case_zero_dist_mat(self):
         first_signature = np.array([6.0, 1.0])
         second_signature = np.array([1.0, 7.0])
         distance_matrix = np.array([[0.0, 0.0],
