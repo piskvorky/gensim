85bd13430ec4155ef8c8e2cd68f8442edc100d70
hanabi1224 <harlowmoo@gmail.com>
2016-01-21 19:56:39 -0800

    Try to initialize pyximport in gensim.__init__ & Add warning when slow versions of word2vec and doc2vec are being used

--- gensim/__init__.py
+++ gensim/__init__.py
@@ -3,7 +3,6 @@ This package contains interfaces and functionality to compute pair-wise document
 similarities within a corpus of documents.
 """
 
-from gensim import parsing, matutils, interfaces, corpora, models, similarities, summarization
 import logging
 
 try:
@@ -11,25 +10,32 @@ try:
 except:
     __version__ = '?'
 
-
 class NullHandler(logging.Handler):
     """For python versions <= 2.6; same as `logging.NullHandler` in 2.7."""
     def emit(self, record):
         pass
-
 logger = logging.getLogger('gensim')
 if len(logger.handlers) == 0:	# To ensure reload() doesn't add another one
     logger.addHandler(NullHandler())
 
-
-def get_includes():
-    """
-        get folder paths that contain .h files
-    """
-    import os.path
-    # voidptr.h under models
-    model_path = os.path.dirname(os.path.abspath(models.__file__))
-    return [
-        model_path,
-    ]
+# try to initialize pyximport before importing models otherwise cython code in word2vec and doc2doc does not compile
+try:
+    import pyximport
+    def get_includes():
+        """
+            get folder paths that contain .h files
+        """
+        import os.path
+        # voidptr.h under models
+        model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'models')
+        return [
+            model_path,
+        ]
     
+    from numpy import get_include as np_get_include
+    include_dirs = [np_get_include()] + get_includes()
+    pyximport.install(setup_args={"include_dirs":include_dirs}, reload_support=True)
+except ImportError as err:
+    logger.warning("Fail to initialize pyximport: {0}".format(err))
+
+from gensim import parsing, matutils, interfaces, models, corpora, similarities, summarization
--- gensim/models/word2vec.py
+++ gensim/models/word2vec.py
@@ -100,8 +100,10 @@ try:
     from gensim.models.word2vec_inner import train_batch_sg, train_batch_cbow
     from gensim.models.word2vec_inner import score_sentence_sg, score_sentence_cbow
     from gensim.models.word2vec_inner import FAST_VERSION, MAX_WORDS_IN_BATCH
+    logger.debug('Fast version of {0} is being used'.format(__name__))
 except ImportError:
     # failed... fall back to plain numpy (20-80x slower training than the above)
+    logger.warning('Slow version of {0} is being used'.format(__name__))
     FAST_VERSION = -1
     MAX_WORDS_IN_BATCH = 10000
 
