e801e0f297d4301fe598b49f8bff6ddb1f5ded07
hanabi1224 <harlowmoo@gmail.com>
2016-01-28 21:00:31 -0800

    Revert cython related change in gensim.__int__

--- gensim/__init__.py
+++ gensim/__init__.py
@@ -3,6 +3,7 @@ This package contains interfaces and functionality to compute pair-wise document
 similarities within a corpus of documents.
 """
 
+from gensim import parsing, matutils, interfaces, corpora, models, similarities, summarization
 import logging
 
 try:
@@ -10,37 +11,12 @@ try:
 except:
     __version__ = '?'
 
+
 class NullHandler(logging.Handler):
     """For python versions <= 2.6; same as `logging.NullHandler` in 2.7."""
     def emit(self, record):
         pass
+
 logger = logging.getLogger('gensim')
 if len(logger.handlers) == 0:	# To ensure reload() doesn't add another one
     logger.addHandler(NullHandler())
-
-# try to initialize pyximport before importing models otherwise cython code in word2vec and doc2doc does not compile
-try:
-    import platform
-    if platform.system().lower() == u'windows':
-        import pyximport
-        def get_includes():
-            """
-                get folder paths that contain .h files
-            """
-            import os.path
-            # voidptr.h under models
-            model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'models')
-            return [
-                model_path,
-            ]
-        
-        from numpy import get_include as np_get_include
-        include_dirs = [np_get_include()] + get_includes()
-        pyximport.install(setup_args={"include_dirs":include_dirs}, reload_support=True)
-    else:
-        # TODO: pyximport initialization for other OS
-        pass
-except ImportError as err:
-    logger.warning("Fail to initialize pyximport: {0}".format(err))
-
-from gensim import parsing, matutils, interfaces, models, corpora, similarities, summarization
