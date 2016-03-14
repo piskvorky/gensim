279cd0039e02a7c7d577e13c9d68bed853cf667a
Manas Ranjan Kar <manasrkar.nmims@gmail.com>
2016-03-14 15:29:19 +0530

    Update glove2word2vec.py
    More suggestions integrated. Hopefully, didn't miss anything.

--- glove2word2vec.py
+++ glove2word2vec.py
@@ -7,22 +7,27 @@
 """
 CLI USAGE: python glove2word2vec.py <GloVe vector file> <Output model file>
 
-Convert GloVe vectors into word2vec C format;
-
+Convert GloVe vectors into word2vec C format for usage;
 model = gensim.models.Word2Vec.load_word2vec_format('/tmp/vectors.txt', binary=False)  # C text format
 
-word2vec embeddings start with a line with the number of lines (tokens?) and the number of dimensions of the file. This allows gensim to allocate memory 
+Word2vec embeddings start with a line with the number of lines (tokens?) and the number of dimensions of the file. This allows gensim to allocate memory 
 accordingly for querying the model. Larger dimensions mean larger memory is held captive. Accordingly, this line has to be inserted into the GloVe 
 embeddings file.
 """
+
+
 import re
 import sys
 import gensim
+import logging
 import smart_open
 
+program=sys.argv[0]
+logger = logging.getLogger(program)
+logging.basicConfig(format='%(asctime)s : %(threadName)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-def glove2word2vec(glove_vector_file,output_model_file):
-
+def glove2word2vec(glove_vector_file, output_model_file):
+    """Convert GloVe vectors into word2vec C format"""
     
     def get_info(glove_file_name):
         """ 
@@ -35,37 +40,41 @@ def glove2word2vec(glove_vector_file,output_model_file):
         else:
             dims=re.findall('\d+',glove_vector_file.split('.')[2])
             dims=''.join(dims)
-        return num_lines,dims
+        return num_lines, dims
     
     def prepend_line(infile, outfile, line):
         """ 
         Function to prepend lines using smart_open
         """
-        with smart_open.smart_open(infile, 'rb') as old:
-            with smart_open.smart_open(outfile, 'wb') as new:
-                new.write(str(line) + "\n")
+        with smart_open.smart_open(infile, ' rb ') as old:
+            with smart_open.smart_open(outfile, ' wb ') as new:
+                new.write(str(line) + " \n ")
                 for line in old:
                     new.write(line)
         return outfile
         
+    num_lines, dims= get_info(glove_vector_file)
     
-    num_lines,dims=get_info(glove_vector_file)
+    logger.info('%d lines with %s dimensions' %(num_lines, dims))
+    
     gensim_first_line = "{} {}".format(num_lines, dims)
+    model_file=prepend_line(glove_vector_file, output_model_file, gensim_first_line)
     
-    print '%s lines with %s dimensions' %(num_lines,dims)
-    
-    model_file=prepend_line(glove_vector_file,output_model_file,gensim_first_line)
+    logger.info('Model %s successfully created !!'%output_model_file)
     
     # Demo: Loads the newly created glove_model.txt into gensim API.  
-    model=gensim.models.Word2Vec.load_word2vec_format(model_file,binary=False) #GloVe Model  
-    print 'Most similar to king are: ', model.most_similar(positive=['king'], topn=10)
-    print 'Similarity score between woman and man is: ', model.similarity('woman', 'man')
-    print 'Model %s successfully created !!'%output_model_file
+    model=gensim.models.Word2Vec.load_word2vec_format(model_file, binary=False) #GloVe Model  
+
+    logger.info('Most similar to king are:%s '%model.most_similar(positive=['king'], topn=10))
+    logger.info('Similarity score between woman and man is %s ' %model.similarity('woman', 'man'))
+    
+    logger.info("Finished running %s", program)
     
     return model_file
 
 if __name__ == "__main__":
-
+    
     glove_vector_file=sys.argv[1]
     output_model_file=sys.argv[2]
-    glove2word2vec(glove_vector_file,output_model_file)
+
+    glove2word2vec(glove_vector_file, output_model_file)
