a304469d470c66c28e63aa4b97a95b8a26d0dbdc
Manas Ranjan Kar <manasrkar.nmims@gmail.com>
2016-03-15 13:18:09 +0530

    Update glove2word2vec.py
    Updated with more suggestions from @piskvorky

--- glove2word2vec.py
+++ glove2word2vec.py
@@ -5,26 +5,25 @@
 # Licensed under the GNU LGPL v2.1 - http://www.gnu.org/licenses/lgpl.html
 
 """
-CLI USAGE: python glove2word2vec.py <GloVe vector file> <Output model file>
+CLI USAGE: python glove2word2vec.py -input <GloVe vector file> -output <Output model file>
 
-Convert GloVe vectors into word2vec C format for usage;
+Convert GloVe vectors into word2vec C format;
+
 model = gensim.models.Word2Vec.load_word2vec_format('/tmp/vectors.txt', binary=False)  # C text format
 
-Word2vec embeddings start with a line with the number of lines (tokens?) and the number of dimensions of the file. This allows gensim to allocate memory 
+word2vec embeddings start with a line with the number of lines (tokens?) and the number of dimensions of the file. This allows gensim to allocate memory 
 accordingly for querying the model. Larger dimensions mean larger memory is held captive. Accordingly, this line has to be inserted into the GloVe 
 embeddings file.
 """
-
 
 import re
 import sys
 import gensim
 import logging
+import argparse
 import smart_open
 
-program=sys.argv[0]
-logger = logging.getLogger(program)
-logging.basicConfig(format='%(asctime)s : %(threadName)s : %(levelname)s : %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def glove2word2vec(glove_vector_file, output_model_file):
     """Convert GloVe vectors into word2vec C format"""
@@ -46,35 +45,44 @@ def glove2word2vec(glove_vector_file, output_model_file):
         """ 
         Function to prepend lines using smart_open
         """
-        with smart_open.smart_open(infile, ' rb ') as old:
-            with smart_open.smart_open(outfile, ' wb ') as new:
+        with smart_open.smart_open(infile, 'rb') as old:
+            with smart_open.smart_open(outfile, 'wb') as new:
                 new.write(str(line) + " \n ")
                 for line in old:
                     new.write(line)
         return outfile
         
     num_lines, dims= get_info(glove_vector_file)
-    
-    logger.info('%d lines with %s dimensions' %(num_lines, dims))
-    
     gensim_first_line = "{} {}".format(num_lines, dims)
     model_file=prepend_line(glove_vector_file, output_model_file, gensim_first_line)
-    
-    logger.info('Model %s successfully created !!'%output_model_file)
-    
-    # Demo: Loads the newly created glove_model.txt into gensim API.  
-    model=gensim.models.Word2Vec.load_word2vec_format(model_file, binary=False) #GloVe Model  
 
-    logger.info('Most similar to king are:%s '%model.most_similar(positive=['king'], topn=10))
-    logger.info('Similarity score between woman and man is %s ' %model.similarity('woman', 'man'))
-    
-    logger.info("Finished running %s", program)
-    
-    return model_file
+    model=gensim.models.Word2Vec.load_word2vec_format(model_file, binary=False)
+    return num_lines, dims, model, model_file
 
 if __name__ == "__main__":
-    
-    glove_vector_file=sys.argv[1]
-    output_model_file=sys.argv[2]
 
-    glove2word2vec(glove_vector_file, output_model_file)
+    program = sys.argv[0]
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("-input", help="Use GloVe model file to convert into word2vec C format", type=str, required=True)
+    parser.add_argument("-output", help="Use file OUTPUT to save the resulting word vectors", type=str, default='output_vectors.txt')
+
+    logging.basicConfig(format='%(asctime)s : %(threadName)s : %(levelname)s : %(message)s', level=logging.INFO)
+    logging.root.setLevel(level=logging.INFO)
+    logger.info("running %s" % ' '.join(sys.argv))
+    
+    args = parser.parse_args()
+
+    glove_vector_file= args.input
+    output_model_file= args.output
+
+    num_lines, dims, model, model_file= glove2word2vec(glove_vector_file, output_model_file)
+
+    logger.info('%d lines with %s dimensions', (num_lines, dims))
+    logger.info('Model %s successfully created',output_model_file)
+    
+    logger.info('Testing the model....')
+    logger.info('Most similar to king are:%s ', model.most_similar(positive=['king'], topn=10))
+    logger.info('Similarity score between woman and man is %s ' , model.similarity('woman', 'man'))
+    
+    logger.info("Finished running %s", program)
