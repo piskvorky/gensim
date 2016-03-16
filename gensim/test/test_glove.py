2662d2d30a5eeb4fc22e9d3375c025cea04a34b1
Manas Ranjan Kar <manasrkar.nmims@gmail.com>
2016-03-16 12:59:48 +0530

    Test file for glove2word2vec.py

---
+++ gensim/test/test_glove.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016 Manas Ranjan Kar <manasrkar91@gmail.com>
+# Licensed under the GNU LGPL v2.1 - http://www.gnu.org/licenses/lgpl.html
+
+"""
+Test for gensim.scripts.glove2word2vec.py
+"""
+
+import os
+import gensim
+from gensim.utils import check_output
+
+module_path = os.path.dirname(gensim.__file__)  
+datapath = os.path.join(module_path, 'test_data', 'testglove.txt') # Sample data files are located in the same folder
+
+output_file = 'sample_word2vec_out.txt'
+
+output = check_output(['python', 'gensim.scripts.glove2word2vec.py', '-i', datapath, '-o', output_file])
+
