be38e36f1e790e0cb1ed88f30620fadd7454fcd1
Manas Ranjan Kar <manasrkar.nmims@gmail.com>
2016-03-23 22:11:39 +0530

    Update glove2word2vec.py

--- gensim/scripts/glove2word2vec.py
+++ gensim/scripts/glove2word2vec.py
@@ -35,7 +35,7 @@ def get_glove_info(glove_file_name):
 def glove2word2vec(glove_input_file, word2vec_output_file):
     """Convert `glove_input_file` in GloVe format into `word2vec_output_file` in word2vec format."""
     num_lines, num_dims = get_glove_info(glove_input_file)
-    header = "{0} {0}".format(num_lines, num_dims)
+    header = "{} {}".format(num_lines, num_dims)
     logger.info("converting %i vectors from %s to %s", num_lines, glove_input_file, word2vec_output_file)
 
     with smart_open(word2vec_output_file, 'wb') as fout:
