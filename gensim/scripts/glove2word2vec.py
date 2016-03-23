b16b94320d0f2f224b3722dd3111adff4884029e
Manas Ranjan Kar <manasrkar.nmims@gmail.com>
2016-03-23 18:20:28 +0530

    Update glove2word2vec.py

--- gensim/scripts/glove2word2vec.py
+++ gensim/scripts/glove2word2vec.py
@@ -39,7 +39,7 @@ def glove2word2vec(glove_input_file, word2vec_output_file):
 
     with smart_open(word2vec_output_file, 'w') as fout:
         fout.write("%s %s\n" % (str(num_lines), str(num_dims)))
-        with smart_open(glove_input_file, 'r') as fin:
+        with smart_open(glove_input_file, 'rb') as fin:
             for line in fin:
                 fout.write(line)
     return num_lines, num_dims
