9245a4d9d29a5ecf666099be658a2af294e740e0
dsquareindia <ashu.9412@gmail.com>
2016-05-27 16:20:56 -0400

    Made minor changes.

--- gensim/segmentation.py
+++ gensim/segmentation.py
@@ -16,8 +16,14 @@ logger = logging.getLogger(__name__)
 def S_One_Pre(topics):
     """
     This function performs S_One_Pre segmentation on a list of topics.
-    S_One_Pre segmentation is defined as: {(W', W*) | W' = {w_i};
-                                                      W* = {w_j}; w_i, w_j belongs to W; i > j}
+    S_One_Pre segmentation is defined as: S_One_Pre = {(W', W*) | W' = {w_i};
+                                                                  W* = {w_j}; w_i, w_j belongs to W; i > j}
+    Example usage:
+
+        >>> topics = [np.array([9, 10, 7])
+        >>> S_One_Pre(topics[0])
+        {0: [(10, 9), (7, 9), (7, 10)]}
+
     Args:
     ----
     topics : list of topics obtained from an algorithm such as LDA. Is a list such as [array([ 9, 10, 11]), array([ 9, 10,  7]), ...]
@@ -30,9 +36,9 @@ def S_One_Pre(topics):
 
     for t, top_words in enumerate(topics):
         s_one_pre_t = []
-        for w_star in top_words[1:]:
-            w_star_index = np.where(top_words == w_star)[0] # To get index of w_star in top_words
-            for w_prime in top_words[:w_star_index]:
-                s_one_pre_t.append((w_star, w_prime))
+        for w_prime in top_words[1:]:
+            w_prime_index = np.where(top_words == w_prime)[0] # To get index of w_star in top_words
+            for w_star in top_words[:w_prime_index]:
+                s_one_pre_t.append((w_prime, w_star))
         s_one_pre[t] = s_one_pre_t
     return s_one_pre
