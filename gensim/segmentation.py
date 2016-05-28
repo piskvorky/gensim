cfda773fe44ca7041a346e38b51199fb1c0a1a51
dsquareindia <ashu.9412@gmail.com>
2016-05-28 00:55:51 -0400

    Returns a list now.

--- gensim/segmentation.py
+++ gensim/segmentation.py
@@ -18,11 +18,11 @@ def S_One_Pre(topics):
     This function performs S_One_Pre segmentation on a list of topics.
     S_One_Pre segmentation is defined as: S_One_Pre = {(W', W*) | W' = {w_i};
                                                                   W* = {w_j}; w_i, w_j belongs to W; i > j}
-    Example usage:
+    Example:
 
-        >>> topics = [np.array([9, 10, 7])
-        >>> S_One_Pre(topics[0])
-        {0: [(10, 9), (7, 9), (7, 10)]}
+        >>> topics = [np.array([1, 2, 3]), np.array([4, 5, 6])]
+        >>> S_One_Pre(topics)
+        [[(2, 1), (3, 1), (3, 2)], [(5, 4), (6, 4), (6, 5)]]
 
     Args:
     ----
@@ -30,9 +30,9 @@ def S_One_Pre(topics):
 
     Returns:
     -------
-    s_one_pre : Dictionary of list of (W', W*) tuples for all unique topic ids
+    s_one_pre : list of list of (W', W*) tuples for all unique topic ids
     """
-    s_one_pre = {}
+    s_one_pre = []
 
     for t, top_words in enumerate(topics):
         s_one_pre_t = []
@@ -40,5 +40,32 @@ def S_One_Pre(topics):
             w_prime_index = np.where(top_words == w_prime)[0] # To get index of w_star in top_words
             for w_star in top_words[:w_prime_index]:
                 s_one_pre_t.append((w_prime, w_star))
-        s_one_pre[t] = s_one_pre_t
-    return s_one_pre
+        s_one_pre.append(s_one_pre_t)
+
+    return s_one_pre # need to make generator
+
+def S_One_Set(topics):
+    """
+    This function performs S_One_Set segmentation on a list of topics.
+    S_One_Set segmentation is defined as: S_One_Set = {(W', W*) | W' = {w_i}; w_i belongs to W;
+                                                                  W* = W}
+    Example:
+        >>> topics = [np.array([9, 10, 7])
+        >>> S_One_(topics[0])
+    Args:
+    ----
+    topics : list of topics obtained from an algorithm such as LDA. Is a list such as [array([ 9, 10, 11]), array([ 9, 10,  7]), ...]
+
+    Returns:
+    -------
+    s_one_set : list of list of (W', W*) tuples for all unique topic ids.
+    """
+    s_one_set = []
+
+    for t, top_words in enumerate(topics):
+        s_one_set_t = []
+        for w_prime in top_words:
+            s_one_set_t.append((w_prime, top_words))
+        s_one_set.append(s_one_set_t)
+
+    return s_one_set # need to yield
