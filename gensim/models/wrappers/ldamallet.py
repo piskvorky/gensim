5d9149a39f1a7614cff251fd810d507fda9903a5
dsquareindia <ashu.9412@gmail.com>
2016-07-04 11:26:41 +0530

    Made changes in documentation, indentation.

--- gensim/models/wrappers/ldamallet.py
+++ gensim/models/wrappers/ldamallet.py
@@ -358,16 +358,16 @@ def malletmodel2ldamodel(mallet_model, gamma_threshold=0.001, iterations=50):
     Args:
     ----
     mallet_model : Trained mallet model
-    gamma_threshold : To be used for LdaModel training.
-    iterations : number of iterations to be used for LdaModel training.
+    gamma_threshold : To be used for inference in the new LdaModel.
+    iterations : number of iterations to be used for inference in the new LdaModel.
 
     Returns:
     -------
     model_gensim : LdaModel instance; copied gensim LdaModel
     """
     model_gensim = LdaModel(
-                id2word=mallet_model.id2word, num_topics=mallet_model.num_topics,
-                alpha=mallet_model.alpha, iterations=iterations,
-                gamma_threshold=gamma_threshold)
+        id2word=mallet_model.id2word, num_topics=mallet_model.num_topics,
+        alpha=mallet_model.alpha, iterations=iterations,
+        gamma_threshold=gamma_threshold)
     model_gensim.expElogbeta[:] = mallet_model.wordtopics
     return model_gensim
