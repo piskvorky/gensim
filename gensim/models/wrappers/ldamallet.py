fb8e4e5ea0da6530cf61ef09b88666e19ad22932
Devashish Deshpande <ashu.9412@gmail.com>
2016-07-06 21:45:40 +0530

    Add malletmodel2ldamodel transformation function (#766)

--- gensim/models/wrappers/ldamallet.py
+++ gensim/models/wrappers/ldamallet.py
@@ -44,6 +44,7 @@ from smart_open import smart_open
 
 from gensim import utils, matutils
 from gensim.utils import check_output
+from gensim.models.ldamodel import LdaModel
 
 logger = logging.getLogger(__name__)
 
@@ -350,3 +351,27 @@ class LdaMallet(utils.SaveLoad):
                     if total_weight:
                         doc = [(id_, float(weight) / total_weight) for id_, weight in doc]
                 yield doc
+
+
+def malletmodel2ldamodel(mallet_model, gamma_threshold=0.001, iterations=50):
+    """
+    Function to convert mallet model to gensim LdaModel. This works by copying the
+    training model weights (alpha, beta...) from a trained mallet model into the
+    gensim model.
+
+    Args:
+    ----
+    mallet_model : Trained mallet model
+    gamma_threshold : To be used for inference in the new LdaModel.
+    iterations : number of iterations to be used for inference in the new LdaModel.
+
+    Returns:
+    -------
+    model_gensim : LdaModel instance; copied gensim LdaModel
+    """
+    model_gensim = LdaModel(
+        id2word=mallet_model.id2word, num_topics=mallet_model.num_topics,
+        alpha=mallet_model.alpha, iterations=iterations,
+        gamma_threshold=gamma_threshold)
+    model_gensim.expElogbeta[:] = mallet_model.wordtopics
+    return model_gensim
