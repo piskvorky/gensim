8ab18860551a5a84370ddd8d0d5a0f920f7e1952
Devashish Deshpande <ashu.9412@gmail.com>
2016-07-20 20:58:52 +0530

    Add converter of LDA model in VW and gensim (#798)

--- gensim/models/wrappers/ldavowpalwabbit.py
+++ gensim/models/wrappers/ldavowpalwabbit.py
@@ -66,6 +66,7 @@ import subprocess
 import numpy
 
 from gensim import utils, matutils
+from gensim.models.ldamodel import LdaModel
 
 LOG = logging.getLogger(__name__)
 
@@ -559,3 +560,25 @@ def _run_vw_command(cmd):
 def _bit_length(num):
     """Return number of bits needed to encode given number."""
     return len(bin(num).lstrip('-0b'))
+
+def vwmodel2ldamodel(vw_model, iterations=50):
+    """
+    Function to convert vowpal wabbit model to gensim LdaModel. This works by
+    simply copying the training model weights (alpha, beta...) from a trained
+    vwmodel into the gensim model.
+
+    Args:
+    ----
+    vw_model : Trained vowpal wabbit model.
+    iterations : Number of iterations to be used for inference of the new LdaModel.
+
+    Returns:
+    -------
+    model_gensim : LdaModel instance; copied gensim LdaModel.
+    """
+    model_gensim = LdaModel(
+        num_topics=vw_model.num_topics, id2word=vw_model.id2word, chunksize=vw_model.chunksize,
+        passes=vw_model.passes, alpha=vw_model.alpha, eta=vw_model.eta, decay=vw_model.decay,
+        offset=vw_model.offset, iterations=iterations, gamma_threshold=vw_model.gamma_threshold)
+    model_gensim.expElogbeta[:] = vw_model._get_topics()
+    return model_gensim
