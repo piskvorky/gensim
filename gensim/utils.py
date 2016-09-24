b11e13d2b20b696e58430a08ad99442a52e02ca6
Aman <metalaman@gmail.com>
2016-09-24 22:01:49 +0530

    Update utils.py

--- gensim/utils.py
+++ gensim/utils.py
@@ -1164,4 +1164,4 @@ def sample_dict(d, n=10, use_random=True):
      selected_keys = [(key, d[key]) for key in selected_keys]
      if selected_keys is not None:
 		return True,selected_keys
-     return False	
+     return False,None
