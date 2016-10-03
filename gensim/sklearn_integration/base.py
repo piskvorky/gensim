4386c2f9a6309a503bd8fabc588f6c2a02f7e538
AadityaJ <aadityajamuar@gmail.com>
2016-10-03 22:23:27 +0530

    adding base code sklearn wrapper and relevant test cases:

---
+++ gensim/sklearn_integration/base.py
@@ -0,0 +1,9 @@
+"""
+scikit learn interface for gensim for easy use of gensim with scikit-learn
+"""
+import numpy as np
+class BaseClass(object):
+    def __init__(self):
+        """init"""
+    def run(self):
+        return return np.array([0,0,0])
