d5149fd25175a3f9388646150b6730c062cc7d5c
Mohit Rathore <mrmohitrathoremr@gmail.com>
2016-10-09 12:16:12 +0530

    fix #795

--- gensim/models/doc2vec.py
+++ gensim/models/doc2vec.py
@@ -529,9 +529,9 @@ class Doctag(namedtuple('Doctag', 'offset, word_count, doc_count')):
 
 class Doc2Vec(Word2Vec):
     """Class for training, using and evaluating neural networks described in http://arxiv.org/pdf/1405.4053v2.pdf"""
-    def __init__(self, documents=None, size=300, alpha=0.025, window=8, min_count=5,
-                 max_vocab_size=None, sample=0, seed=1, workers=1, min_alpha=0.0001,
-                 dm=1, hs=1, negative=0, dbow_words=0, dm_mean=0, dm_concat=0, dm_tag_count=1,
+    def __init__(self, documents=None, size=300, alpha=0.025, min_count=5,
+                 max_vocab_size=None, seed=1, workers=1, min_alpha=0.0001,
+                 dm=1, dbow_words=0, dm_concat=0, dm_tag_count=1,
                  docvecs=None, docvecs_mapfile=None, comment=None, trim_rule=None, **kwargs):
         """
         Initialize the model from an iterable of `documents`. Each document is a
@@ -601,9 +601,9 @@ class Doc2Vec(Word2Vec):
 
         """
         super(Doc2Vec, self).__init__(
-            size=size, alpha=alpha, window=window, min_count=min_count, max_vocab_size=max_vocab_size,
-            sample=sample, seed=seed, workers=workers, min_alpha=min_alpha,
-            sg=(1+dm) % 2, hs=hs, negative=negative, cbow_mean=dm_mean,
+            size=size, alpha=alpha, min_count=min_count, max_vocab_size=max_vocab_size,
+            seed=seed, workers=workers, min_alpha=min_alpha,
+            sg=(1+dm) % 2,
             null_word=dm_concat, **kwargs)
         self.dbow_words = dbow_words
         self.dm_concat = dm_concat
