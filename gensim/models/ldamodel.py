d2b23c62222598acca3a4c499417c775c2444c85
Partho Mandal <ppmandal@asu.edu>
2016-10-21 04:16:45 -0700

    Update ldamodel.py

--- gensim/models/ldamodel.py
+++ gensim/models/ldamodel.py
@@ -907,6 +907,7 @@ class LdaModel(interfaces.TransformationABC, basemodel.BaseTopicModel):
         """
         if chunksize is None:
             chunksize = self.chunksize
+        chunksize = min(chunksize, 2) # chunksize should be > 1
             
         if minimum_probability is None:
             minimum_probability = self.minimum_probability
