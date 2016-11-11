17a01cfa6363eb0c13600e4abcc7cfe68fd9c38d
Partho Mandal <ppmandal@asu.edu>
2016-11-11 06:34:14 -0700

    Fix #963 : Passing all the params when calling lda.get_document_topics for a corpus (#978)
    * Fix PR #963
    
    * Added entry in CHANGELOG.md
    
    * Revert "Added entry in CHANGELOG.md"
    
    This reverts commit 49dce4da4116c1e0f7bfc6a15646d1e7752c0257.
    
    * Added new entry in CHANGELOG.md
    
    * Update test_ldamodel.py
    
    * Update CHANGELOG.md
    
    * Updated tutorial for get_document_topics
    
    * Added parameters in ldamulticore constructor
    
    * Added parameters for ldamulticore
    
    * Update test_ldamodel.py
    
    * test added to check filtering effect of minimum_probability and minimum_phi_value

--- gensim/test/test_ldamodel.py
+++ gensim/test/test_ldamodel.py
@@ -249,6 +249,55 @@ class TestLdaModel(unittest.TestCase, basetests.TestBaseTopicModel):
                 self.assertTrue(isinstance(k, int))
                 self.assertTrue(isinstance(v, float))
 
+        #Test case to use the get_document_topic function for the corpus
+        all_topics = model.get_document_topics(self.corpus, per_word_topics=True)
+        
+        self.assertEqual(model.state.numdocs, len(corpus))
+        
+        for topic in all_topics:
+            self.assertTrue(isinstance(topic, tuple))
+            for k, v in topic[0]: # list of doc_topics
+                self.assertTrue(isinstance(k, int))
+                self.assertTrue(isinstance(v, float))
+
+            for w, topic_list in topic[1]: # list of word_topics
+                self.assertTrue(isinstance(w, int))
+                self.assertTrue(isinstance(topic_list, list))
+
+            for w, phi_values in topic[2]: # list of word_phis
+                self.assertTrue(isinstance(w, int))
+                self.assertTrue(isinstance(phi_values, list))
+
+        #Test case to check the filtering effect of minimum_probability and minimum_phi_value
+        doc_topic_count_na = 0
+        word_phi_count_na = 0
+
+        all_topics = model.get_document_topics(self.corpus, minimum_probability=0.8, minimum_phi_value=1.0, per_word_topics=True)
+        
+        self.assertEqual(model.state.numdocs, len(corpus))
+        
+        for topic in all_topics:
+            self.assertTrue(isinstance(topic, tuple))
+            for k, v in topic[0]: # list of doc_topics
+                self.assertTrue(isinstance(k, int))
+                self.assertTrue(isinstance(v, float))
+                if len(topic[0]) != 0:
+                    doc_topic_count_na += 1
+
+            for w, topic_list in topic[1]: # list of word_topics
+                self.assertTrue(isinstance(w, int))
+                self.assertTrue(isinstance(topic_list, list))
+
+            for w, phi_values in topic[2]: # list of word_phis
+                self.assertTrue(isinstance(w, int))
+                self.assertTrue(isinstance(phi_values, list))
+                if len(phi_values) != 0:
+                    word_phi_count_na += 1
+
+        self.assertTrue(model.state.numdocs > doc_topic_count_na)
+        self.assertTrue( sum([len(i) for i in corpus]) > word_phi_count_na)
+
+
         doc_topics, word_topics, word_phis = model.get_document_topics(self.corpus[1], per_word_topics=True)
 
         for k, v in doc_topics:
