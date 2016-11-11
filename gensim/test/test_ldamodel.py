6c60c1d1509b1c0f67a30188938674546898e967
anmol01gulati <anmol01gulati@gmail.com>
2016-11-11 10:14:25 +0530

    Removed blank line in test_ldamodel, work around for Travis-CI issue #971

--- gensim/test/test_ldamodel.py
+++ gensim/test/test_ldamodel.py
@@ -299,7 +299,6 @@ class TestLdaModel(unittest.TestCase, basetests.TestBaseTopicModel):
     def testPasses(self):
         # long message includes the original error message with a custom one
         self.longMessage = True
-
         # construct what we expect when passes aren't involved
         test_rhots = list()
         model = self.class_(id2word=dictionary, chunksize=1, num_topics=2)
