73478279e4aa5dca59d1bf49a5528afafd65b107
Sourav Singh <ssouravsingh12@gmail.com>
2016-11-13 08:08:12 -0800

    PEP8 fixes for Parsing

--- gensim/scripts/glove2word2vec.py
+++ gensim/scripts/glove2word2vec.py
@@ -40,7 +40,11 @@ def get_glove_info(glove_file_name):
 def glove2word2vec(glove_input_file, word2vec_output_file):
     """Convert `glove_input_file` in GloVe format into `word2vec_output_file in word2vec format."""
     num_lines, num_dims = get_glove_info(glove_input_file)
-    logger.info("converting %i vectors from %s to %s", num_lines, glove_input_file, word2vec_output_file)
+    logger.info(
+        "converting %i vectors from %s to %s",
+        num_lines,
+        glove_input_file,
+        word2vec_output_file)
     if sys.version_info < (3,):
         with smart_open(word2vec_output_file, 'wb') as fout:
             fout.write("%s %s\n" % (num_lines, num_dims))
@@ -58,7 +62,9 @@ def glove2word2vec(glove_input_file, word2vec_output_file):
 
 
 if __name__ == "__main__":
-    logging.basicConfig(format='%(asctime)s : %(threadName)s : %(levelname)s : %(message)s', level=logging.INFO)
+    logging.basicConfig(
+        format='%(asctime)s : %(threadName)s : %(levelname)s : %(message)s',
+        level=logging.INFO)
     logging.root.setLevel(level=logging.INFO)
     logger.info("running %s", ' '.join(sys.argv))
 
@@ -79,28 +85,47 @@ if __name__ == "__main__":
 
     # do the actual conversion
     num_lines, num_dims = glove2word2vec(args.input, args.output)
-    logger.info('Converted model with %i vectors and %i dimensions', num_lines, num_dims)
+    logger.info(
+        'Converted model with %i vectors and %i dimensions',
+        num_lines,
+        num_dims)
     # test that the converted model loads successfully
-    model = gensim.models.Word2Vec.load_word2vec_format(args.output, binary=False)
+    model = gensim.models.Word2Vec.load_word2vec_format(
+        args.output, binary=False)
     logger.info('Model %s successfully loaded', model)
     try:
         logger.info('testing the model....')
         if sys.version_info < (3,):
             with smart_open(args.output, 'rb') as f:
-                seed_word1, seed_word2 = random.sample([line.split()[0] for line in f], 2)
+                seed_word1, seed_word2 = random.sample(
+                    [line.split()[0] for line in f], 2)
         else:
             with smart_open(args.output, 'r') as f:
-                seed_word1, seed_word2 = random.sample([line.split()[0] for line in f], 2)
-        logger.info('top-10 most similar words to "%s" are: %s', seed_word1, model.most_similar(positive=[seed_word1], topn=10))
-        logger.info('similarity score between %s and %s: %s', seed_word1, seed_word2, model.similarity(seed_word1, seed_word2))
+                seed_word1, seed_word2 = random.sample(
+                    [line.split()[0] for line in f], 2)
+        logger.info(
+            'top-10 most similar words to "%s" are: %s',
+            seed_word1,
+            model.most_similar(
+                positive=[seed_word1],
+                topn=10))
+        logger.info(
+            'similarity score between %s and %s: %s',
+            seed_word1,
+            seed_word2,
+            model.similarity(
+                seed_word1,
+                seed_word2))
     except:
-        logger.error('error encountered. checking for model file creation now....')
+        logger.error(
+            'error encountered. checking for model file creation now....')
         if os.path.isfile(os.path.join(args.output)):
-            logger.info('model file %s was created but could not be loaded.', args.output)
+            logger.info(
+                'model file %s was created but could not be loaded.',
+                args.output)
         else:
             logger.info('model file %s creation failed. ')
         logger.info('please check the parameters and input file format.')
         raise
 
     logger.info("finished running %s", program)
-    
