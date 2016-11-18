7045ea7d1c412fc2db0d413d037ccb49c369c0c0
Ólavur Mortensen <olavurmortensen@gmail.com>
2016-11-18 13:39:21 +0100

    Just removed a print statement.

--- gensim/models/ldamodel.py
+++ gensim/models/ldamodel.py
@@ -547,7 +547,6 @@ class LdaModel(interfaces.TransformationABC, basemodel.BaseTopicModel):
         corpus_words = sum(cnt for document in chunk for _, cnt in document)
         subsample_ratio = 1.0 * total_docs / len(chunk)
         perwordbound = self.bound(chunk, subsample_ratio=subsample_ratio) / (subsample_ratio * corpus_words)
-        print(perwordbound)
         logger.info("%.3f per-word bound, %.1f perplexity estimate based on a held-out corpus of %i documents with %i words" %
                     (perwordbound, np.exp2(-perwordbound), len(chunk), corpus_words))
         return perwordbound
