734833fe91cba207c45c72f1e8a19163cab4be34
Lev Konstantinovskiy <lev.konst@gmail.com>
2016-12-23 12:42:15 -0200

    Revert wordrank PR (#1056)

--- gensim/models/wrappers/__init__.py
+++ gensim/models/wrappers/__init__.py
@@ -5,4 +5,3 @@ This package contains wrappers for other topic modeling programs.
 from .ldamallet import LdaMallet
 from .dtmmodel import DtmModel
 from .ldavowpalwabbit import LdaVowpalWabbit
-from .wordrank import Wordrank
