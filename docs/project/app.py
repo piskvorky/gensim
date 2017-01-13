eac6796f7109a6c1f1778589396c90db725562f5
markroxor <mrmohitrathoremr@gmail.com>
2017-01-13 23:38:32 +0530

    flask ready

---
+++ docs/project/app.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+from flask_flatpages import FlatPages
+from flask_frozen import Freezer
+
+app = Flask(__name__)
+app.config.from_pyfile('settings.py')
+pages = FlatPages(app)
+freezer = Freezer(app)
---
+++ docs/project/settings.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+REPO_NAME = "gensim"  # Used for FREEZER_BASE_URL
+DEBUG = True
+
+# Assumes the app is located in the same directory
+# where this file resides
+APP_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def parent_dir(path):
+    '''Return the parent of a directory.'''
+    return os.path.abspath(os.path.join(path, os.pardir))
+
+PROJECT_ROOT = parent_dir(APP_DIR)
+# In order to deploy to Github pages, you must build the static files to
+# the project root
+FREEZER_DESTINATION = PROJECT_ROOT
+# Since this is a repo page (not a Github user page),
+# we need to set the BASE_URL to the correct url as per GH Pages' standards
+FREEZER_BASE_URL = "http://localhost/{0}".format(REPO_NAME)
+FREEZER_REMOVE_EXTRA_FILES = False  # IMPORTANT: If this is True, all app files
+                                    # will be deleted when you run the freezer
+FLATPAGES_MARKDOWN_EXTENSIONS = ['codehilite']
+FLATPAGES_ROOT = os.path.join(APP_DIR, 'pages')
+FLATPAGES_EXTENSION = '.md'
