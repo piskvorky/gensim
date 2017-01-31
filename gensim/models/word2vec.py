a18b3df11564d3c3abbb7dba740ecc1fef5f29ce
javkrei-cargox <javier@cargox.com.br>
2017-01-31 14:52:44 -0200

    Fix numerics in word2vec make_cum_table. Fix #865 (#1122)
    * fix #865
    
    * fix #865 bis

--- gensim/models/word2vec.py
+++ gensim/models/word2vec.py
@@ -468,9 +468,11 @@ class Word2Vec(utils.SaveLoad):
         vocab_size = len(self.wv.index2word)
         self.cum_table = zeros(vocab_size, dtype=uint32)
         # compute sum of all power (Z in paper)
-        train_words_pow = float(sum([self.wv.vocab[word].count**power for word in self.wv.vocab]))
+        train_words_pow = 0.0
+        for word_index in xrange(vocab_size):
+            train_words_pow += self.wv.vocab[self.wv.index2word[word_index]].count**power
         cumulative = 0.0
-        for word_index in range(vocab_size):
+        for word_index in xrange(vocab_size):
             cumulative += self.wv.vocab[self.wv.index2word[word_index]].count**power
             self.cum_table[word_index] = round(cumulative / train_words_pow * domain)
         if len(self.cum_table) > 0:
