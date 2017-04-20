edd41a403ae070cea3084d1b539f3e04d30de4c7
Prakhar Pratyush <er.prakhar2b@gmail.com>
2017-04-21 02:34:12 +0530

    handling whitespace error in docsim.py

--- gensim/similarities/docsim.py
+++ gensim/similarities/docsim.py
@@ -190,11 +190,10 @@ class Similarity(interfaces.SimilarityABC):
             # undocumented feature: set output_prefix=None to create the server in temp
             self.output_prefix = utils.randfname(prefix='simserver')
         else:
-            self.output_prefix = os.path.join(os.path.dirname(output_prefix), 'shard', '')   # '' to get the trailing slash
-            
+            self.output_prefix = os.path.join(os.path.dirname(output_prefix), 'shard', '')   # '' to get the trailing slash 
             if not os.path.exists(self.output_prefix):
                 os.makedirs(self.output_prefix)
-            
+
         logger.info("starting similarity index under %s", self.output_prefix)
         self.num_features = num_features
         self.num_best = num_best
@@ -447,10 +446,10 @@ class Similarity(interfaces.SimilarityABC):
         """
         self.close_shard()
         if fname is None:
-            fname = os.path.join(self.output_prefix ,  'index')   #default file name - index
+            fname = os.path.join(self.output_prefix, 'index')   # default file name - index
         else:
-            fname = os.path.join(self.output_prefix , fname)
-            
+            fname = os.path.join(self.output_prefix, fname)
+
         super(Similarity, self).save(fname, *args, **kwargs)
 
     def destroy(self):
