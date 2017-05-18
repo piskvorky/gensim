1e3c59e1f35606ce30dad9fdda567ce6c5c7303b
Chinmaya Pancholi <chinmayapancholi13@gmail.com>
2017-05-18 11:00:45 -0700

    deleted data in  and updated docstring in __init__.py

--- gensim/keras_integration/__init__.py
+++ gensim/keras_integration/__init__.py
@@ -3,6 +3,6 @@
 #
 # Copyright (C) 2011 Radim Rehurek <radimrehurek@seznam.cz>
 # Licensed under the GNU LGPL v2.1 - http://www.gnu.org/licenses/lgpl.html
-"""Keras wrappers for gensim.
+"""Wrappers to allow gensim models as input into Keras.
 Contains various gensim based implementations which can be integrated with Keras.
 """
