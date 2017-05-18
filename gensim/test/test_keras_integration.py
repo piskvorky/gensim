d682180f018ec2039fd1ca3e85cd35be289eccfc
Chinmaya Pancholi <chinmayapancholi13@gmail.com>
2017-05-17 19:43:43 -0700

    PEP8 changes in code

--- gensim/test/test_keras_integration.py
+++ gensim/test/test_keras_integration.py
@@ -1,12 +1,9 @@
-import six
 import unittest
 import os
-import codecs
-import pickle
 import sys
 import keras
 import numpy as np
-from gensim.models import word2vec, keyedvectors
+from gensim.models import word2vec
 from keras.engine import Input
 from keras.models import Model
 from keras.layers import merge
@@ -31,13 +28,16 @@ sentences = [
 module_path = os.path.dirname(__file__) # needed because sample data files are located in the same folder
 datapath = lambda fname: os.path.join(module_path, 'test_data', fname)
 
+
 class TestKerasWord2VecWrapper(unittest.TestCase):
     def setUp(self):
         self.model_cos_sim = word2vec.Word2Vec(sentences, size=100, min_count=1, hs=1)
-        self.model_twenty_ng = word2vec.Word2Vec(word2vec.LineSentence(datapath('20_newsgroup_keras_w2v_data.txt')) ,min_count=1)
+        self.model_twenty_ng = word2vec.Word2Vec(word2vec.LineSentence(datapath('20_newsgroup_keras_w2v_data.txt')), min_count=1)
 
     def testWord2VecTraining(self):
-        """Test word2vec training."""
+        """
+        Test word2vec training.
+        """
         model = self.model_cos_sim
         self.assertTrue(model.wv.syn0.shape == (len(model.wv.vocab), 100))
         self.assertTrue(model.syn1.shape == (len(model.wv.vocab), 100))
@@ -51,7 +51,9 @@ class TestKerasWord2VecWrapper(unittest.TestCase):
         self.assertEqual(sims, sims2)
 
     def testEmbeddingLayerCosineSim(self):
-        """Test Keras 'Embedding' layer returned by 'get_embedding_layer' function for a simple word similarity task."""
+        """
+        Test Keras 'Embedding' layer returned by 'get_embedding_layer' function for a simple word similarity task.
+        """
         keras_w2v_model = self.model_cos_sim
         keras_w2v_model_wv = keras_w2v_model.wv
 
@@ -72,11 +74,11 @@ class TestKerasWord2VecWrapper(unittest.TestCase):
         self.assertTrue(type(output[0][0][0][0]) == np.float32)     #verify that  a float is returned
 
     def testEmbeddingLayer20NewsGroup(self):
-        """Test Keras 'Embedding' layer returned by 'get_embedding_layer' function for a smaller version of the 20NewsGroup classification problem."""
+        """
+        Test Keras 'Embedding' layer returned by 'get_embedding_layer' function for a smaller version of the 20NewsGroup classification problem.
+        """
         TEXT_DATA_DIR = datapath('./20_newsgroup_keras/')
         MAX_SEQUENCE_LENGTH = 1000
-        MAX_NB_WORDS = 20000
-        EMBEDDING_DIM = 100
 
         # Prepare text samples and their labels
 
@@ -109,19 +111,19 @@ class TestKerasWord2VecWrapper(unittest.TestCase):
         tokenizer.fit_on_texts(texts)
         sequences = tokenizer.texts_to_sequences(texts)
 
-        word_index = tokenizer.word_index
+        # word_index = tokenizer.word_index
         data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
         labels = to_categorical(np.asarray(labels))
 
         x_train = data
         y_train = labels
 
-        #prepare the embedding layer using the wrapper
+        # prepare the embedding layer using the wrapper
         Keras_w2v = self.model_twenty_ng
         Keras_w2v_wv = Keras_w2v.wv
         embedding_layer = Keras_w2v_wv.get_embedding_layer()
 
-        #create a 1D convnet to solve our classification task
+        # create a 1D convnet to solve our classification task
         sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
         embedded_sequences = embedding_layer(sequence_input)
         x = Conv1D(128, 5, activation='relu')(embedded_sequences)
@@ -138,8 +140,8 @@ class TestKerasWord2VecWrapper(unittest.TestCase):
         model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['acc'])
         fit_ret_val = model.fit(x_train, y_train, batch_size=1)
 
-        #verify the type of the object returned after training
-        self.assertTrue(type(fit_ret_val)==keras.callbacks.History)     #value returned is a `History` instance. Its `history` attribute contains all information collected during training.
+        # verify the type of the object returned after training
+        self.assertTrue(type(fit_ret_val) == keras.callbacks.History)  # value returned is a `History` instance. Its `history` attribute contains all information collected during training.
 
 
 if __name__ == '__main__':
