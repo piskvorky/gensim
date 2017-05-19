08872c06e00fc3567092cc4d7d155062fe79339a
Menshikh Ivan <menshikh.iv@gmail.com>
2017-05-19 12:31:34 +0500

    Add tests_require to enable extras to install when calling python setup.py test (#1336)
    * add test_req
    
    * mv test env to separate variable (rm duplication)

--- setup.py
+++ setup.py
@@ -226,6 +226,12 @@ Copyright (c) 2009-now Radim Rehurek
 """
 
 
+test_env = ['testfixtures',
+            'unittest2',
+            'Morfessor==2.0.2a4',
+            'scikit-learn',
+            'pyemd']
+
 setup(
     name='gensim',
     version='2.1.0',
@@ -282,15 +288,11 @@ setup(
         'six >= 1.5.0',
         'smart_open >= 1.2.1',
     ],
+    tests_require=test_env,
     extras_require={
         'distributed': ['Pyro4 >= 4.27'],
         'wmd': ['pyemd >= 0.2.0'],
-        'test': [
-            'testfixtures',
-            'unittest2',
-            'Morfessor==2.0.2a4',
-            'scikit-learn'
-        ],
+        'test': test_env,
     },
 
     include_package_data=True,
