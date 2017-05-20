dd743be2fcfbf490772687ffa9ec5f86825b68e6
Chinmaya Pancholi <chinmayapancholi13@gmail.com>
2017-05-19 17:33:55 -0700

    added csv to flake8 ignore list and updated test data slightly

--- gensim/test/test_keras_integration.py
+++ gensim/test/test_keras_integration.py
@@ -92,19 +92,18 @@ class TestKerasWord2VecWrapper(unittest.TestCase):
                 label_id = len(labels_index)
                 labels_index[name] = label_id
                 for fname in sorted(os.listdir(path)):
-                    if fname.isdigit():
-                        fpath = os.path.join(path, fname)
-                        if sys.version_info < (3,):
-                            f = open(fpath)
-                        else:
-                            f = open(fpath, encoding='latin-1')
-                        t = f.read()
-                        i = t.find('\n\n')  # skip header
-                        if 0 < i:
-                            t = t[i:]
-                        texts.append(t)
-                        f.close()
-                        labels.append(label_id)
+                    fpath = os.path.join(path, fname)
+                    if sys.version_info < (3,):
+                        f = open(fpath)
+                    else:
+                        f = open(fpath, encoding='latin-1')
+                    t = f.read()
+                    i = t.find('\n\n')  # skip header
+                    if 0 < i:
+                        t = t[i:]
+                    texts.append(t)
+                    f.close()
+                    labels.append(label_id)
 
         # Vectorize the text samples into a 2D integer tensor
         tokenizer = Tokenizer()
