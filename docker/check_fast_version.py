a40b1dfbf2e930f0f580f678b238baae55ed712b
parulsethi <parul1sethi@gmail.com>
2017-05-27 23:07:16 +0530

    remove unused imports

--- docker/check_fast_version.py
+++ docker/check_fast_version.py
@@ -1,8 +1,6 @@
 import sys
 
 try:
-    from gensim.models.word2vec_inner import train_batch_sg, train_batch_cbow
-    from gensim.models.word2vec_inner import score_sentence_sg, score_sentence_cbow
     from gensim.models.word2vec_inner import FAST_VERSION, MAX_WORDS_IN_BATCH
 
     print('FAST_VERSION ok ! Retrieved with value ', FAST_VERSION)
