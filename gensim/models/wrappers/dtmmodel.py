2c84bca625ce0ea7258b340c02dc61a3abd50a82
ivan <menshikh.iv@gmail.com>
2017-05-28 00:42:48 +0500

    Fix PR1200

--- gensim/models/wrappers/dtmmodel.py
+++ gensim/models/wrappers/dtmmodel.py
@@ -22,6 +22,7 @@ Example:
 
 import logging
 import random
+import warnings
 import tempfile
 import os
 from subprocess import PIPE
@@ -308,8 +309,7 @@ class DtmModel(utils.SaveLoad):
     def print_topic(self, topicid, time, topn=10, num_words=None):
         """Return the given topic, formatted as a string."""
         if num_words is not None:  # deprecated num_words is used
-            logger.warning("The parameter num_words for print_topic(() would be deprecated in the updated version.")
-            logger.warning("Please use topn instead.")
+            warnings.warn("The parameter num_words for print_topic() would be deprecated in the updated version. Please use topn instead.")
             topn = num_words
 
         return ' + '.join(['%.3f*%s' % v for v in self.show_topic(topicid, time, topn)])
