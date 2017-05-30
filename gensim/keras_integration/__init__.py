448e350e07bb724e21aa8f0c1e4b429a4b128450
Chinmaya Pancholi <chinmayapancholi13@gmail.com>
2017-05-29 17:09:20 -0700

    removed 'keras_integration' folder

--- gensim/keras_integration/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# Copyright (C) 2011 Radim Rehurek <radimrehurek@seznam.cz>
-# Licensed under the GNU LGPL v2.1 - http://www.gnu.org/licenses/lgpl.html
-"""Wrappers to allow gensim models as input into Keras.
-Contains various gensim based implementations which can be integrated with Keras.
-"""
