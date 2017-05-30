ee3c6cadc57e381450d461cb8d837ed57493803c
ViciOs <791105792@qq.com>
2017-05-30 22:17:47 +0800

    add space after ,

--- gensim/matutils.py
+++ gensim/matutils.py
@@ -446,7 +446,7 @@ def isbow(vec):
 def kullback_leibler(vec1, vec2, num_features=None):
     """
     A distance metric between two probability distributions.
-    Returns a distance value in range <0,+∞> where values closer to 0 mean less distance (and a higher similarity)
+    Returns a distance value in range <0, +∞> where values closer to 0 mean less distance (and a higher similarity)
     Uses the scipy.stats.entropy method to identify kullback_leibler convergence value.
     If the distribution draws from a certain number of docs, that value must be passed.
     """
