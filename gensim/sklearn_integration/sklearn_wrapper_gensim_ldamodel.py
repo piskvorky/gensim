8b8118c000ecc67e55d9774eaea811519139428a
Chinmaya Pancholi <chinmayapancholi13@gmail.com>
2017-06-01 14:20:24 -0700

    added note about traning mode in 'partial_fit'

--- gensim/sklearn_integration/sklearn_wrapper_gensim_ldamodel.py
+++ gensim/sklearn_integration/sklearn_wrapper_gensim_ldamodel.py
@@ -126,6 +126,8 @@ class SklearnWrapperLdaModel(models.LdaModel, TransformerMixin, BaseEstimator):
     def partial_fit(self, X):
         """
         Train model over X.
+        By default, 'online' mode is used for training the LDA model.
+        Configure `passes` and `update_every` params at init to choose the mode among 'online', 'mini-batch', 'batch'
         """
         if sparse.issparse(X):
             X = matutils.Sparse2Corpus(X)
