9b5ffd0770a7c5846b57971b0954b5f77b8e8c4c
Chinmaya Pancholi <chinmayapancholi13@gmail.com>
2017-06-05 04:50:52 -0700

    modified condition for online(multi-pass) case slightly

--- gensim/sklearn_integration/sklearn_wrapper_gensim_ldamodel.py
+++ gensim/sklearn_integration/sklearn_wrapper_gensim_ldamodel.py
@@ -129,7 +129,7 @@ class SklearnWrapperLdaModel(models.LdaModel, TransformerMixin, BaseEstimator):
         By default, 'online (single-pass)' mode is used for training the LDA model.
         Configure `passes` and `update_every` params at init to choose the mode among :
             - online (single-pass) : update_every != None and passes == 1
-            - online (multi-pass) : update_every != None and passes != 1
+            - online (multi-pass) : update_every != None and passes > 1
             - batch : update_every == None
         """
         if sparse.issparse(X):
