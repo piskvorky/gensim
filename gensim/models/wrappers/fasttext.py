f421b053202f111963ee6c182116054ae57590c3
Prakhar Pratyush <pnation2020@gmail.com>
2017-06-20 01:13:46 +0530

    logging info modified plus changes a/c review

--- gensim/models/wrappers/fasttext.py
+++ gensim/models/wrappers/fasttext.py
@@ -219,9 +219,11 @@ class FastText(Word2Vec):
         kwargs['ignore'] = kwargs.get('ignore', ['syn0norm', 'syn0_all_norm'])
         super(FastText, self).save(*args, **kwargs)
 
+    """
     @classmethod
     def load_word2vec_format(cls, *args, **kwargs):
         return FastTextKeyedVectors.load_word2vec_format(*args, **kwargs)
+    """
 
     @classmethod
     def load_fasttext_format(cls, model_file, encoding='utf8'):
@@ -285,7 +287,7 @@ class FastText(Word2Vec):
     def load_dict(self, file_handle, encoding='utf8'):
         vocab_size, nwords, _ = self.struct_unpack(file_handle, '@3i')
         # Vocab stored by [Dictionary::save](https://github.com/facebookresearch/fastText/blob/master/src/dictionary.cc)
-        logger.info("loading projection weights")
+        logger.info("loading vocabulary words")
 
         self.struct_unpack(file_handle, '@1q')  # number of tokens
         if self.new_format:
@@ -300,12 +302,24 @@ class FastText(Word2Vec):
             word = word_bytes.decode(encoding)
             count, _ = self.struct_unpack(file_handle, '@qb')
 
-            if word != "__label__":
-                self.wv.vocab[word] = Vocab(index=i, count=count)
-                self.wv.index2word.append(word)
+            #if word != "__label__":
+
+
+            if i == nwords and i < vocab_size:
+                """
+                To handle the error in pretrained vector wiki.fr (French).
+                For more info : https://github.com/facebookresearch/fastText/issues/218
+
+                """
+                assert word == "__label__"
+                continue   # don't add word to vocab
+
+            self.wv.vocab[word] = Vocab(index=i, count=count)
+            self.wv.index2word.append(word)
 
         assert len(self.wv.vocab) == nwords, 'mismatch between vocab sizes'
         if len(self.wv.vocab) != vocab_size:
+            logger.warning("mismatch between vocab sizes")
             logger.warning("If you are loading any model other than pretrained vector wiki.fr, ")
             logger.warning("Please report to Gensim.")
 
@@ -348,9 +362,9 @@ class FastText(Word2Vec):
         all_ngrams = []
         self.wv.syn0 = np.zeros((len(self.wv.vocab), self.vector_size), dtype=REAL)
 
-        for w, v in self.wv.vocab.items():
+        for w, vocab in self.wv.vocab.items():
             all_ngrams += self.compute_ngrams(w, self.wv.min_n, self.wv.max_n)
-            self.wv.syn0[self.wv.vocab[w].index] += np.array(self.wv.syn0_all[self.wv.vocab[w].index])
+            self.wv.syn0[vocab.index] += np.array(self.wv.syn0_all[vocab.index])
 
         all_ngrams = set(all_ngrams)
         self.num_ngram_vectors = len(all_ngrams)
@@ -363,13 +377,15 @@ class FastText(Word2Vec):
 
         ngram_weights = self.wv.syn0_all
 
-        for w, v in self.wv.vocab.items():
+        logger.info("loading vocabulary weights")
+
+        for w, vocab in self.wv.vocab.items():
             word_ngrams = self.compute_ngrams(w, self.wv.min_n, self.wv.max_n)
             for word_ngram in word_ngrams:
-                self.wv.syn0[self.wv.vocab[w].index] += np.array(ngram_weights[self.wv.ngrams[word_ngram]])
+                self.wv.syn0[vocab.index] += np.array(ngram_weights[self.wv.ngrams[word_ngram]])
 
-            self.wv.syn0[self.wv.vocab[w].index] /= (len(word_ngrams) + 1)
-        logger.info("loaded %s matrix from %s" % (self.wv.syn0.shape))
+            self.wv.syn0[vocab.index] /= (len(word_ngrams) + 1)
+        logger.info("loaded %s matrix", self.wv.syn0.shape)
 
     @staticmethod
     def compute_ngrams(word, min_n, max_n):
