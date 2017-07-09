68eb54e5b6530ce6123179eb632617b22de00332
parulsethi <parul1sethi@gmail.com>
2017-07-10 04:21:25 +0530

    remove duplication

--- gensim/models/ldamodel.py
+++ gensim/models/ldamodel.py
@@ -971,30 +971,34 @@ class LdaModel(interfaces.TransformationABC, basemodel.BaseTopicModel):
 
         return values
 
-    def diff(self, other, distance="kulback_leibler", num_words=100, n_ann_terms=10, normed=True, diagonal=False, matrix=True, annotation=True):
+    def diff(self, other, distance="kullback_leibler", num_words=100, n_ann_terms=10, normed=True, diagonal=False, annotation=True):
         """
         Calculate difference topic2topic between two Lda models
         `other` instances of `LdaMulticore` or `LdaModel`
         `distance` is function that will be applied to calculate difference between any topic pair.
-        Available values: `kulback_leibler`, `hellinger` and `jaccard`
+        Available values: `kullback_leibler`, `hellinger` and `jaccard`
         `num_words` is quantity of most relevant words that used if distance == `jaccard` (also used for annotation)
         `n_ann_terms` is max quantity of words in intersection/symmetric difference between topics (used for annotation)
         Returns a matrix Z with shape (m1.num_topics, m2.num_topics), where Z[i][j] - difference between topic_i and topic_j
         and matrix annotation with shape (m1.num_topics, m2.num_topics, 2, None),
-        where
+        where:
+
             annotation[i][j] = [[`int_1`, `int_2`, ...], [`diff_1`, `diff_2`, ...]] and
             `int_k` is word from intersection of `topic_i` and `topic_j` and
             `diff_l` is word from symmetric difference of `topic_i` and `topic_j`
-        `normed` is a flag. If `true`, matrix Z will be normalized
+            `normed` is a flag. If `true`, matrix Z will be normalized
+
         Example:
+
         >>> m1, m2 = LdaMulticore.load(path_1), LdaMulticore.load(path_2)
         >>> mdiff, annotation = m1.diff(m2)
         >>> print(mdiff) # get matrix with difference for each topic pair from `m1` and `m2`
         >>> print(annotation) # get array with positive/negative words for each topic pair from `m1` and `m2`
+
         """
 
         distances = {
-            "kulback_leibler": kullback_leibler,
+            "kullback_leibler": kullback_leibler,
             "hellinger": hellinger,
             "jaccard": jaccard_distance,
         }
@@ -1016,54 +1020,38 @@ class LdaModel(interfaces.TransformationABC, basemodel.BaseTopicModel):
         if distance == "jaccard":
             d1, d2 = fst_topics, snd_topics
 
-        if matrix:
-            z = np.zeros((t1_size, t2_size))
-
-            for topic1 in range(t1_size):
-                for topic2 in range(t2_size):
-                    z[topic1][topic2] = distance_func(d1[topic1], d2[topic2])
-
-            if normed:
-                if np.abs(np.max(z)) > 1e-8:
-                    z /= np.max(z)
-
-            if annotation:
-                annotation = [[None] * t1_size for _ in range(t2_size)]
-
-                for topic1 in range(t1_size):
-                    for topic2 in range(t2_size):
-                        pos_tokens = fst_topics[topic1] & snd_topics[topic2]
-                        neg_tokens = fst_topics[topic1].symmetric_difference(snd_topics[topic2])
-
-                        pos_tokens = sample(pos_tokens, min(len(pos_tokens), n_ann_terms))
-                        neg_tokens = sample(neg_tokens, min(len(neg_tokens), n_ann_terms))
-
-                        annotation[topic1][topic2] = [pos_tokens, neg_tokens]
-
         if diagonal:
-            assert t1_size == t2_size, 'mismatch between number of topics in both model'
-            z = np.zeros(t1_size)
+            t_size = min(t1_size, t2_size)
+            z = np.zeros(t_size)
+            if annotation:
+                diff_terms = np.zeros(t_size, dtype=list)
+        else:
+            z = np.zeros((t1_size, t2_size))
+            if annotation:
+                diff_terms = np.zeros((t1_size, t2_size), dtype=list)
 
-            for topic in range(t1_size):
-                z[topic] = distance_func(d1[topic], d2[topic])
+        for topic in np.ndindex(z.shape):
+            topic1 = topic[0]
+            if diagonal:
+                topic2 = topic1
+            else:
+                topic2 = topic[1]
 
-            if normed:
+            z[topic] = distance_func(d1[topic1], d2[topic2])
+            if annotation:
+                pos_tokens = fst_topics[topic1] & snd_topics[topic2]
+                neg_tokens = fst_topics[topic1].symmetric_difference(snd_topics[topic2])
+
+                pos_tokens = sample(pos_tokens, min(len(pos_tokens), n_ann_terms))
+                neg_tokens = sample(neg_tokens, min(len(neg_tokens), n_ann_terms))
+
+                diff_terms[topic] = [pos_tokens, neg_tokens]
+                
+        if normed:
                 if np.abs(np.max(z)) > 1e-8:
                     z /= np.max(z)
 
-            if annotation:
-                annotation = [None] * t1_size
-
-                for topic in range(t1_size):
-                        pos_tokens = fst_topics[topic] & snd_topics[topic]
-                        neg_tokens = fst_topics[topic].symmetric_difference(snd_topics[topic])
-
-                        pos_tokens = sample(pos_tokens, min(len(pos_tokens), n_ann_terms))
-                        neg_tokens = sample(neg_tokens, min(len(neg_tokens), n_ann_terms))
-
-                        annotation[topic] = [pos_tokens, neg_tokens]
-
-        return z, annotation
+        return z, diff_terms
 
     def __getitem__(self, bow, eps=None):
         """
