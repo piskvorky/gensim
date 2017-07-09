dd871c3b22e6751a636b8469362a25e78cd85273
Sweeney, Mack <mackenzie.sweeney@capitalone.com>
2017-07-09 16:30:03 -0400

    Move `BrownCorpus` to `textcorpus` module and refactor to subclass from `TextDirectoryCorpus`.

--- gensim/corpora/textcorpus.py
+++ gensim/corpora/textcorpus.py
@@ -671,3 +671,25 @@ class Text8Corpus(TextTokensIterator, TextCorpus):
     def getstream(self):
         for sentence_tokens in self._sentence_token_stream():
             yield ' '.join(sentence_tokens)
+
+
+def brown_corpus_tokenizer(text):
+    text = utils.to_unicode(text)
+    # each file line is a single sentence in the Brown corpus
+    # each token is WORD/POS_TAG
+    token_tags = [t.split('/') for t in text.split() if len(t.split('/')) == 2]
+    # ignore words with non-alphabetic tags like ",", "!" etc (punctuation, weird stuff)
+    return [
+        "%s/%s" % (token.lower(), tag[:2])
+        for token, tag in token_tags
+        if tag[:2].isalpha()
+    ]
+
+
+class BrownCorpus(TextTokensIterator, TextDirectoryCorpus):
+    """Iterate over sentences from the Brown corpus (part of NLTK data)."""
+
+    def __init__(self, source, dictionary=None, metadata=False, processes=-1):
+        super(self.__class__, self).__init__(
+            source, dictionary, metadata, max_depth=0, tokenizer=brown_corpus_tokenizer,
+            character_filters=[], token_filters=[], processes=processes)
--- gensim/models/word2vec.py
+++ gensim/models/word2vec.py
@@ -1452,28 +1452,6 @@ class Word2Vec(utils.SaveLoad):
         return self.running_training_loss
 
 
-class BrownCorpus(object):
-    """Iterate over sentences from the Brown corpus (part of NLTK data)."""
-    def __init__(self, dirname):
-        self.dirname = dirname
-
-    def __iter__(self):
-        for fname in os.listdir(self.dirname):
-            fname = os.path.join(self.dirname, fname)
-            if not os.path.isfile(fname):
-                continue
-            for line in utils.smart_open(fname):
-                line = utils.to_unicode(line)
-                # each file line is a single sentence in the Brown corpus
-                # each token is WORD/POS_TAG
-                token_tags = [t.split('/') for t in line.split() if len(t.split('/')) == 2]
-                # ignore words with non-alphabetic tags like ",", "!" etc (punctuation, weird stuff)
-                words = ["%s/%s" % (token.lower(), tag[:2]) for token, tag in token_tags if tag[:2].isalpha()]
-                if not words:  # don't bother sending out empty sentences
-                    continue
-                yield words
-
-
 # Example: ./word2vec.py -train data.txt -output vec.txt -size 200 -window 5 -sample 1e-4 -negative 5 -hs 0 -binary 0 -cbow 1 -iter 3
 if __name__ == "__main__":
     import argparse
