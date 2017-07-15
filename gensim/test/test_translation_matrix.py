3c9b5ac8ed87e8cf989c82b1289817758328750b
robotcator <problemset@163.com>
2017-07-15 15:47:45 +0800

    fix travis CI error

--- gensim/test/test_translation_matrix.py
+++ gensim/test/test_translation_matrix.py
@@ -26,22 +26,6 @@ class TestTranslationMatrix(unittest.TestCase):
 
         translation_matrix.TranslationMatrix(word_pair, source_word_vec, target_word_vec)
 
-    def test_translate(self):
-        train_file = datapath("OPUS_en_it_europarl_train_5K.txt")
-        with utils.smart_open(train_file, "r") as f:
-            word_pair = [tuple(utils.to_unicode(line).strip().split()) for line in f]
-
-        source_word_vec_file = datapath("EN.200K.cbow1_wind5_hs0_neg10_size300_smpl1e-05.txt")
-        source_word_vec = KeyedVectors.load_word2vec_format(source_word_vec_file, binary=False)
-
-        target_word_vec_file = datapath("IT.200K.cbow1_wind5_hs0_neg10_size300_smpl1e-05.txt")
-        target_word_vec = KeyedVectors.load_word2vec_format(target_word_vec_file, binary=False)
-
-        transmat = translation_matrix.TranslationMatrix(word_pair, source_word_vec, target_word_vec)
-
-        test_word_pair = [("for", "per"), ("that", "che"), ("with", "con")]
-        test_source_word, test_target_word = zip(*test_word_pair)
-        transmat.translate(test_source_word, 3)
 
     def testPersistence(self):
         """Test storing/loading the entire model."""
@@ -78,7 +62,7 @@ class TestTranslationMatrix(unittest.TestCase):
         test_word_pair = [("one", "uno"), ("two", "due"), ("three", "tre"), ("four", "quattro"),
                           ("for", "per"), ("that", "che"), ("with", "con")]
         test_source_word, test_target_word = zip(*test_word_pair)
-        translated_word = transmat.translate(test_source_word, 3)
+        transmat.translate(test_source_word, topn=3)
 
     def test_translate_GC(self):
         train_file = datapath("OPUS_en_it_europarl_train_5K.txt")
@@ -96,4 +80,4 @@ class TestTranslationMatrix(unittest.TestCase):
         test_word_pair = [("one", "uno"), ("two", "due"), ("three", "tre"), ("four", "quattro"),
                           ("for", "per"), ("that", "che"), ("with", "con")]
         test_source_word, test_target_word = zip(*test_word_pair)
-        translated_word = transmat.translate(test_source_word, 3, additional=10)
+        transmat.translate(test_source_word, topn=3, additional=10)
