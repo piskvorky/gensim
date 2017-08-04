e84b22071d74de9ecd793c81c830a84d7d8ef460
ivan <menshikh.iv@gmail.com>
2017-08-04 18:37:24 +0500

    Add basic version checking

--- continuous_integration/travis/run.sh
+++ continuous_integration/travis/run.sh
@@ -3,6 +3,7 @@
 set -e
 
 pip freeze
+python -c "import platform, sys, numpy, scipy, gensim; from gensim.models import word2vec; print('Platform: {}\nPython: {}\nNumpy: {}\nSciPy: {}\ngensim: {}\nFAST_VERSION: {}'.format(platform.platform(), sys.version, numpy.__version__, scipy.__version__, gensim.__version__, word2vec.FAST_VERSION))"
 
 if [[ "$ONLY_CODESTYLE" == "yes" ]]; then
 	continuous_integration/travis/flake8_diff.sh
