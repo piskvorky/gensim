f567dee022be5aff13d8ca595f151e1cc67da464
chaitalisaini <chaitalisaini@gmail.com>
2017-08-04 15:18:12 +0530

    Updated download function

--- gensim/__main__.py
+++ gensim/__main__.py
@@ -4,7 +4,6 @@ import sys
 import argparse
 from .api import download
 from .api import catalogue
-from .api import link
 from .api import info
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Gensim console API")
@@ -13,10 +12,6 @@ if __name__ == '__main__':
         "-d", "--download", nargs=1,
         help="To download a corpus/model : python -m gensim -d "
         "corpus/model name")
-    group.add_argument(
-        "-l", "--link", nargs=2,
-        help="To store a shortcut to a corpus/model : python -m gensim -l "
-        "source destination")
     group.add_argument(
         "-i", "--info", nargs=1,
         help="To get information about a corpus/model : python -m gensim -i "
@@ -27,12 +22,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     if sys.argv[1] == "-d" or sys.argv[1] == "--download":
         download(sys.argv[2])
-
-    elif sys.argv[1] == "-l" or sys.argv[1] == "--link":
-        link(sys.argv[2], sys.argv[3])
-
     elif sys.argv[1] == "-i" or sys.argv[1] == "--info":
         info(sys.argv[2])
-
     elif sys.argv[1] == "-c" or sys.argv[1] == "--catalogue":
         catalogue()
--- gensim/api/__init__.py
+++ gensim/api/__init__.py
@@ -1,11 +1,14 @@
-import subprocess
+from __future__ import print_function
 import json
 import sys
 import os
-import pip
-import importlib
-from pathlib import Path
-import os
+import six
+import tarfile
+try:
+    import urllib.request as urllib
+except ImportError:
+    import urllib
+
 try:
     from urllib.request import urlopen
 except ImportError:
@@ -13,22 +16,26 @@ except ImportError:
 
 
 def download(file):
-    url = "https://github.com/chaitaliSaini/Corpus_and_models/releases/download/"
-    data = catalogue(False)
-    corpuses = data['gensim']['corpus']
-    models = data['gensim']['model']
-    if file not in corpuses and file not in models:
-        print("Incorrect corpus/model name.")
-    else:
-        url = url+file+"/"+file+".tar.gz"
-        print("Downloading {m}".format(m=file))
-        subprocess.call(
-            [sys.executable, '-m', 'pip', 'install', '--no-cache-dir', url],
-            env=os.environ.copy())
+    url = "https://github.com/chaitaliSaini/Corpus_and_models/releases/"
+    url = url+"download/"+file+"/"+file+".tar.gz"
+    user_dir = os.path.expanduser('~')
+    base_dir = os.path.join(user_dir, 'gensim-data')
+    extracted_folder_dir = os.path.join(base_dir, file)
+    if not os.path.exists(base_dir):
+        os.makedirs(base_dir)
+    compressed_folder_name = file+".tar.gz"
+    compressed_folder_dir = os.path.join(base_dir, compressed_folder_name)
+    urllib.urlretrieve(url, compressed_folder_dir)
+    if not os.path.exists(extracted_folder_dir):
+        os.makedirs(extracted_folder_dir)
+    tar = tarfile.open(compressed_folder_dir)
+    tar.extractall(extracted_folder_dir)
+    tar.close()
 
 
 def catalogue(print_list=True):
-    url = "https://raw.githubusercontent.com/chaitaliSaini/Corpus_and_models/master/list.json"
+    url = "https://raw.githubusercontent.com/chaitaliSaini/Corpus_and_models/"
+    url = url + "master/list.json"
     response = urlopen(url)
     data = response.read().decode("utf-8")
     if print_list == 1:
@@ -44,35 +51,6 @@ def catalogue(print_list=True):
             print(model)
     else:
         return json.loads(data)
-
-
-def link(file_name, shortcut_name):
-    packages = pip.get_installed_distributions()
-    package_is_installed = 0
-    for package in packages:
-        if package.project_name == file_name:
-            package_is_installed = 1
-            break
-    if package_is_installed == 0:
-        print("The model/corpus {f} has not been installed".format(f=file_name))
-        print("For installing use: python -m gensim -d {f}".format(f=file_name))
-        sys.exit(0)
-    package = importlib.import_module(file_name)
-    package_path = Path(package.__file__).parent.parent
-    package_path = package_path / file_name / file_name
-    gensim_path = importlib.import_module("gensim")
-    gensim_path = Path(gensim_path.__file__).parent
-    shortcut_path = gensim_path / 'data' / shortcut_name
-    if os.path.exists(str(shortcut_path)):
-        print("This shortcut link already exists.")
-        sys.exit(0)
-    try:
-        os.symlink(str(package_path), str(shortcut_path))
-    except:
-        print("Shortcut creation failed in gensim/data.")
-        sys.exit(0)
-    print("Shortcut creation successful. The model/corpus can now be found"
-          " in gensim/data.")
 
 
 def info(file_name):
