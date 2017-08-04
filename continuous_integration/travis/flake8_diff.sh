6fa4cddde7752af1cc81fbacd42ac132c3f48524
ivan <menshikh.iv@gmail.com>
2017-08-04 20:34:59 +0500

    fix flake8 problems with ignore list

--- continuous_integration/travis/flake8_diff.sh
+++ continuous_integration/travis/flake8_diff.sh
@@ -118,7 +118,7 @@ echo '--------------------------------------------------------------------------
 # Excluding vec files since they contain non-utf8 content and flake8 raises exception for non-utf8 input
 # We need the following command to exit with 0 hence the echo in case
 # there is no match
-MODIFIED_FILES="$(git diff --name-only $COMMIT_RANGE -- . ':(exclude)*.vec' || echo "no_match")"
+MODIFIED_FILES="$(git diff --name-only $COMMIT_RANGE -- . | grep "*.py" || echo "no_match")"
 
 check_files() {
     files="$1"
@@ -134,6 +134,6 @@ check_files() {
 if [[ "$MODIFIED_FILES" == "no_match" ]]; then
     echo "No file has been modified"
 else
-    check_files "$(echo "$MODIFIED_FILES" )" "--ignore=E501,E731,E12,W503 --exclude=*.sh,*.md,*.yml,*.rst,*.ipynb,*.txt,*.csv,*.vec,Dockerfile*,*.c,*.pyx,*.inc"
+    check_files "$(echo "$MODIFIED_FILES" )" "--ignore=E501,E731,E12,W503"
 fi
 echo -e "No problem detected by flake8\n"
