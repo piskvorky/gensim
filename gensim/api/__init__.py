d8257a387237208fa5fe5add7380614e079e78f0
chaitalisaini <chaitalisaini@gmail.com>
2017-08-11 11:19:19 +0530

    Added load function

--- gensim/api/__init__.py
+++ gensim/api/__init__.py
@@ -1,11 +1,12 @@
 from __future__ import print_function
-
-import logging
+from __future__ import absolute_import
 import json
 import os
 import tarfile
 import shutil
-from ..utils import SaveLoad
+import logging
+import sys
+import importlib
 try:
     import urllib.request as urllib
 except ImportError:
@@ -16,10 +17,7 @@ try:
 except ImportError:
     from urllib2 import urlopen
 
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', filename="api.log", level=logging.INFO)
-console = logging.StreamHandler()
-console.setLevel(logging.INFO)
-logging.getLogger('').addHandler(console)
+logger = logging.getLogger('gensim.api')
 
 
 def download(file_name):
@@ -29,37 +27,42 @@ def download(file_name):
     base_dir = os.path.join(user_dir, 'gensim-data')
     extracted_folder_dir = os.path.join(base_dir, file_name)
     if not os.path.exists(base_dir):
-        logging.info("Creating {}".format(base_dir))
+        logger.info("Creating {}".format(base_dir))
         os.makedirs(base_dir)
         if os.path.exists(base_dir):
-            logging.info("Creation successful. Models/corpus can be accessed"
-                         "via {}".format(base_dir))
+            logger.info(
+                "Creation successful. Models/corpus can be accessed"
+                "via {}".format(base_dir))
         else:
-            logging.error("Not able to create {d}. Make sure you have the "
-                          "correct read/write permissions of the {d} or you "
-                          "can try creating it manually".format(d=base_dir))
+            logger.error(
+                "Not able to create {d}. Make sure you have the "
+                "correct read/write permissions of the {d} or you "
+                "can try creating it manually".format(d=base_dir))
     compressed_folder_name = file_name + ".tar.gz"
     compressed_folder_dir = os.path.join(base_dir, compressed_folder_name)
     if not os.path.exists(extracted_folder_dir):
-        logging.info("Downloading {}".format(file_name))
+        logger.info("Downloading {}".format(file_name))
         urllib.urlretrieve(url, compressed_folder_dir)
-        logging.info("{} downloaded".format(file_name))
-        logging.info("Creating {}".format(extracted_folder_dir))
+        logger.info("{} downloaded".format(file_name))
+        logger.info("Creating {}".format(extracted_folder_dir))
         os.makedirs(extracted_folder_dir)
         if os.path.exists(extracted_folder_dir):
-            logging.info("Creation of {} successful"
-                         ".".format(extracted_folder_dir))
+            logger.info(
+                "Creation of {} successful"
+                ".".format(extracted_folder_dir))
             tar = tarfile.open(compressed_folder_dir)
-            logging.info("Extracting files from"
-                         "{}".format(extracted_folder_dir))
+            logger.info(
+                "Extracting files from"
+                "{}".format(extracted_folder_dir))
             tar.extractall(extracted_folder_dir)
             tar.close()
-            logging.info("{} installed".format(file_name))
+            logger.info("{} installed".format(file_name))
         else:
-            logging.error("Not able to create {d}. Make sure you have the "
-                          "correct read/write permissions of the {d} or you "
-                          "can try creating it "
-                          "manually".format(d=extracted_folder_dir))
+            logger.error(
+                "Not able to create {d}. Make sure you have the "
+                "correct read/write permissions of the {d} or you "
+                "can try creating it "
+                "manually".format(d=extracted_folder_dir))
     else:
         print("{} has already been installed".format(file_name))
 
@@ -69,7 +72,7 @@ def catalogue(print_list=True):
     url = url + "master/list.json"
     response = urlopen(url)
     data = response.read().decode("utf-8")
-    if print_list == 1:
+    if print_list:
         data = json.loads(data)
         corpuses = data['gensim']['corpus']
         models = data['gensim']['model']
@@ -94,3 +97,22 @@ def info(file_name):
         print(data['gensim']['model'][file_name])
     else:
         print("Incorrect model/corpus name.")
+
+
+def load(file_name, return_path=False):
+    user_dir = os.path.expanduser('~')
+    base_dir = os.path.join(user_dir, 'gensim-data')
+    folder_dir = os.path.join(base_dir, file_name)
+    if not os.path.exists(folder_dir):
+        print(
+            "Incorrect model/corpus name. Use catalogue() to get a list of "
+            "avalible models/corpus. If the model/corpus name you entered is"
+            " in the catalogue, then please download the model/corpus by "
+            "calling download({f}) function".format(f=file_name))
+    elif return_path:
+        return folder_dir
+    else:
+        sys.path.insert(0, base_dir)
+        module = __import__(file_name)
+        data = module.load_data()
+        return data
