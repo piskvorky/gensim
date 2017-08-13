102c14a516ccad5eb1c686c218c09c456a6a965c
Chinmaya Pancholi <chinmayapancholi13@gmail.com>
2017-08-14 00:00:30 +0530

    updated unit tests for fasttext

--- gensim/test/test_fasttext.py
+++ gensim/test/test_fasttext.py
@@ -8,6 +8,7 @@ import os
 import numpy as np
 
 from gensim import utils
+from gensim.models.word2vec import LineSentence
 from gensim.models.fasttext import FastText as FT_gensim
 from gensim.models.wrappers.fasttext import FastText as FT_wrapper
 
@@ -69,39 +70,45 @@ class TestFastTextModel(unittest.TestCase):
         self.models_equal(model, model2)
 
     def test_against_fasttext_wrapper(self, model_gensim, model_wrapper):
-        sims_gensim = model_gensim.most_similar('war', topn=50)
-        sims_wrapper = model_wrapper.most_similar('war', topn=50)
+        sims_gensim = model_gensim.most_similar('night', topn=10)
+        sims_gensim_words = (list(map(lambda x:x[0], sims_gensim)))
+
+        sims_wrapper = model_wrapper.most_similar('night', topn=10)
+        sims_wrapper_words = (list(map(lambda x:x[0], sims_wrapper)))
+
         self.assertEqual(sims_gensim, sims_wrapper)
 
     def test_cbow_hs(self):
         model_wrapper = FT_wrapper.train(ft_path='/home/chinmaya/GSOC/Gensim/fastText/fasttext', 
             corpus_file=datapath('lee_background.cor'), output_file='/home/chinmaya/GSOC/Gensim/fasttext_out1', model='cbow', size=50,
-            alpha=0.05, window=8, min_count=5, word_ngrams=1, loss='hs', sample=1e-3, negative=0, iter=5, min_n=3, max_n=6, sorted_vocab=1, threads=12)
+            alpha=0.05, window=2, min_count=5, word_ngrams=1, loss='hs', sample=1e-3, negative=0, iter=3, min_n=3, max_n=6, sorted_vocab=1, threads=1)
 
-        model_gensim = FT_gensim(size=50, sg=0, cbow_mean=1, alpha=0.05, window=8, hs=1, negative=0,
-            min_count=5, iter=5, batch_words=1000, word_ngrams=1, sample=1e-3, min_n=3, max_n=6,
-            sorted_vocab=1, workers=12, min_alpha=0.0001)
+        model_gensim = FT_gensim(size=50, sg=0, cbow_mean=1, alpha=0.05, window=2, hs=1, negative=0,
+            min_count=5, iter=3, batch_words=1000, word_ngrams=1, sample=1e-3, min_n=3, max_n=6,
+            sorted_vocab=1, workers=12, min_alpha=0.0)
             
-        model_gensim.build_vocab(list_corpus)
+        lee_data = LineSentence(datapath('lee_background.cor'))
+        model_gensim.build_vocab(lee_data)
         orig0 = np.copy(model_gensim.wv.syn0[0])
-        model_gensim.train(list_corpus, total_examples=model_gensim.corpus_count, epochs=model_gensim.iter)
-        self.assertFalse((orig0 == model_gensim.wv.syn0[1]).all())  # vector should vary after training
+        model_gensim.train(lee_data, total_examples=model_gensim.corpus_count, epochs=model_gensim.iter)
+        self.assertFalse((orig0 == model_gensim.wv.syn0[0]).all())  # vector should vary after training
         
         self.test_against_fasttext_wrapper(model_gensim, model_wrapper)
 
     def test_cbow_neg(self):
         model_wrapper = FT_wrapper.train(ft_path='/home/chinmaya/GSOC/Gensim/fastText/fasttext', 
             corpus_file=datapath('lee_background.cor'), output_file='/home/chinmaya/GSOC/Gensim/fasttext_out1', model='cbow', size=50,
-            alpha=0.05, window=8, min_count=5, word_ngrams=1, loss='ns', sample=1e-3, negative=15, iter=5, min_n=3, max_n=6, sorted_vocab=1, threads=12)
+            alpha=0.05, window=2, min_count=5, word_ngrams=1, loss='ns', sample=1e-3, negative=15, iter=7, min_n=3, max_n=6, sorted_vocab=1, threads=1)
 
-        model_gensim = FT_gensim(size=50, sg=0, cbow_mean=1, alpha=0.05, window=8, hs=0, negative=15,
-            min_count=5, iter=5, batch_words=1000, word_ngrams=1, sample=1e-3, min_n=3, max_n=6,
-            sorted_vocab=1, workers=12, min_alpha=0.0001)
+        model_gensim = FT_gensim(size=50, sg=0, cbow_mean=1, alpha=0.05, window=2, hs=0, negative=15,
+            min_count=1, iter=7, batch_words=1000, word_ngrams=1, sample=1e-3, min_n=3, max_n=6,
+            sorted_vocab=1, workers=1, min_alpha=0.0)
             
-        model_gensim.build_vocab(list_corpus)
+        lee_data = LineSentence(datapath('lee_background.cor'))
+        model_gensim.build_vocab(lee_data)
         orig0 = np.copy(model_gensim.wv.syn0[0])
-        model_gensim.train(list_corpus, total_examples=model_gensim.corpus_count, epochs=model_gensim.iter)
-        self.assertFalse((orig0 == model_gensim.wv.syn0[1]).all())  # vector should vary after training
+        model_gensim.train(lee_data, total_examples=model_gensim.corpus_count, epochs=model_gensim.iter)
+        self.assertFalse((orig0 == model_gensim.wv.syn0[0]).all())  # vector should vary after training
         
         self.test_against_fasttext_wrapper(model_gensim, model_wrapper)
 
