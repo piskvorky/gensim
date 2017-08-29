b678518347489bd7110fe29f03a7abc747b4cabe
Sweeney, Mack <mackenzie.sweeney@capitalone.com>
2017-08-28 21:51:38 -0400

    fix corpora.__init__ import order

--- gensim/corpora/__init__.py
+++ gensim/corpora/__init__.py
@@ -2,11 +2,11 @@
 This package contains implementations of various streaming corpus I/O format.
 """
 
+from .indexedcorpus import IndexedCorpus  # must appear before the other classes
 # bring corpus classes directly into package namespace, to save some typing
 from .bleicorpus import BleiCorpus
 from .dictionary import Dictionary
 from .hashdictionary import HashDictionary
-from .indexedcorpus import IndexedCorpus  # must appear before the other classes
 from .lowcorpus import LowCorpus
 from .malletcorpus import MalletCorpus
 from .mmcorpus import MmCorpus
