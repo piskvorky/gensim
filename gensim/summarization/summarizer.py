3b4982e7339d15c9bfc91eeae354cfe47489c398
Federico Barrios <fbarrios@fi.uba.ar>
2017-09-05 16:36:18 -0300

    Fix comment.

--- gensim/summarization/summarizer.py
+++ gensim/summarization/summarizer.py
@@ -157,7 +157,8 @@ def summarize_corpus(corpus, ratio=0.2):
     _set_graph_edge_weights(graph)
     _remove_unreachable_nodes(graph)
 
-    # Cannot calculate eigenvectors if number of unique words in text < 3. Warns user to add more text. The function ends.
+    # Cannot calculate eigenvectors if number of unique documents in corpus < 3.
+    # Warns user to add more text. The function ends.
     if len(graph.nodes()) < 3:
         logger.warning("Please add more sentences to the text. The number of reachable nodes is below 3")
         return
