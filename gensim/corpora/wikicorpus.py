67cdc1c800fbe66d842e1d7ab1f7a4f54b551a30
Menshikh Ivan <menshikh.iv@gmail.com>
2017-09-14 14:17:46 +0500

    Add file patterns to documentation

--- gensim/corpora/wikicorpus.py
+++ gensim/corpora/wikicorpus.py
@@ -257,7 +257,7 @@ def init_to_ignore_interrupt():
 
 class WikiCorpus(TextCorpus):
     """
-    Treat a wikipedia articles dump (\*articles.xml.bz2) as a (read-only) corpus.
+    Treat a wikipedia articles dump (<LANG>wiki-<YYYYMMDD>-pages-articles.xml.bz2 or <LANG>wiki-latest-pages-articles.xml.bz2) as a (read-only) corpus.
 
     The documents are extracted on-the-fly, so that the whole (massive) dump
     can stay compressed on disk.
