e667069e90136c7d7c814e5d75ec8832e45805d1
Kimmo Kärkkäinen <kimmo.karkkainen@gmail.com>
2017-09-14 14:01:37 +0300

    Fix incorrect initialization ShardedCorpus with a generator. Fix #1511 (#1512)
    Fix incorrect initialization ShardedCorpus with a generator. Fix #1511.

--- gensim/test/test_sharded_corpus.py
+++ gensim/test/test_sharded_corpus.py
@@ -256,6 +256,18 @@ class TestShardedCorpus(unittest.TestCase):
             fname = dataset._shard_name(n)
             self.assertTrue(os.path.isfile(fname))
 
+    def test_init_with_generator(self):
+
+        def data_generator():
+            yield [(0, 1)]
+            yield [(1, 1)]
+
+        gen_tmp_fname = self.tmp_fname + '.generator'
+        corpus = ShardedCorpus(gen_tmp_fname, data_generator(), dim=2)
+
+        self.assertEqual(2, len(corpus))
+        self.assertEqual(1, corpus[0][0])
+
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
