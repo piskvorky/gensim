c1554cdd3f5d9aa91393b221631d11e7dffa8fc3
Sourav Singh <souravsingh@users.noreply.github.com>
2017-10-17 18:37:04 +0530

    Fix PEP8 errors in model

--- gensim/models/sldamodel.py
+++ gensim/models/sldamodel.py
@@ -14,21 +14,22 @@ from scipy.misc import logsumexp
 
 from gensim import interfaces, utils, matutils
 
+
 def sampling_from_dist(prob):
-    """ 
+    """
     Sample index from a list of unnormalised probability distribution
     same as np.random.multinomial(1, prob/np.sum(prob)).argmax()
-    
+
     Parameters
     ----------
     prob: ndarray
         array of unnormalised probability distribution
-    
+
     Returns
     -------
     new_topic: return a sampled index
     """
-    
+
     thr = prob.sum() * np.random.rand()
     new_topic = 0
     tmp = prob[new_topic]
@@ -36,6 +37,7 @@ def sampling_from_dist(prob):
         new_topic += 1
         tmp += prob[new_topic]
     return new_topic
+
 
 def get_top_words(topic_word_matrix, vocab, topic, n_words=20):
     if not isinstance(vocab, np.ndarray):
@@ -73,11 +75,10 @@ class SLdaModel(interfaces.TransformationABC, basemodel.BaseTopicModel):
                 Seed for random number generator
         """
         super(SLdaModel, self).__init__(n_doc=n_doc, n_voca=n_voca, n_topic=n_topic, alpha=alpha, beta=beta,
-                                                 **kwargs)
+                                        **kwargs)
         self.eta = np.random.normal(scale=5, size=self.n_topic)
         self.sigma = sigma
-        
-    
+
     def random_init(self, docs):
         """
         Random initialization of topics
@@ -93,7 +94,7 @@ class SLdaModel(interfaces.TransformationABC, basemodel.BaseTopicModel):
                 self.TW[topic, word] += 1
                 self.sum_T[topic] += 1
                 self.DT[di, topic] += 1
-                
+
     def log_likelihood(self, docs, responses):
         """
         Calculate log-likelihood function
@@ -114,13 +115,14 @@ class SLdaModel(interfaces.TransformationABC, basemodel.BaseTopicModel):
             l1 += gammaln(self.TW[ki, :]).sum() - gammaln(self.TW[ki, :].sum())
 
         return l1
-    
+
     def sample_heldout_doc(self, max_iter, heldout_docs):
         """
-        Calculate Topic sum on heldout docs. 
+        Calculate Topic sum on heldout docs.
         """
         h_doc_topics = list()
-        h_doc_topic_sum = np.zeros([len(heldout_docs), self.n_topic]) + self.alpha
+        h_doc_topic_sum = np.zeros(
+            [len(heldout_docs), self.n_topic]) + self.alpha
 
         # random init
         for di in xrange(len(heldout_docs)):
@@ -150,10 +152,9 @@ class SLdaModel(interfaces.TransformationABC, basemodel.BaseTopicModel):
                     h_doc_topic_sum[di, new_topic] += 1
 
         return h_doc_topic_sum
-    
-    
+
     def fit(self, docs, responses, max_iter=100):
-        """ 
+        """
         Fit sLDA model using Stochastic Expectation Maximisation.
         """
         self.random_init(docs)
@@ -168,9 +169,10 @@ class SLdaModel(interfaces.TransformationABC, basemodel.BaseTopicModel):
                     self.TW[old_topic, word] -= 1
                     self.sum_T[old_topic] -= 1
                     self.DT[di, old_topic] -= 1
-                    
-                    # Calculate z-bar 
-                    z_bar = np.zeros([self.n_topic, self.n_topic]) + self.DT[di, :] + np.identity(self.n_topic)
+
+                    # Calculate z-bar
+                    z_bar = np.zeros([self.n_topic, self.n_topic]) + \
+                        self.DT[di, :] + np.identity(self.n_topic)
                     z_bar /= self.DT[di, :].sum() + 1
 
                     # update
@@ -186,7 +188,10 @@ class SLdaModel(interfaces.TransformationABC, basemodel.BaseTopicModel):
 
             # estimate parameters
             z_bar = self.DT / self.DT.sum(1)[:, np.newaxis]  # DxK
-            self.eta = solve(np.dot(z_bar.T, z_bar), np.dot(z_bar.T, responses))
+            self.eta = solve(
+                np.dot(
+                    z_bar.T, z_bar), np.dot(
+                    z_bar.T, responses))
 
             # compute mean absolute error
             mae = np.mean(np.abs(responses - np.dot(z_bar, self.eta)))
