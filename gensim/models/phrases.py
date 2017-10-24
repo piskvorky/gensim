e866d3f0646ac354636ec4b6a947c0b4a34d02a3
ivan <menshikh.iv@gmail.com>
2017-10-24 15:56:43 +0500

    fix docstring

--- gensim/models/phrases.py
+++ gensim/models/phrases.py
@@ -140,30 +140,33 @@ class Phrases(interfaces.TransformationABC):
         setting. `scoring` can be set with either a string that refers to a built-in scoring function,
         or with a function with the expected parameter names. Two built-in scoring functions are available
         by setting `scoring` to a string:
-            'default': from "Efficient Estimaton of Word Representations in Vector Space" by
-                Mikolov, et. al.:
-                (count(worda followed by wordb) - min_count) * N /
-                (count(worda) * count(wordb)) > threshold`, where `N` is the total vocabulary size.
-            'npmi': normalized pointwise mutual information, from "Normalized (Pointwise) Mutual
+
+        'default': from "Efficient Estimaton of Word Representations in Vector Space" by
+                   Mikolov, et. al.:
+                   (count(worda followed by wordb) - min_count) * N /
+                   (count(worda) * count(wordb)) > threshold`, where `N` is the total vocabulary size.
+        'npmi': normalized pointwise mutual information, from "Normalized (Pointwise) Mutual
                 Information in Colocation Extraction" by Gerlof Bouma:
                 ln(prop(worda followed by wordb) / (prop(worda)*prop(wordb))) /
                 - ln(prop(worda followed by wordb)
                 where prop(n) is the count of n / the count of everything in the entire corpus
 
-            'npmi' is more robust when dealing with common words that form part of common bigrams, and
-            ranges from -1 to 1, but is slower to calculate than the default
+        'npmi' is more robust when dealing with common words that form part of common bigrams, and
+        ranges from -1 to 1, but is slower to calculate than the default
+
         To use a custom scoring function, create a function with the following parameters and set the `scoring`
         parameter to the custom function. You must use all the parameters in your function call, even if the
         function does not require all the parameters.
+
             worda_count: number of occurrances in `sentences` of the first token in the phrase being scored
             wordb_count: number of occurrances in `sentences` of the second token in the phrase being scored
             bigram_count: number of occurrances in `sentences` of the phrase being scored
             len_vocab: the number of unique tokens in `sentences`
             min_count: the `min_count` setting of the Phrases class
             corpus_word_count: the total number of (non-unique) tokens in `sentences`
+
         A scoring function without any of these parameters (even if the parameters are not used) will
-            raise a ValueError on initialization of the Phrases class
-        The scoring function must be picklable
+        raise a ValueError on initialization of the Phrases class. The scoring function must be picklable.
 
         """
         if min_count <= 0:
