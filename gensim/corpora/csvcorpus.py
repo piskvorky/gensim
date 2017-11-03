ae698671691ca5eeea537af29da738c72eb3b623
Timofey Yefimov <anotherbugmaster@gmail.com>
2017-11-03 19:18:56 +0500

    Annotate csvcorpus

--- gensim/corpora/csvcorpus.py
+++ gensim/corpora/csvcorpus.py
@@ -4,10 +4,7 @@
 # Copyright (C) 2013 Zygmunt Zając <zygmunt@fastml.com>
 # Licensed under the GNU LGPL v2.1 - http://www.gnu.org/licenses/lgpl.html
 
-"""
-Corpus in CSV format.
-
-"""
+"""Corpus in CSV format."""
 
 
 from __future__ import with_statement
@@ -22,18 +19,24 @@ logger = logging.getLogger('gensim.corpora.csvcorpus')
 
 
 class CsvCorpus(interfaces.CorpusABC):
-    """
-    Corpus in CSV format. The CSV delimiter, headers etc. are guessed automatically
-    based on the file content.
-
+    """Corpus in CSV format.
+    
+    The CSV delimiter, headers etc. are guessed automatically based on the
+    file content.
+    
     All row values are expected to be ints/floats.
 
     """
 
     def __init__(self, fname, labels):
-        """
-        Initialize the corpus from a file.
-        `labels` = are class labels present in the input file? => skip the first column
+        """Initialize the corpus from a file.
+
+        Parameters
+        ----------
+        fname : str
+            Filename
+        labels : bool
+            Whether to skip the first column
 
         """
         logger.info("loading corpus from %s", fname)
@@ -48,10 +51,7 @@ class CsvCorpus(interfaces.CorpusABC):
         logger.info("sniffed CSV delimiter=%r, headers=%s", self.dialect.delimiter, self.headers)
 
     def __iter__(self):
-        """
-        Iterate over the corpus, returning one sparse vector at a time.
-
-        """
+        """Iterate over the corpus, returning one sparse vector at a time."""
         reader = csv.reader(utils.smart_open(self.fname), self.dialect)
         if self.headers:
             next(reader)    # skip the headers
