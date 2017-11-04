c983477b203ea07478507bf0ca7295ed146d9abe
horpto <__singleton__@hackerdom.ru>
2017-11-05 04:54:46 +0500

    fix #1682, custom dtype of keyedvectors

--- gensim/models/keyedvectors.py
+++ gensim/models/keyedvectors.py
@@ -156,7 +156,7 @@ class KeyedVectors(utils.SaveLoad):
                 if binary:
                     fout.write(utils.to_utf8(word) + b" " + row.tostring())
                 else:
-                    fout.write(utils.to_utf8("%s %s\n" % (word, ' '.join("%f" % val for val in row))))
+                    fout.write(utils.to_utf8("%s %s\n" % (word, ' '.join(repr(val) for val in row))))
 
     @classmethod
     def load_word2vec_format(cls, fname, fvocab=None, binary=False, encoding='utf8', unicode_errors='strict',
@@ -199,35 +199,35 @@ class KeyedVectors(utils.SaveLoad):
                     counts[word] = int(count)
 
         logger.info("loading projection weights from %s", fname)
+        result = cls()
+        def add_word(word, weights):
+            word_id = len(result.vocab)
+            if word in result.vocab:
+                logger.warning("duplicate word '%s' in %s, ignoring all but first", word, fname)
+                return
+            if counts is None:
+                # most common scenario: no vocab file given. just make up some bogus counts, in descending order
+                result.vocab[word] = Vocab(index=word_id, count=vocab_size - word_id)
+            elif word in counts:
+                # use count from the vocab file
+                result.vocab[word] = Vocab(index=word_id, count=counts[word])
+            else:
+                # vocab file given, but word is missing -- set count to None (TODO: or raise?)
+                logger.warning("vocabulary file is incomplete: '%s' is missing", word)
+                result.vocab[word] = Vocab(index=word_id, count=None)
+            result.syn0[word_id] = weights
+            result.index2word.append(word)
+
         with utils.smart_open(fname) as fin:
             header = utils.to_unicode(fin.readline(), encoding=encoding)
             vocab_size, vector_size = (int(x) for x in header.split())  # throws for invalid file format
             if limit:
                 vocab_size = min(vocab_size, limit)
-            result = cls()
             result.vector_size = vector_size
             result.syn0 = zeros((vocab_size, vector_size), dtype=datatype)
 
-            def add_word(word, weights):
-                word_id = len(result.vocab)
-                if word in result.vocab:
-                    logger.warning("duplicate word '%s' in %s, ignoring all but first", word, fname)
-                    return
-                if counts is None:
-                    # most common scenario: no vocab file given. just make up some bogus counts, in descending order
-                    result.vocab[word] = Vocab(index=word_id, count=vocab_size - word_id)
-                elif word in counts:
-                    # use count from the vocab file
-                    result.vocab[word] = Vocab(index=word_id, count=counts[word])
-                else:
-                    # vocab file given, but word is missing -- set count to None (TODO: or raise?)
-                    logger.warning("vocabulary file is incomplete: '%s' is missing", word)
-                    result.vocab[word] = Vocab(index=word_id, count=None)
-                result.syn0[word_id] = weights
-                result.index2word.append(word)
-
             if binary:
-                binary_len = dtype(REAL).itemsize * vector_size
+                binary_len = dtype(datatype).itemsize * vector_size
                 for _ in xrange(vocab_size):
                     # mixed text and binary: read text first, then binary
                     word = []
@@ -240,7 +240,7 @@ class KeyedVectors(utils.SaveLoad):
                         if ch != b'\n':  # ignore newlines in front of words (some binary files have)
                             word.append(ch)
                     word = utils.to_unicode(b''.join(word), encoding=encoding, errors=unicode_errors)
-                    weights = fromstring(fin.read(binary_len), dtype=REAL)
+                    weights = fromstring(fin.read(binary_len), dtype=datatype)
                     add_word(word, weights)
             else:
                 for line_no in xrange(vocab_size):
@@ -250,7 +250,7 @@ class KeyedVectors(utils.SaveLoad):
                     parts = utils.to_unicode(line.rstrip(), encoding=encoding, errors=unicode_errors).split(" ")
                     if len(parts) != vector_size + 1:
                         raise ValueError("invalid vector on line %s (is this really the text format?)" % line_no)
-                    word, weights = parts[0], [REAL(x) for x in parts[1:]]
+                    word, weights = parts[0], [datatype(x) for x in parts[1:]]
                     add_word(word, weights)
         if result.syn0.shape[0] != len(result.vocab):
             logger.info(
