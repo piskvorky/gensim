6dd6915b7d5af38202f14f97d478b9ba350e9ff9
Jayant Jain <jayantjain1992@gmail.com>
2017-11-08 05:48:02 +0530

    More unittests for PoincareModel

--- gensim/test/test_poincare.py
+++ gensim/test/test_poincare.py
@@ -13,9 +13,9 @@ import logging
 import unittest
 import os
 
-import numpy
+import numpy as np
 
-from gensim.models.poincare import PoincareData
+from gensim.models.poincare import PoincareData, PoincareModel
 
 
 module_path = os.path.dirname(__file__)  # needed because sample data files are located in the same folder
@@ -34,6 +34,55 @@ class TestPoincareData(unittest.TestCase):
         relations = [relation for relation in PoincareData(utf8_file)]
         self.assertEqual(len(relations), 2)
         self.assertEqual(relations[0], (u'tímto', u'budeš'))
+
+
+class TestPoincareModel(unittest.TestCase):
+    def setUp(self):
+        self.data = PoincareData(datapath('poincare_hypernyms.tsv'))
+
+    def test_data_counts(self):
+        """Tests whether data has been loaded correctly and completely."""
+        model = PoincareModel(self.data)
+        self.assertEqual(len(model.all_relations), 5)
+        self.assertEqual(len(model.term_relations[model.wv.vocab['kangaroo.n.01'].index]), 3)
+        self.assertEqual(len(model.wv.vocab), 7)
+        self.assertTrue('mammal.n.01' not in model.term_relations)
+
+    def test_vector_shape(self):
+        """Tests whether vectors are initialized with the correct size."""
+        model = PoincareModel(self.data, size=20)
+        self.assertEqual(model.wv.syn0.shape, (7, 20))
+
+    def test_training(self):
+        """Tests that vectors are different before and after training."""
+        model = PoincareModel(self.data, iter=2)
+        old_vectors = np.copy(model.wv.syn0)
+        model.train()
+        self.assertFalse(np.allclose(old_vectors, model.wv.syn0))
+
+    def test_reproducible(self):
+        """Tests that vectors are same for two independent models trained with the same seed."""
+        model_1 = PoincareModel(self.data, iter=2, seed=1)
+        model_1.train()
+
+        model_2 = PoincareModel(self.data, iter=2, seed=1)
+        model_2.train()
+        self.assertTrue(np.allclose(model_1.wv.syn0, model_2.wv.syn0))
+
+    def test_burn_in(self):
+        """Tests that vectors are different for models with and without burn-in."""
+        model_1 = PoincareModel(self.data, iter=2, burn_in=0)
+        model_1.train()
+
+        model_2 = PoincareModel(self.data, iter=2, burn_in=1)
+        model_2.train()
+        self.assertFalse(np.allclose(model_1.wv.syn0, model_2.wv.syn0))
+
+    def test_negatives(self):
+        """Tests that correct number of negatives are used."""
+        model = PoincareModel(self.data, negative=5)
+        self.assertEqual(len(model.get_candidate_negatives()), 5)
+
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
