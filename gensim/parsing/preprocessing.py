41f42b35dac5c87947ccc5bb11389ca0a0c32421
ivan <menshikh.iv@gmail.com>
2017-11-13 23:44:06 +0500

    Fix preprocessing

--- gensim/parsing/preprocessing.py
+++ gensim/parsing/preprocessing.py
@@ -4,21 +4,31 @@
 # Licensed under the GNU LGPL v2.1 - http://www.gnu.org/licenses/lgpl.html
 
 """This module contains methods for parsing and preprocessing strings. Let's consider the most noticeable:
-:func:`~gensim.parsing.preprocessing.remove_stopwords` - take string, remove all words those are among stopwords;
-:func:`~gensim.parsing.preprocessing.preprocess_string` -  take string, apply list of chosen filters to it,
-where filters are methods from this module.
+
+* :func:`~gensim.parsing.preprocessing.remove_stopwords` - remove all stopwords from string
+* :func:`~gensim.parsing.preprocessing.preprocess_string` -  preprocess string (in default NLP meaning)
 
 Examples:
 ---------
 >>> from gensim.parsing.preprocessing import remove_stopwords
->>> s = "Better late than never, but better never late."
->>> remove_stopwords(s)
+>>> remove_stopwords("Better late than never, but better never late.")
 u'Better late never, better late.'
+>>>
+>>> preprocess_string("<i>Hel 9lo</i> <b>Wo9 rld</b>! Th3     weather_is really g00d today, isn't it?")
+[u'hel', u'rld', u'weather', u'todai', u'isn']
 
->>> from gensim.parsing.preprocessing import preprocess_string
->>> s = "<i>Hel 9lo</i> <b>Wo9 rld</b>! Th3     weather_is really g00d today, isn't it?"
->>> preprocess_string(s)
-[u'hel', u'rld', u'weather', u'todai', u'isn'
+
+Data:
+-----
+
+.. data:: STOPWORDS - Set of stopwords from Stone, Denis, Kwantes (2010).
+.. data:: RE_PUNCT - Regexp for search an punctuation.
+.. data:: RE_TAGS - Regexp for search an tags.
+.. data:: RE_NUMERIC - Regexp for search an numbers.
+.. data:: RE_NONALPHA - Regexp for search an non-alphabetic character.
+.. data:: RE_AL_NUM - Regexp for search a position between letters and digits.
+.. data:: RE_NUM_AL - Regexp for search a position between digits and letters .
+.. data:: RE_WHITESPACE - Regexp for search space characters.
 
 """
 
@@ -30,59 +40,49 @@ from gensim import utils
 from gensim.parsing.porter import PorterStemmer
 
 
-# improved list from Stone, Denis, Kwantes (2010)
-STOPWORDS = """
-a about above across after afterwards again against all almost alone along already also although always am among amongst amoungst amount an and another any anyhow anyone anything anyway anywhere are around as at back be
-became because become becomes becoming been before beforehand behind being below beside besides between beyond bill both bottom but by call can
-cannot cant co computer con could couldnt cry de describe
-detail did didn do does doesn doing don done down due during
-each eg eight either eleven else elsewhere empty enough etc even ever every everyone everything everywhere except few fifteen
-fify fill find fire first five for former formerly forty found four from front full further get give go
-had has hasnt have he hence her here hereafter hereby herein hereupon hers herself him himself his how however hundred i ie
-if in inc indeed interest into is it its itself keep last latter latterly least less ltd
-just
-kg km
-made make many may me meanwhile might mill mine more moreover most mostly move much must my myself name namely
-neither never nevertheless next nine no nobody none noone nor not nothing now nowhere of off
-often on once one only onto or other others otherwise our ours ourselves out over own part per
-perhaps please put rather re
-quite
-rather really regarding
-same say see seem seemed seeming seems serious several she should show side since sincere six sixty so some somehow someone something sometime sometimes somewhere still such system take ten
-than that the their them themselves then thence there thereafter thereby therefore therein thereupon these they thick thin third this those though three through throughout thru thus to together too top toward towards twelve twenty two un under
-until up unless upon us used using
-various very very via
-was we well were what whatever when whence whenever where whereafter whereas whereby wherein whereupon wherever whether which while whither who whoever whole whom whose why will with within without would yet you
-your yours yourself yourselves
-"""
+STOPWORDS = frozenset([
+    'all', 'six', 'just', 'less', 'being', 'indeed', 'over', 'move', 'anyway', 'four', 'not', 'own', 'through',
+    'using', 'fify', 'where', 'mill', 'only', 'find', 'before', 'one', 'whose', 'system', 'how', 'somewhere',
+    'much', 'thick', 'show', 'had', 'enough', 'should', 'to', 'must', 'whom', 'seeming', 'yourselves', 'under',
+    'ours', 'two', 'has', 'might', 'thereafter', 'latterly', 'do', 'them', 'his', 'around', 'than', 'get', 'very',
+    'de', 'none', 'cannot', 'every', 'un', 'they', 'front', 'during', 'thus', 'now', 'him', 'nor', 'name', 'regarding',
+    'several', 'hereafter', 'did', 'always', 'who', 'didn', 'whither', 'this', 'someone', 'either', 'each', 'become',
+    'thereupon', 'sometime', 'side', 'towards', 'therein', 'twelve', 'because', 'often', 'ten', 'our', 'doing', 'km',
+    'eg', 'some', 'back', 'used', 'up', 'go', 'namely', 'computer', 'are', 'further', 'beyond', 'ourselves', 'yet',
+    'out', 'even', 'will', 'what', 'still', 'for', 'bottom', 'mine', 'since', 'please', 'forty', 'per', 'its',
+    'everything', 'behind', 'does', 'various', 'above', 'between', 'it', 'neither', 'seemed', 'ever', 'across', 'she',
+    'somehow', 'be', 'we', 'full', 'never', 'sixty', 'however', 'here', 'otherwise', 'were', 'whereupon', 'nowhere',
+    'although', 'found', 'alone', 're', 'along', 'quite', 'fifteen', 'by', 'both', 'about', 'last', 'would',
+    'anything', 'via', 'many', 'could', 'thence', 'put', 'against', 'keep', 'etc', 'amount', 'became', 'ltd', 'hence',
+    'onto', 'or', 'con', 'among', 'already', 'co', 'afterwards', 'formerly', 'within', 'seems', 'into', 'others',
+    'while', 'whatever', 'except', 'down', 'hers', 'everyone', 'done', 'least', 'another', 'whoever', 'moreover',
+    'couldnt', 'throughout', 'anyhow', 'yourself', 'three', 'from', 'her', 'few', 'together', 'top', 'there', 'due',
+    'been', 'next', 'anyone', 'eleven', 'cry', 'call', 'therefore', 'interest', 'then', 'thru', 'themselves',
+    'hundred', 'really', 'sincere', 'empty', 'more', 'himself', 'elsewhere', 'mostly', 'on', 'fire', 'am', 'becoming',
+    'hereby', 'amongst', 'else', 'part', 'everywhere', 'too', 'kg', 'herself', 'former', 'those', 'he', 'me', 'myself',
+    'made', 'twenty', 'these', 'was', 'bill', 'cant', 'us', 'until', 'besides', 'nevertheless', 'below', 'anywhere',
+    'nine', 'can', 'whether', 'of', 'your', 'toward', 'my', 'say', 'something', 'and', 'whereafter', 'whenever',
+    'give', 'almost', 'wherever', 'is', 'describe', 'beforehand', 'herein', 'doesn', 'an', 'as', 'itself', 'at',
+    'have', 'in', 'seem', 'whence', 'ie', 'any', 'fill', 'again', 'hasnt', 'inc', 'thereby', 'thin', 'no', 'perhaps',
+    'latter', 'meanwhile', 'when', 'detail', 'same', 'wherein', 'beside', 'also', 'that', 'other', 'take', 'which',
+    'becomes', 'you', 'if', 'nobody', 'unless', 'whereas', 'see', 'though', 'may', 'after', 'upon', 'most', 'hereupon',
+    'eight', 'but', 'serious', 'nothing', 'such', 'why', 'off', 'a', 'don', 'whereby', 'third', 'i', 'whole', 'noone',
+    'sometimes', 'well', 'amoungst', 'yours', 'their', 'rather', 'without', 'so', 'five', 'the', 'first', 'with',
+    'make', 'once'
+])
 
-# set of stopwords for :func:`~gensim.parsing.preprocessing.remove_stopwords`.
-STOPWORDS = frozenset(w for w in STOPWORDS.split() if w)
 
-# remove punctuation according to :func:`~gensim.parsing.preprocessing.strip_punctuation`.
 RE_PUNCT = re.compile(r'([%s])+' % re.escape(string.punctuation), re.UNICODE)
-
-# remove tags according to :func:`~gensim.parsing.preprocessing.strip_tags`.
 RE_TAGS = re.compile(r"<([^>]+)>", re.UNICODE)
-
-# remove digits according to :func:`~gensim.parsing.preprocessing.strip_numeric`.
 RE_NUMERIC = re.compile(r"[0-9]+", re.UNICODE)
-
-# remove not a word characters according to :func:`~gensim.parsing.preprocessing.non_alphanum`.
 RE_NONALPHA = re.compile(r"\W", re.UNICODE)
-
-# add spaces between letters & digits according to :func:`~gensim.parsing.preprocessing.split_alphanum`.
 RE_AL_NUM = re.compile(r"([a-z]+)([0-9]+)", flags=re.UNICODE)
-
-# add spaces between digits & letters according to :func:`~gensim.parsing.preprocessing.split_alphanum`.
 RE_NUM_AL = re.compile(r"([0-9]+)([a-z]+)", flags=re.UNICODE)
-
-# remove repeating in a row whitespace characters according to :func:`~gensim.parsing.preprocessing.multiple_whitespaces`.
 RE_WHITESPACE = re.compile(r"(\s)+", re.UNICODE)
 
 
 def remove_stopwords(s):
-    """Remove `STOPWORDS` from s.
+    """Remove :const:`~gensim.parsing.preprocessing.STOPWORDS` from `s`.
 
     Parameters
     ----------
@@ -91,13 +91,12 @@ def remove_stopwords(s):
     Returns
     -------
     str
-        Unicode string without stopwords.
+        Unicode string without :const:`~gensim.parsing.preprocessing.STOPWORDS`.
 
     Examples
     --------
     >>> from gensim.parsing.preprocessing import remove_stopwords
-    >>> s = "Better late than never, but better never late."
-    >>> remove_stopwords(s)
+    >>> remove_stopwords("Better late than never, but better never late.")
     u'Better late never, better late.'
 
     """
@@ -106,7 +105,7 @@ def remove_stopwords(s):
 
 
 def strip_punctuation(s):
-    """Replace punctuation characters with spaces.
+    """Replace punctuation characters with spaces using :const:`~gensim.parsing.preprocessing.RE_PUNCT`.
 
     Parameters
     ----------
@@ -120,8 +119,7 @@ def strip_punctuation(s):
     Examples
     --------
     >>> from gensim.parsing.preprocessing import strip_punctuation
-    >>> s = "A semicolon is a stronger break than a comma, but not as much as a full stop!"
-    >>> strip_punctuation(s)
+    >>> strip_punctuation("A semicolon is a stronger break than a comma, but not as much as a full stop!")
     u'A semicolon is a stronger break than a comma  but not as much as a full stop '
 
     """
@@ -129,16 +127,11 @@ def strip_punctuation(s):
     return RE_PUNCT.sub(" ", s)
 
 
-# unicode.translate cannot delete characters like str can
 strip_punctuation2 = strip_punctuation
-
-# def strip_punctuation2(s):
-#     s = utils.to_unicode(s)
-#     return s.translate(None, string.punctuation)
 
 
 def strip_tags(s):
-    """Remove tags from s.
+    """Remove tags from `s` using :const:`~gensim.parsing.preprocessing.RE_TAGS`.
 
     Parameters
     ----------
@@ -152,8 +145,7 @@ def strip_tags(s):
     Examples
     --------
     >>> from gensim.parsing.preprocessing import strip_tags
-    >>> s = "<i>Hello</i> <b>World</b>!"
-    >>> strip_tags(s)
+    >>> strip_tags("<i>Hello</i> <b>World</b>!")
     u'Hello World!'
 
     """
@@ -162,7 +154,7 @@ def strip_tags(s):
 
 
 def strip_short(s, minsize=3):
-    """Remove words with length lesser than minsize (default = 3).
+    """Remove words with length lesser than `minsize` from `s`.
 
     Parameters
     ----------
@@ -172,18 +164,15 @@ def strip_short(s, minsize=3):
     Returns
     -------
     str
-        Unicode string without words with length lesser than minsize.
+        Unicode string without short words.
 
     Examples
     --------
     >>> from gensim.parsing.preprocessing import strip_short
-    >>> s = "salut les amis du 59"
-    >>> strip_short(s)
+    >>> strip_short("salut les amis du 59")
     u'salut les amis'
-
-    >>> from gensim.parsing.preprocessing import strip_short
-    >>> s = "one two three four five six seven eight nine ten"
-    >>> strip_short(s,5)
+    >>>
+    >>> strip_short("one two three four five six seven eight nine ten", minsize=5)
     u'three seven eight'
 
     """
@@ -192,7 +181,7 @@ def strip_short(s, minsize=3):
 
 
 def strip_numeric(s):
-    """Remove digits from s.
+    """Remove digits from `s` using :const:`~gensim.parsing.preprocessing.RE_NUMERIC`.
 
     Parameters
     ----------
@@ -201,13 +190,12 @@ def strip_numeric(s):
     Returns
     -------
     str
-        Unicode string without digits.
+        Unicode  string without digits.
 
     Examples
     --------
     >>> from gensim.parsing.preprocessing import strip_numeric
-    >>> s = "0text24gensim365test"
-    >>> strip_numeric(s)
+    >>> strip_numeric("0text24gensim365test")
     u'textgensimtest'
 
     """
@@ -216,8 +204,7 @@ def strip_numeric(s):
 
 
 def strip_non_alphanum(s):
-    """Remove not a word characters from s.
-    (Word characters - alphanumeric & underscore)
+    """Remove non-alphabetic characters from `s` using :const:`~gensim.parsing.preprocessing.RE_NONALPHA`.
 
     Parameters
     ----------
@@ -228,11 +215,14 @@ def strip_non_alphanum(s):
     str
         Unicode string without not a word characters.
 
+    Notes
+    -----
+    Word characters - alphanumeric & underscore.
+
     Examples
     --------
     >>> from gensim.parsing.preprocessing import strip_non_alphanum
-    >>> s = "if-you#can%read$this&then@this#method^works"
-    >>> strip_non_alphanum(s)
+    >>> strip_non_alphanum("if-you#can%read$this&then@this#method^works")
     u'if you can read this then this method works'
 
     """
@@ -241,8 +231,8 @@ def strip_non_alphanum(s):
 
 
 def strip_multiple_whitespaces(s):
-    r"""Remove repeating whitespace characters (spaces, tabs, line breaks) from s
-    and turns tabs & line breaks into spaces.
+    r"""Remove repeating whitespace characters (spaces, tabs, line breaks) from `s`
+    and turns tabs & line breaks into spaces using :const:`~gensim.parsing.preprocessing.RE_WHITESPACE`.
 
     Parameters
     ----------
@@ -256,8 +246,7 @@ def strip_multiple_whitespaces(s):
     Examples
     --------
     >>> from gensim.parsing.preprocessing import strip_multiple_whitespaces
-    >>> s = "salut" + '\r' + " les" + '\n' + "         loulous!"
-    >>> strip_multiple_whitespaces(s)
+    >>> strip_multiple_whitespaces("salut" + '\r' + " les" + '\n' + "         loulous!")
     u'salut les loulous!'
 
     """
@@ -266,7 +255,7 @@ def strip_multiple_whitespaces(s):
 
 
 def split_alphanum(s):
-    """Add spaces between digits & letters in s.
+    """Add spaces between digits & letters in `s` using :const:`~gensim.parsing.preprocessing.RE_AL_NUM`.
 
     Parameters
     ----------
@@ -280,8 +269,7 @@ def split_alphanum(s):
     Examples
     --------
     >>> from gensim.parsing.preprocessing import split_alphanum
-    >>> s = "24.0hours7 days365 a1b2c3"
-    >>> split_alphanum(s)
+    >>> split_alphanum("24.0hours7 days365 a1b2c3")
     u'24.0 hours 7 days 365 a 1 b 2 c 3'
 
     """
@@ -291,7 +279,7 @@ def split_alphanum(s):
 
 
 def stem_text(text):
-    """Transform s into lowercase and (porter-)stemmed version.
+    """Transform `s` into lowercase and stem it.
 
     Parameters
     ----------
@@ -300,14 +288,13 @@ def stem_text(text):
     Returns
     -------
     str
-        Lowercase and (porter-)stemmed version of string `text`.
+        Unicode lowercased and porter-stemmed version of string `text`.
 
     Examples
     --------
     >>> from gensim.parsing.preprocessing import stem_text
-    >>> text = "While it is quite useful to be able to search a large collection of documents almost instantly for a joint occurrence of a collection of exact words, for many searching purposes, a little fuzziness would help. "
-    >>> stem_text(text)
-    u'while it is quit us to be abl to search a larg collect of document almost instantli for a joint occurr of a collect of exact words, for mani search purposes, a littl fuzzi would help.'
+    >>> stem_text("While it is quite useful to be able to search a large collection of documents almost instantly.")
+    u'while it is quit us to be abl to search a larg collect of document almost instantly.'
 
     """
     text = utils.to_unicode(text)
@@ -326,31 +313,34 @@ DEFAULT_FILTERS = [
 
 
 def preprocess_string(s, filters=DEFAULT_FILTERS):
-    """Apply list of chosen filters to s, where filters are methods from this module.
-    Default list of filters consists of: :func:`~gensim.parsing.preprocessing.strip_tags`,
-    :func:`~gensim.parsing.preprocessing.strip_punctuation`, :func:`~gensim.parsing.preprocessing.strip_multiple_whitespaces`,
-    :func:`~gensim.parsing.preprocessing.strip_numeric`, :func:`~gensim.parsing.preprocessing.remove_stopwords`,
-    :func:`~gensim.parsing.preprocessing.strip_short`,  :func:`~gensim.parsing.preprocessing.stem_text`.
-    <function <lambda>> in signature means that we use lambda function for applying methods to filters.
+    """Apply list of chosen filters to `s`.
+
+    Default list of filters:
+
+    * :func:`~gensim.parsing.preprocessing.strip_tags`,
+    * :func:`~gensim.parsing.preprocessing.strip_punctuation`,
+    * :func:`~gensim.parsing.preprocessing.strip_multiple_whitespaces`,
+    * :func:`~gensim.parsing.preprocessing.strip_numeric`,
+    * :func:`~gensim.parsing.preprocessing.remove_stopwords`,
+    * :func:`~gensim.parsing.preprocessing.strip_short`,
+    * :func:`~gensim.parsing.preprocessing.stem_text`.
 
     Parameters
     ----------
     s : str
-    filters: list, optional
+    filters: list of functions, optional
 
     Returns
     -------
-    list
-        List of unicode strings.
+    list of str
+        Processed strings (cleaned).
 
     Examples
     --------
     >>> from gensim.parsing.preprocessing import preprocess_string
-    >>> s = "<i>Hel 9lo</i> <b>Wo9 rld</b>! Th3     weather_is really g00d today, isn't it?"
-    >>> preprocess_string(s)
+    >>> preprocess_string("<i>Hel 9lo</i> <b>Wo9 rld</b>! Th3     weather_is really g00d today, isn't it?")
     [u'hel', u'rld', u'weather', u'todai', u'isn']
-
-    >>> from gensim.parsing.preprocessing import preprocess_string, strip_tags, strip_punctuation
+    >>>
     >>> s = "<i>Hel 9lo</i> <b>Wo9 rld</b>! Th3     weather_is really g00d today, isn't it?"
     >>> CUSTOM_FILTERS = [lambda x: x.lower(), strip_tags, strip_punctuation]
     >>> preprocess_string(s, CUSTOM_FILTERS)
@@ -378,8 +368,7 @@ def preprocess_documents(docs):
     Examples
     --------
     >>> from gensim.parsing.preprocessing import preprocess_documents
-    >>> s = ["<i>Hel 9lo</i> <b>Wo9 rld</b>!", "Th3     weather_is really g00d today, isn't it?"]
-    >>> preprocess_documents(s)
+    >>> preprocess_documents(["<i>Hel 9lo</i> <b>Wo9 rld</b>!", "Th3     weather_is really g00d today, isn't it?"])
     [[u'hel', u'rld'], [u'weather', u'todai', u'isn']]
 
     """
