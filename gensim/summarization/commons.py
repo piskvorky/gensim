c6c608b0d510d382a3d3aa09095a67ae91c4e86d
Yuri Isakov <isakovuv@gmail.com>
2017-11-16 19:43:04 +0300

    added doctrings for graph modules

--- gensim/summarization/commons.py
+++ gensim/summarization/commons.py
@@ -3,10 +3,46 @@
 #
 # Licensed under the GNU LGPL v2.1 - http://www.gnu.org/licenses/lgpl.html
 
+"""This module provides functions of creatinf graph from sequence of values and
+removing of unreachable nodes.
+
+
+Examples
+--------
+
+Create simple graph and add edges. Let's kake a look at nodes.
+
+>>> gg = build_graph(['Felidae', 'Lion', 'Tiger', 'Wolf'])
+>>> gg.add_edge(("Felidae", "Lion"))
+>>> gg.add_edge(("Felidae", "Tiger"))
+>>> gg.nodes()
+['Felidae', 'Lion', 'Tiger', 'Wolf']
+
+Remove nodes with no edges.
+
+>>> remove_unreachable_nodes(gg)
+>>> gg.nodes()
+['Felidae', 'Lion', 'Tiger']
+
+"""
+
 from gensim.summarization.graph import Graph
 
 
 def build_graph(sequence):
+    """Creates and returns graph with given sequence of values.
+
+    Parameters
+    ----------
+    sequence : list
+        Sequence of values.
+
+    Returns
+    -------
+    Graph
+        Created graph.
+
+    """
     graph = Graph()
     for item in sequence:
         if not graph.has_node(item):
@@ -15,6 +51,15 @@ def build_graph(sequence):
 
 
 def remove_unreachable_nodes(graph):
+    """Removes unreachable nodes (nodes with no edges). Works inplace.
+
+    Parameters
+    ----------
+    graph : Graph
+        Given graph.
+
+    """
+
     for node in graph.nodes():
         if sum(graph.edge_weight((node, other)) for other in graph.neighbors(node)) == 0:
             graph.del_node(node)
--- gensim/summarization/graph.py
+++ gensim/summarization/graph.py
@@ -3,11 +3,41 @@
 #
 # Licensed under the GNU LGPL v2.1 - http://www.gnu.org/licenses/lgpl.html
 
+"""This module contains abstract class IGraph represents graphs interface and
+class Graph (based on IGraph) which implements undirected graph.
+
+Examples
+--------
+
+Create simple graph with 4 nodes.
+
+>>> g = Graph()
+>>> g.add_node('Felidae')
+>>> g.add_node('Lion')
+>>> g.add_node('Tiger')
+>>> g.add_node('Wolf')
+>>> g.nodes()
+['Felidae', 'Lion', 'Tiger', 'Wolf']
+
+Add some edges. Let's check neighbours.
+
+>>> g.add_edge(("Felidae", "Lion"))
+>>> g.add_edge(("Felidae", "Tiger"))
+>>> g.neighbors("Felidae")
+['Lion', 'Tiger']
+
+One node has no neighbours.
+
+>>> g.neighbors("Wolf")
+[]
+
+"""
+
 from abc import ABCMeta, abstractmethod
 
 
 class IGraph(object):
-    """ Represents the interface or contract that the graph for TextRank
+    """Represents the interface or contract that the graph for TextRank
     should implement.
     """
     __metaclass__ = ABCMeta
@@ -15,20 +45,26 @@ class IGraph(object):
     @abstractmethod
     def nodes(self):
         """
-        Return node list.
+        Returns all nodes of graph.
 
-        @rtype:  list
-        @return: Node list.
+        Returns
+        -------
+        list of node
+            Nodes of graph.
+
         """
         pass
 
     @abstractmethod
     def edges(self):
         """
-        Return all edges in the graph.
+        Returns all edges of graph.
 
-        @rtype:  list
-        @return: List of all edges in the graph.
+        Returns
+        -------
+        list of (tuple of node)
+            Edges of graph.
+
         """
         pass
 
@@ -37,107 +73,137 @@ class IGraph(object):
         """
         Return all nodes that are directly accessible from given node.
 
-        @type  node: node
-        @param node: Node identifier
+        Parameters
+        ----------
+        node : str or float
+            Given node identifier.
 
-        @rtype:  list
-        @return: List of nodes directly accessible from given node.
+        Returns
+        -------
+        list of node
+            Nodes directly accessible from given `node`.
+
         """
         pass
 
     @abstractmethod
     def has_node(self, node):
-        """
-        Return whether the requested node exists.
+        """Returns whether the requested node exists.
 
-        @type  node: node
-        @param node: Node identifier
+        Parameters
+        ----------
+        node : str or float
+            Given node identifier.
 
-        @rtype:  boolean
-        @return: Truth-value for node existence.
+        Returns
+        -------
+        bool
+            True if `node` exists, False otherwise.
+
         """
         pass
 
     @abstractmethod
     def add_node(self, node, attrs=None):
-        """
-        Add given node to the graph.
-
-        @attention: While nodes can be of any type, it's strongly recommended
+        """Adds given node to the graph.
+        
+        Note
+        ----
+        While nodes can be of any type, it's strongly recommended
         to use only numbers and single-line strings as node identifiers if you
         intend to use write().
 
-        @type  node: node
-        @param node: Node identifier.
+        Parameters
+        ----------
+        node : float or str
+            Given node
+        attrs : list
+            Node attributes specified as (attribute, value)
 
-        @type  attrs: list
-        @param attrs: List of node attributes specified as (attribute, value)
-        tuples.
         """
         pass
 
     @abstractmethod
     def add_edge(self, edge, wt=1, label='', attrs=None):
-        """
-        Add an edge to the graph connecting two nodes.
+        """Adds an edge to the graph connecting two nodes. An edge, here, 
+        is a tuple of two nodes.
 
-        An edge, here, is a pair of nodes like C{(n, m)}.
+        Parameters
+        ----------
+        edge : tuple of node
+            Given edge.
+        wt : float
+            Weight of new edge.
+        label : str
+            Edge label.
+        attrs : list
+            Node attributes specified as (attribute, value)
 
-        @type  edge: tuple
-        @param edge: Edge.
-
-        @type  wt: number
-        @param wt: Edge weight.
-
-        @type  label: string
-        @param label: Edge label.
-
-        @type  attrs: list
-        @param attrs: List of node attributes specified as (attribute, value)
-        tuples.
         """
         pass
 
     @abstractmethod
     def has_edge(self, edge):
-        """
-        Return whether an edge exists.
+        """Returns whether an edge exists.
 
-        @type  edge: tuple
-        @param edge: Edge.
+        Parameters
+        ----------
+        edge : tuple of node
+            Given edge. An edge, here, is a tuple of two nodes.
 
-        @rtype:  boolean
-        @return: Truth-value for edge existence.
+        Returns
+        -------
+        bool
+            True if `edge` exists, False otherwise.
+
         """
         pass
 
     @abstractmethod
     def edge_weight(self, edge):
-        """
-        Get the weight of an edge.
+        """Returns weigth of given edge.
 
-        @type  edge: edge
-        @param edge: One edge.
+        Parameters
+        ----------
+        edge : tuple of node
+            Given edge.
 
-        @rtype:  number
-        @return: Edge weight.
+        Returns
+        -------
+        float
+            Edge weight.
+
         """
         pass
 
     @abstractmethod
     def del_node(self, node):
-        """
-        Remove a node from the graph.
+        """Removes node and its edges from graph.
 
-        @type  node: node
-        @param node: Node identifier.
+        Parameters
+        ----------
+        node : float or str
+            Given node.
+        
         """
         pass
 
 
 class Graph(IGraph):
     """
-    Implementation of an undirected graph, based on Pygraph
+    Implementation of an undirected graph, based on IGraph.
+
+    Attributes
+    ----------
+    WEIGHT_ATTRIBUTE_NAME : str
+        Name of weight attribute in graph.
+    DEFAULT_WEIGHT : float
+        Weight set by default.
+    LABEL_ATTRIBUTE_NAME : str
+        Name of attribute.
+    DEFAULT_LABEL : str
+        Label set by default.
+
     """
 
     WEIGHT_ATTRIBUTE_NAME = "weight"
@@ -160,19 +226,91 @@ class Graph(IGraph):
         self.node_neighbors = {}
 
     def has_edge(self, edge):
+        """Returns whether an edge exists.
+
+        Parameters
+        ----------
+        edge : tuple of node
+            Given edge. An edge, here, is a tuple of two nodes.
+
+        Returns
+        -------
+        bool
+            True if `edge` exists, False otherwise.
+
+        """
         u, v = edge
         return (u, v) in self.edge_properties and (v, u) in self.edge_properties
 
     def edge_weight(self, edge):
+        """Returns weigth of given edge.
+
+        Parameters
+        ----------
+        edge : tuple of node
+            Given edge.
+
+        Returns
+        -------
+        float
+            Edge weight.
+
+        """
         return self.get_edge_properties(edge).setdefault(self.WEIGHT_ATTRIBUTE_NAME, self.DEFAULT_WEIGHT)
 
     def neighbors(self, node):
+        """Returns all nodes that are directly accessible from given node.
+
+        Parameters
+        ----------
+        node : float or str
+            Given node identifier.
+
+        Returns
+        -------
+        list of node
+            Nodes directly accessible from given `node`.
+
+        """
         return self.node_neighbors[node]
 
     def has_node(self, node):
+        """Returns whether the requested node exists.
+
+        Parameters
+        ----------
+        node : float or str
+            Given node identifier.
+
+        Returns
+        -------
+        bool
+            True if `node` exists, False otherwise.
+
+        """
         return node in self.node_neighbors
 
     def add_edge(self, edge, wt=1, label='', attrs=None):
+        """Adds an edge to the graph connecting two nodes. An edge, here, 
+        is a tuple of two nodes.
+
+        Parameters
+        ----------
+        edge : tuple of node
+            Given edge.
+        wt : float
+            Weight of new edge.
+        label : str
+            Edge label.
+        attrs : list
+            Node attributes specified as (attribute, value).
+
+        Raises
+        ------
+        ValueError
+            If `edge` already exists in graph.
+
+        """
         if attrs is None:
             attrs = []
         u, v = edge
@@ -187,6 +325,27 @@ class Graph(IGraph):
             raise ValueError("Edge (%s, %s) already in graph" % (u, v))
 
     def add_node(self, node, attrs=None):
+        """Adds given node to the graph.
+        
+        Note
+        ----
+        While nodes can be of any type, it's strongly recommended
+        to use only numbers and single-line strings as node identifiers if you
+        intend to use write().
+
+        Parameters
+        ----------
+        node : float or str
+            Given node.
+        attrs : list
+            Node attributes specified as (attribute, value)
+
+        Raises
+        ------
+        ValueError
+            If `node` already exists in graph.
+
+        """
         if attrs is None:
             attrs = []
         if node not in self.node_neighbors:
@@ -197,43 +356,137 @@ class Graph(IGraph):
 
     def nodes(self):
         return list(self.node_neighbors.keys())
+        """Returns all nodes of graph.
+
+        Returns
+        -------
+        list of node
+            Nodes of graph.
+        
+        """
 
     def edges(self):
+        """Returns all edges of graph.
+
+        Returns
+        -------
+        list of edges (tuple of node)
+            Edges of graph.
+        
+        """
         return [a for a in self.edge_properties.keys()]
 
     def del_node(self, node):
+        """Removes given node and its edges from graph.
+
+        Parameters
+        ----------
+        node : float or str
+            Given node.
+        
+        """
         for each in list(self.neighbors(node)):
             if each != node:
                 self.del_edge((each, node))
         del self.node_neighbors[node]
         del self.node_attr[node]
 
-    # Helper methods
     def get_edge_properties(self, edge):
+        """Returns properties of given given edge. If edge doesn't exist 
+        empty dictionary will be returned.
+
+        Parameters
+        ----------
+        edge : tuple of node
+            Given edge.
+
+        Returns
+        -------
+        dict
+            Properties of graph.
+        
+        """
         return self.edge_properties.setdefault(edge, {})
 
     def add_edge_attributes(self, edge, attrs):
+        """Adds attributes `attrs`to given edge. Order of nodes in edge doesn't 
+        matter.
+
+        Parameters
+        ----------
+        edge : tuple of node
+            Given edge.
+        attrs : list
+            Provided attributes to add.
+        
+        """
         for attr in attrs:
             self.add_edge_attribute(edge, attr)
 
     def add_edge_attribute(self, edge, attr):
+        """Adds attribute `attr`to given edge. Order of nodes in edge doesn't 
+        matter.
+
+        Parameters
+        ----------
+        edge : tuple of node
+            Given edge.
+
+        attr : object
+            Provided attribute to add.
+        
+        """
         self.edge_attr[edge] = self.edge_attributes(edge) + [attr]
 
         if edge[0] != edge[1]:
             self.edge_attr[(edge[1], edge[0])] = self.edge_attributes((edge[1], edge[0])) + [attr]
 
     def edge_attributes(self, edge):
+        """Returns attributes of given edge. In case of non existing edge 
+        returns empty list.
+
+        Parameters
+        ----------
+        edge : tuple of node
+            Given edge.
+        
+        Returns
+        -------
+        list
+            Attributes of given edge.
+        
+        """
         try:
             return self.edge_attr[edge]
         except KeyError:
             return []
 
     def set_edge_properties(self, edge, **properties):
+        """Adds `properties` to given edge. Order of nodes in edge doesn't 
+        matter.
+
+        Parameters
+        ----------
+        edge : tuple of node
+            Given edge.
+
+        properties : dictionary
+            Properties to add.
+        
+        """
         self.edge_properties.setdefault(edge, {}).update(properties)
         if edge[0] != edge[1]:
             self.edge_properties.setdefault((edge[1], edge[0]), {}).update(properties)
 
     def del_edge(self, edge):
+        """Removes given edges from graph.
+
+        Parameters
+        ----------
+        edge : tuple of node
+            Given edge.
+        
+        """
         u, v = edge
         self.node_neighbors[u].remove(v)
         self.del_edge_labeling((u, v))
@@ -242,6 +495,14 @@ class Graph(IGraph):
             self.del_edge_labeling((v, u))
 
     def del_edge_labeling(self, edge):
+        """Removes attributes and properties if given edge.
+
+        Parameters
+        ----------
+        edge : tuple of node
+            Given edge.
+        
+        """
         keys = [edge, edge[::-1]]
 
         for key in keys:
