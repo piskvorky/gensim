df9903a43ed65721f754a6cd0000e4238080be2e
Timofey Yefimov <anotherbugmaster@gmail.com>
2017-11-23 05:45:03 +0500

    Revert "Fix typo"
    This reverts commit 3cc34fffb821af60f09d80b39dff0ae1a9a9cc4e.

--- gensim/corpora/__init__.py
+++ gensim/corpora/__init__.py
@@ -1,5 +1,5 @@
 """
-This package contains implementations of various streaming corpus I/O formats.
+This package contains implementations of various streaming corpus I/O format.
 """
 
 # bring corpus classes directly into package namespace, to save some typing
