fa3aa4991aad469d23304dbd99b74d0537e0748a
manneshiva <manneshiva+github@gmail.com>
2017-11-26 17:58:28 +0530

    cythonizes training using skipgram with negative sampling

--- gensim/models/fasttext.py
+++ gensim/models/fasttext.py
@@ -9,77 +9,85 @@ from numpy import zeros, ones, vstack, sum as np_sum, empty, float32 as REAL
 from gensim.models.word2vec import Word2Vec, train_sg_pair, train_cbow_pair
 from gensim.models.wrappers.fasttext import FastTextKeyedVectors
 from gensim.models.wrappers.fasttext import FastText as Ft_Wrapper, compute_ngrams, ft_hash
+from gensim import matutils
 
 logger = logging.getLogger(__name__)
 
-MAX_WORDS_IN_BATCH = 10000
+try:
+    # TODO : log FAST_VERSION
+    from gensim.models.fasttext_inner import train_batch_sg
+    from gensim.models.fasttext_inner import FAST_VERSION, MAX_WORDS_IN_BATCH
 
+except ImportError:
+    # why falling back - log
+    # failed... fall back to plain numpy (20-80x slower training than the above)
+    FAST_VERSION = -1
+    MAX_WORDS_IN_BATCH = 10000
 
-def train_batch_cbow(model, sentences, alpha, work=None, neu1=None):
-    result = 0
-    for sentence in sentences:
-        word_vocabs = [model.wv.vocab[w] for w in sentence if w in model.wv.vocab and
-                       model.wv.vocab[w].sample_int > model.random.rand() * 2**32]
-        for pos, word in enumerate(word_vocabs):
-            reduced_window = model.random.randint(model.window)
-            start = max(0, pos - model.window + reduced_window)
-            window_pos = enumerate(word_vocabs[start:(pos + model.window + 1 - reduced_window)], start)
-            word2_indices = [word2.index for pos2, word2 in window_pos if (word2 is not None and pos2 != pos)]
+    def train_batch_cbow(model, sentences, alpha, work=None, neu1=None):
+        result = 0
+        for sentence in sentences:
+            word_vocabs = [model.wv.vocab[w] for w in sentence if w in model.wv.vocab and
+                           model.wv.vocab[w].sample_int > model.random.rand() * 2**32]
+            for pos, word in enumerate(word_vocabs):
+                reduced_window = model.random.randint(model.window)
+                start = max(0, pos - model.window + reduced_window)
+                window_pos = enumerate(word_vocabs[start:(pos + model.window + 1 - reduced_window)], start)
+                word2_indices = [word2.index for pos2, word2 in window_pos if (word2 is not None and pos2 != pos)]
 
-            word2_subwords = []
-            vocab_subwords_indices = []
-            ngrams_subwords_indices = []
+                word2_subwords = []
+                vocab_subwords_indices = []
+                ngrams_subwords_indices = []
 
-            for index in word2_indices:
-                vocab_subwords_indices += [index]
-                word2_subwords += model.wv.ngrams_word[model.wv.index2word[index]]
+                for index in word2_indices:
+                    vocab_subwords_indices += [index]
+                    word2_subwords += model.wv.ngrams_word[model.wv.index2word[index]]
 
-            for subword in word2_subwords:
-                ngrams_subwords_indices.append(model.wv.ngrams[subword])
+                for subword in word2_subwords:
+                    ngrams_subwords_indices.append(model.wv.ngrams[subword])
 
-            l1_vocab = np_sum(model.wv.syn0_vocab[vocab_subwords_indices], axis=0)  # 1 x vector_size
-            l1_ngrams = np_sum(model.wv.syn0_ngrams[ngrams_subwords_indices], axis=0)  # 1 x vector_size
+                l1_vocab = np_sum(model.wv.syn0_vocab[vocab_subwords_indices], axis=0)  # 1 x vector_size
+                l1_ngrams = np_sum(model.wv.syn0_ngrams[ngrams_subwords_indices], axis=0)  # 1 x vector_size
 
-            l1 = np_sum([l1_vocab, l1_ngrams], axis=0)
-            subwords_indices = [vocab_subwords_indices] + [ngrams_subwords_indices]
-            if (subwords_indices[0] or subwords_indices[1]) and model.cbow_mean:
-                l1 /= (len(subwords_indices[0]) + len(subwords_indices[1]))
+                l1 = np_sum([l1_vocab, l1_ngrams], axis=0)
+                subwords_indices = [vocab_subwords_indices] + [ngrams_subwords_indices]
+                if (subwords_indices[0] or subwords_indices[1]) and model.cbow_mean:
+                    l1 /= (len(subwords_indices[0]) + len(subwords_indices[1]))
 
-            # train on the sliding window for target word
-            train_cbow_pair(model, word, subwords_indices, l1, alpha, is_ft=True)
-        result += len(word_vocabs)
-    return result
+                train_cbow_pair(model, word, subwords_indices, l1, alpha, is_ft=True)  # train on the sliding window for target word
+            result += len(word_vocabs)
+        return result
 
+    def train_batch_sg(model, sentences, alpha, work=None, neu1=None):
+        result = 0
+        for sentence in sentences:
+            word_vocabs = [model.wv.vocab[w] for w in sentence if w in model.wv.vocab and
+                           model.wv.vocab[w].sample_int > model.random.rand() * 2**32]
+            for pos, word in enumerate(word_vocabs):
+                reduced_window = model.random.randint(model.window)  # `b` in the original word2vec code
+                # now go over all words from the (reduced) window, predicting each one in turn
+                start = max(0, pos - model.window + reduced_window)
 
-def train_batch_sg(model, sentences, alpha, work=None):
-    result = 0
-    for sentence in sentences:
-        word_vocabs = [model.wv.vocab[w] for w in sentence if w in model.wv.vocab and
-                       model.wv.vocab[w].sample_int > model.random.rand() * 2**32]
-        for pos, word in enumerate(word_vocabs):
-            reduced_window = model.random.randint(model.window)  # `b` in the original word2vec code
-            # now go over all words from the (reduced) window, predicting each one in turn
-            start = max(0, pos - model.window + reduced_window)
+                subwords_indices = [word.index]
+                word2_subwords = model.wv.ngrams_word[model.wv.index2word[word.index]]
 
-            subwords_indices = [word.index]
-            word2_subwords = model.wv.ngrams_word[model.wv.index2word[word.index]]
+                for subword in word2_subwords:
+                    subwords_indices.append(model.wv.ngrams[subword])
 
-            for subword in word2_subwords:
-                subwords_indices.append(model.wv.ngrams[subword])
+                for pos2, word2 in enumerate(word_vocabs[start:(pos + model.window + 1 - reduced_window)], start):
+                    if pos2 != pos:  # don't train on the `word` itself
+                        train_sg_pair(model, model.wv.index2word[word2.index], subwords_indices, alpha, is_ft=True)
 
-            for pos2, word2 in enumerate(word_vocabs[start:(pos + model.window + 1 - reduced_window)], start):
-                if pos2 != pos:  # don't train on the `word` itself
-                    train_sg_pair(model, model.wv.index2word[word2.index], subwords_indices, alpha, is_ft=True)
-
-        result += len(word_vocabs)
-    return result
+            result += len(word_vocabs)
+        return result
 
 
 class FastText(Word2Vec):
-    def __init__(self, sentences=None, sg=0, hs=0, size=100, alpha=0.025, window=5, min_count=5,
-                 max_vocab_size=None, word_ngrams=1, loss='ns', sample=1e-3, seed=1, workers=3, min_alpha=0.0001,
-                 negative=5, cbow_mean=1, hashfxn=hash, iter=5, null_word=0, min_n=3, max_n=6,
-                 sorted_vocab=1, bucket=2000000, trim_rule=None, batch_words=MAX_WORDS_IN_BATCH):
+    def __init__(
+            self, sentences=None, sg=0, hs=0, size=100, alpha=0.025, window=5, min_count=5,
+            max_vocab_size=None, word_ngrams=1, loss='ns', sample=1e-3, seed=1, workers=3, min_alpha=0.0001,
+            negative=5, cbow_mean=1, hashfxn=hash, iter=5, null_word=0, min_n=3, max_n=6, sorted_vocab=1, bucket=2000000,
+            trim_rule=None, batch_words=MAX_WORDS_IN_BATCH):
 
         # fastText specific params
         self.bucket = bucket
@@ -89,12 +97,10 @@ class FastText(Word2Vec):
         if self.word_ngrams <= 1 and self.max_n == 0:
             self.bucket = 0
 
-        super(FastText, self).__init__(
-            sentences=sentences, size=size, alpha=alpha, window=window, min_count=min_count,
+        super(FastText, self).__init__(sentences=sentences, size=size, alpha=alpha, window=window, min_count=min_count,
             max_vocab_size=max_vocab_size, sample=sample, seed=seed, workers=workers, min_alpha=min_alpha,
             sg=sg, hs=hs, negative=negative, cbow_mean=cbow_mean, hashfxn=hashfxn, iter=iter, null_word=null_word,
-            trim_rule=trim_rule, sorted_vocab=sorted_vocab, batch_words=batch_words
-        )
+            trim_rule=trim_rule, sorted_vocab=sorted_vocab, batch_words=batch_words)
 
     def initialize_word_vectors(self):
         self.wv = FastTextKeyedVectors()
@@ -104,16 +110,13 @@ class FastText(Word2Vec):
     def build_vocab(self, sentences, keep_raw_vocab=False, trim_rule=None, progress_per=10000, update=False):
         if update:
             if not len(self.wv.vocab):
-                raise RuntimeError(
-                    "You cannot do an online vocabulary-update of a model which has no prior vocabulary. "
-                    "First build the vocabulary of your model with a corpus before doing an online update."
-                )
+                raise RuntimeError("You cannot do an online vocabulary-update of a model which has no prior vocabulary. "
+                    "First build the vocabulary of your model with a corpus "
+                    "before doing an online update.")
             self.old_vocab_len = len(self.wv.vocab)
             self.old_hash2index_len = len(self.wv.hash2index)
 
-        super(FastText, self).build_vocab(
-            sentences, keep_raw_vocab=keep_raw_vocab, trim_rule=trim_rule, progress_per=progress_per, update=update
-        )
+        super(FastText, self).build_vocab(sentences, keep_raw_vocab=keep_raw_vocab, trim_rule=trim_rule, progress_per=progress_per, update=update)
         self.init_ngrams(update=update)
 
     def init_ngrams(self, update=False):
@@ -170,18 +173,10 @@ class FastText(Word2Vec):
 
             rand_obj = np.random
             rand_obj.seed(self.seed)
-            new_vocab_rows = rand_obj.uniform(
-                -1.0 / self.vector_size, 1.0 / self.vector_size,
-                (len(self.wv.vocab) - self.old_vocab_len, self.vector_size)
-            )
+            new_vocab_rows = rand_obj.uniform(-1.0 / self.vector_size, 1.0 / self.vector_size, (len(self.wv.vocab) - self.old_vocab_len, self.vector_size))
             new_vocab_lockf_rows = ones((len(self.wv.vocab) - self.old_vocab_len, self.vector_size), dtype=REAL)
-            new_ngram_rows = rand_obj.uniform(
-                -1.0 / self.vector_size, 1.0 / self.vector_size,
-                (len(self.wv.hash2index) - self.old_hash2index_len, self.vector_size)
-            )
-            new_ngram_lockf_rows = ones(
-                (len(self.wv.hash2index) - self.old_hash2index_len, self.vector_size), dtype=REAL
-            )
+            new_ngram_rows = rand_obj.uniform(-1.0 / self.vector_size, 1.0 / self.vector_size, (len(self.wv.hash2index) - self.old_hash2index_len, self.vector_size))
+            new_ngram_lockf_rows = ones((len(self.wv.hash2index) - self.old_hash2index_len, self.vector_size), dtype=REAL)
 
             self.wv.syn0_vocab = vstack([self.wv.syn0_vocab, new_vocab_rows])
             self.syn0_vocab_lockf = vstack([self.syn0_vocab_lockf, new_vocab_lockf_rows])
@@ -192,19 +187,15 @@ class FastText(Word2Vec):
         rand_obj = np.random
         rand_obj.seed(self.seed)
         for index in range(len(self.wv.vocab)):
-            self.wv.syn0_vocab[index] = rand_obj.uniform(
-                -1.0 / self.vector_size, 1.0 / self.vector_size, self.vector_size
-            )
+            self.wv.syn0_vocab[index] = rand_obj.uniform(-1.0 / self.vector_size, 1.0 / self.vector_size, self.vector_size)
         for index in range(len(self.wv.hash2index)):
-            self.wv.syn0_ngrams[index] = rand_obj.uniform(
-                -1.0 / self.vector_size, 1.0 / self.vector_size, self.vector_size
-            )
+            self.wv.syn0_ngrams[index] = rand_obj.uniform(-1.0 / self.vector_size, 1.0 / self.vector_size, self.vector_size)
 
     def _do_train_job(self, sentences, alpha, inits):
         work, neu1 = inits
         tally = 0
         if self.sg:
-            tally += train_batch_sg(self, sentences, alpha, work)
+            tally += train_batch_sg(self, sentences, alpha, work, neu1)
         else:
             tally += train_batch_cbow(self, sentences, alpha, work, neu1)
 
---
+++ gensim/models/fasttext_inner.pyx
@@ -0,0 +1,246 @@
+#!/usr/bin/env cython
+# coding: utf-8
+
+import cython
+import numpy as np
+cimport numpy as np
+
+from libc.math cimport exp
+from libc.math cimport log
+from libc.string cimport memset
+from libc.stdio cimport printf
+from libc.stdlib cimport calloc, free
+from gensim import matutils
+
+
+# scipy <= 0.15
+try:
+    from scipy.linalg.blas import fblas
+except ImportError:
+    # in scipy > 0.15, fblas function has been removed
+    import scipy.linalg.blas as fblas
+
+from word2vec_inner cimport bisect_left, random_int32, \
+     scopy, saxpy, sdot, dsdot, snrm2, sscal, \
+     REAL_t, EXP_TABLE, \
+     our_dot, our_saxpy, \
+     our_dot_double, our_dot_float, our_dot_noblas, our_saxpy_noblas
+
+REAL = np.float32
+
+DEF MAX_SENTENCE_LEN = 10000
+DEF MAX_SUBWORDS = 1000
+from word2vec import FAST_VERSION
+
+DEF EXP_TABLE_SIZE = 1000
+DEF MAX_EXP = 6
+
+cdef REAL_t[EXP_TABLE_SIZE] EXP_TABLE
+cdef REAL_t[EXP_TABLE_SIZE] LOG_TABLE
+
+cdef int ONE = 1
+cdef REAL_t ONEF = <REAL_t>1.0
+
+cdef unsigned long long fast_sentence_sg_neg(
+    const int negative, np.uint32_t *cum_table, unsigned long long cum_table_len,
+    REAL_t *syn0_vocab, REAL_t *syn0_ngrams, REAL_t *syn1neg, const int size,
+    const np.uint32_t word_index, const np.uint32_t *subwords_index, const np.uint32_t subwords_len,
+    const REAL_t alpha, REAL_t *work, REAL_t *l1, unsigned long long next_random, REAL_t *word_locks_vocab,
+    REAL_t *word_locks_ngrams) nogil:
+
+    cdef long long a
+    cdef np.uint32_t word2_index = subwords_index[0]
+    cdef long long row1 = word2_index * size, row2
+    cdef unsigned long long modulo = 281474976710655ULL
+    cdef REAL_t f, g, label, f_dot, log_e_f_dot
+    cdef np.uint32_t target_index
+    cdef int d
+
+    memset(work, 0, size * cython.sizeof(REAL_t))
+    memset(l1, 0, size * cython.sizeof(REAL_t))
+    # cdef REAL_t *l1
+    # l1 = <REAL_t *>calloc(size, cython.sizeof(REAL_t))
+    scopy(&size, &syn0_vocab[row1], &ONE, l1, &ONE)
+    for d in range(1, subwords_len):
+        subword_row = subwords_index[d] * size
+        our_saxpy(&size, &ONEF, &syn0_ngrams[subword_row], &ONE, l1, &ONE)
+    cdef REAL_t norm_factor = ONEF / subwords_len
+    sscal(&size, &norm_factor, l1 , &ONE)
+
+    for d in range(negative+1):
+        if d == 0:
+            target_index = word_index
+            label = ONEF
+        else:
+            target_index = bisect_left(cum_table, (next_random >> 16) % cum_table[cum_table_len-1], 0, cum_table_len)
+            next_random = (next_random * <unsigned long long>25214903917ULL + 11) & modulo
+            if target_index == word_index:
+                continue
+            label = <REAL_t>0.0
+
+        row2 = target_index * size
+        f_dot = our_dot(&size, l1, &ONE, &syn1neg[row2], &ONE)
+        if f_dot <= -MAX_EXP or f_dot >= MAX_EXP:
+            continue
+        f = EXP_TABLE[<int>((f_dot + MAX_EXP) * (EXP_TABLE_SIZE / MAX_EXP / 2))]
+        g = (label - f) * alpha
+        our_saxpy(&size, &g, &syn1neg[row2], &ONE, work, &ONE)
+        our_saxpy(&size, &g, l1, &ONE, &syn1neg[row2], &ONE)
+    our_saxpy(&size, &word_locks_vocab[word2_index], work, &ONE, &syn0_vocab[row1], &ONE)
+    for d in range(1, subwords_len):
+        our_saxpy(&size, &word_locks_ngrams[subwords_index[d]], work, &ONE, &syn0_ngrams[subwords_index[d]*size], &ONE)
+    return next_random
+
+
+def train_batch_sg(model, sentences, alpha, _work, _l1):
+    cdef int hs = model.hs
+    cdef int negative = model.negative
+    cdef int sample = (model.sample != 0)
+
+    cdef REAL_t *syn0_vocab = <REAL_t *>(np.PyArray_DATA(model.wv.syn0_vocab))
+    cdef REAL_t *word_locks_vocab = <REAL_t *>(np.PyArray_DATA(model.syn0_vocab_lockf))
+    cdef REAL_t *syn0_ngrams = <REAL_t *>(np.PyArray_DATA(model.wv.syn0_ngrams))
+    cdef REAL_t *word_locks_ngrams = <REAL_t *>(np.PyArray_DATA(model.syn0_ngrams_lockf))
+
+    cdef REAL_t *work
+    cdef REAL_t *l1
+    
+    cdef REAL_t _alpha = alpha
+    cdef int size = model.layer1_size
+
+    cdef int codelens[MAX_SENTENCE_LEN]
+    cdef np.uint32_t indexes[MAX_SENTENCE_LEN]
+    cdef np.uint32_t reduced_windows[MAX_SENTENCE_LEN]
+    cdef int sentence_idx[MAX_SENTENCE_LEN + 1]
+    cdef int window = model.window
+
+    cdef int i, j, k
+    cdef int effective_words = 0, effective_sentences = 0
+    cdef int sent_idx, idx_start, idx_end
+
+    # For negative sampling
+    cdef REAL_t *syn1neg
+    cdef np.uint32_t *cum_table
+    cdef unsigned long long cum_table_len
+    # for sampling (negative and frequent-word downsampling)
+    cdef unsigned long long next_random
+
+    # For passing subwords information as C objects for nogil
+    cdef np.uint32_t subwords_idx_len[MAX_SENTENCE_LEN]
+    cdef np.uint32_t subwords_idx[MAX_SENTENCE_LEN][MAX_SUBWORDS]
+
+
+    if negative:
+        syn1neg = <REAL_t *>(np.PyArray_DATA(model.syn1neg))
+        cum_table = <np.uint32_t *>(np.PyArray_DATA(model.cum_table))
+        cum_table_len = len(model.cum_table)
+    if negative or sample:
+        next_random = (2**24) * model.random.randint(0, 2**24) + model.random.randint(0, 2**24)
+
+    # convert Python structures to primitive types, so we can release the GIL
+    work = <REAL_t *>np.PyArray_DATA(_work)
+    l1 = <REAL_t *>np.PyArray_DATA(_l1)
+    # l1 = <REAL_t *>calloc(size, cython.sizeof(REAL_t))
+
+    # prepare C structures so we can go "full C" and release the Python GIL
+    vlookup = model.wv.vocab
+    sentence_idx[0] = 0  # indices of the first sentence always start at 0
+    for sent in sentences:
+        if not sent:
+            continue  # ignore empty sentences; leave effective_sentences unchanged
+        for token in sent:
+            word = vlookup[token] if token in vlookup else None
+            if word is None:
+                continue  # leaving `effective_words` unchanged = shortening the sentence = expanding the window
+            if sample and word.sample_int < random_int32(&next_random):
+                continue
+            indexes[effective_words] = word.index
+
+            effective_words += 1
+            if effective_words == MAX_SENTENCE_LEN:
+                break  # TODO: log warning, tally overflow?
+
+        # keep track of which words go into which sentence, so we don't train
+        # across sentence boundaries.
+        # indices of sentence number X are between <sentence_idx[X], sentence_idx[X])
+        effective_sentences += 1
+        sentence_idx[effective_sentences] = effective_words
+
+        if effective_words == MAX_SENTENCE_LEN:
+            break  # TODO: log warning, tally overflow?
+
+    # precompute "reduced window" offsets in a single randint() call
+    for i, item in enumerate(model.random.randint(0, window, effective_words)):
+        reduced_windows[i] = item
+
+    for i, word_idx in enumerate(indexes):
+        if i >= effective_words:
+            break
+        word_subwords = model.wv.ngrams_word[model.wv.index2word[word_idx]]
+        subwords_idx_len[i] = len(word_subwords) + 1
+        subwords_idx[i][0] = word_idx
+        for j, subword in enumerate(word_subwords):
+            subwords_idx[i][j+1] = model.wv.ngrams[subword]
+
+    with nogil:
+        for sent_idx in range(effective_sentences):
+            idx_start = sentence_idx[sent_idx]
+            idx_end = sentence_idx[sent_idx + 1]
+            for i in range(idx_start, idx_end):
+                j = i - window + reduced_windows[i]
+                if j < idx_start:
+                    j = idx_start
+                k = i + window + 1 - reduced_windows[i]
+                if k > idx_end:
+                    k = idx_end
+                for j in range(j, k):
+                    if j == i:
+                        continue
+                    if negative:
+                        next_random = fast_sentence_sg_neg(negative, cum_table, cum_table_len, syn0_vocab, syn0_ngrams, syn1neg, size, indexes[j], subwords_idx[i], subwords_idx_len[i], _alpha, work, l1, next_random, word_locks_vocab, word_locks_ngrams)
+
+    return effective_words
+
+
+def init():
+    """
+    Precompute function `sigmoid(x) = 1 / (1 + exp(-x))`, for x values discretized
+    into table EXP_TABLE.  Also calculate log(sigmoid(x)) into LOG_TABLE.
+    """
+    global our_dot
+    global our_saxpy
+
+    cdef int i
+    cdef float *x = [<float>10.0]
+    cdef float *y = [<float>0.01]
+    cdef float expected = <float>0.1
+    cdef int size = 1
+    cdef double d_res
+    cdef float *p_res
+
+    # build the sigmoid table
+    for i in range(EXP_TABLE_SIZE):
+        EXP_TABLE[i] = <REAL_t>exp((i / <REAL_t>EXP_TABLE_SIZE * 2 - 1) * MAX_EXP)
+        EXP_TABLE[i] = <REAL_t>(EXP_TABLE[i] / (EXP_TABLE[i] + 1))
+        LOG_TABLE[i] = <REAL_t>log( EXP_TABLE[i] )
+
+    # check whether sdot returns double or float
+    d_res = dsdot(&size, x, &ONE, y, &ONE)
+    p_res = <float *>&d_res
+    if (abs(d_res - expected) < 0.0001):
+        our_dot = our_dot_double
+        our_saxpy = saxpy
+        return 0  # double
+    elif (abs(p_res[0] - expected) < 0.0001):
+        our_dot = our_dot_float
+        our_saxpy = saxpy
+        return 1  # float
+    else:
+        # neither => use cython loops, no BLAS
+        # actually, the BLAS is so messed up we'll probably have segfaulted above and never even reach here
+        our_dot = our_dot_noblas
+        our_saxpy = our_saxpy_noblas
+        return 2
+
+FAST_VERSION = init()  # initialize the module
+MAX_WORDS_IN_BATCH = MAX_SENTENCE_LEN
