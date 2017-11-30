2c4a25f20fe6c4e5f98ac5d0d04450af34e6e6c9
prerna135 <prnkshp@gmail.com>
2017-11-30 20:33:49 +0530

    Fixing bugs

--- gensim/models/sent2vec_inner.pyx
+++ gensim/models/sent2vec_inner.pyx
@@ -10,6 +10,7 @@ from numpy import zeros, float32 as REAL
 cimport numpy as np
 from libc.math cimport log, exp
 from libc.string cimport memset
+from libc.stdlib cimport calloc, free
 
 # scipy <= 0.15
 try:
@@ -30,27 +31,27 @@ cdef int ONE = 1
 cdef float ONEF = <float>1.0
 
 
-cdef float negative_sampling(const int target, const float lr, float *grad, float *wo, float *hidden,
+cdef (float, int) negative_sampling(const int target, const float lr, float *grad, float *wo, float *hidden,
                               const int vector_size, const int neg, int *negatives,
-                              int *negpos, const int negatives_len)nogil:
+                              int negpos, const int negatives_len):
 
     cdef float loss
     loss = <float> 0.0
     cdef int label_true, label_false
     label_true = <int> 1
     label_false = <int> 0
-    cdef int temp
+    cdef int temp, new_negpos
     for i from 0 <= i <= neg:
         if i == 0:
             loss += binary_logistic(target, label_true, lr, vector_size, wo, grad, hidden)
         else:
-            temp = get_negative(target, negatives, negpos, negatives_len)
+            temp, new_negpos = get_negative(target, negatives, negpos, negatives_len)
             loss += binary_logistic(temp, label_false, lr, vector_size, wo, grad, hidden)
-    return loss
+    return loss, new_negpos
 
 
 cdef float binary_logistic(const int target, const int label, const float lr,
-                            const int vector_size, float *wo, float *grad, float *hidden)nogil:
+                            const int vector_size, float *wo, float *grad, float *hidden):
 
     cdef float score = exp(our_dot(&vector_size, &wo[<int>target], &ONE, hidden, &ONE))
     cdef float alpha = lr * (<float>label - score)
@@ -64,37 +65,40 @@ cdef float binary_logistic(const int target, const int label, const float lr,
         return -log(ONEF - score)
     
 
-cdef int get_negative(const int target, int *negatives,
-                              int *negpos, const int negatives_len)nogil:
+cdef (int, int) get_negative(const int target, int *negatives,
+                              int negpos, const int negatives_len):
 
     cdef int negative
     while True:
-        negative = negatives[<int>negpos[0]]
-        negpos[0] = (negpos[0] + <int>ONE) % negatives_len
+        negative = negatives[<int>negpos]
+        negpos = (negpos + <int>ONE) % negatives_len
         if target != negative:
             break
-    return negative
+    return negative, negpos
 
 
 def update(model, context_, target_, lr_):
 
-    cdef float loss = <float> model.loss
+    cdef float loss_
     cdef float lr = <float> lr_
     cdef int vector_size = <int> model.vector_size
-    cdef int *context = <int *>np.PyArray_DATA(np.array(context_))
+    cdef int *context = <int *>np.PyArray_DATA(context_)
     cdef int target = <int> target_
-    cdef int *negatives = <int *>np.PyArray_DATA(np.array(model.negatives))
-    cdef int negpos = <int> model.negpos
+    cdef int *negatives = <int *>np.PyArray_DATA(model.negatives)
+    cdef int negpos = <int>model.negpos
     cdef int neg = <int> model.neg
-    cdef float *wi = <float *>np.PyArray_DATA(model.wi)
-    cdef float *wo = <float *>np.PyArray_DATA(model.wo)
+    cdef REAL_t *wi = <REAL_t *>np.PyArray_DATA(model.wi)
+    cdef REAL_t *wo = <REAL_t *>np.PyArray_DATA(model.wo)
     cdef int negatives_len = <int>len(model.negatives)
     cdef int context_len = <int>len(context_)
 
-    hidden_ = zeros(vector_size, dtype=REAL)
-    grad_ = zeros(vector_size, dtype=REAL)
-    cdef float *hidden = <float *>np.PyArray_DATA(hidden_)
-    cdef float *grad = <float *>np.PyArray_DATA(grad_)
+    #hidden_ = zeros(vector_size, dtype=REAL)
+    #grad_ = zeros(vector_size, dtype=REAL)
+    #cdef float *hidden = <float *>np.PyArray_DATA(hidden_)
+    #cdef float *grad = <float *>np.PyArray_DATA(grad_)
+    cdef REAL_t *hidden, *grad
+    hidden = <REAL_t *>calloc(vector_size, cython.sizeof(REAL_t))
+    grad = <REAL_t *>calloc(vector_size, cython.sizeof(REAL_t))
     
     cdef np.int32_t i
     for i from 0 <= i < context_len:
@@ -102,8 +106,12 @@ def update(model, context_, target_, lr_):
         
     cdef float alpha = ONEF / <float>(len(context_))
     sscal(&vector_size, &alpha, hidden, &ONE)
-    loss += negative_sampling(target, lr, grad, wo, hidden, vector_size, neg, negatives, &negpos, negatives_len)
+    loss_ , negpos = negative_sampling(target, lr, grad, wo, hidden, vector_size, neg, negatives, negpos, negatives_len)
     model.nexamples += 1
     sscal(&vector_size, &alpha, grad, &ONE)
     for i from 0 <= i < context_len:
         our_saxpy(&vector_size, &ONEF, grad, &ONE, &wi[i], &ONE)
+    model.loss += loss_
+    model.negpos = negpos
+    free(hidden)
+    free(grad)
