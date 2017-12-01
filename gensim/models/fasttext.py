9dafbd552d8e3ed563510805cf8d3ff4b750871a
manneshiva <manneshiva+github@gmail.com>
2017-12-01 13:58:11 +0530

    fixes indentation, unused imports and logging warning for slow version

--- gensim/models/fasttext.py
+++ gensim/models/fasttext.py
@@ -9,16 +9,17 @@ from numpy import zeros, ones, vstack, sum as np_sum, empty, float32 as REAL
 from gensim.models.word2vec import Word2Vec, train_sg_pair, train_cbow_pair
 from gensim.models.wrappers.fasttext import FastTextKeyedVectors
 from gensim.models.wrappers.fasttext import FastText as Ft_Wrapper, compute_ngrams, ft_hash
-from gensim import matutils
 
 logger = logging.getLogger(__name__)
 
 try:
     from gensim.models.fasttext_inner import train_batch_sg, train_batch_cbow
     from gensim.models.fasttext_inner import FAST_VERSION, MAX_WORDS_IN_BATCH
+    logger.debug('Fast version of Fasttext is being used')
 
 except ImportError:
     # failed... fall back to plain numpy (20-80x slower training than the above)
+    logger.warning('Slow version of Fasttext is being used')
     FAST_VERSION = -1
     MAX_WORDS_IN_BATCH = 10000
 
@@ -83,7 +84,7 @@ except ImportError:
 class FastText(Word2Vec):
     def __init__(
             self, sentences=None, sg=0, hs=0, size=100, alpha=0.025, window=5, min_count=5,
-            max_vocab_size=None, word_ngrams=1, loss='ns', sample=1e-3, seed=1, workers=3, min_alpha=0.0001,
+            max_vocab_size=None, word_ngrams=1, sample=1e-3, seed=1, workers=3, min_alpha=0.0001,
             negative=5, cbow_mean=1, hashfxn=hash, iter=5, null_word=0, min_n=3, max_n=6, sorted_vocab=1, bucket=2000000,
             trim_rule=None, batch_words=MAX_WORDS_IN_BATCH):
 
@@ -95,7 +96,8 @@ class FastText(Word2Vec):
         if self.word_ngrams <= 1 and self.max_n == 0:
             self.bucket = 0
 
-        super(FastText, self).__init__(sentences=sentences, size=size, alpha=alpha, window=window, min_count=min_count,
+        super(FastText, self).__init__(
+            sentences=sentences, size=size, alpha=alpha, window=window, min_count=min_count,
             max_vocab_size=max_vocab_size, sample=sample, seed=seed, workers=workers, min_alpha=min_alpha,
             sg=sg, hs=hs, negative=negative, cbow_mean=cbow_mean, hashfxn=hashfxn, iter=iter, null_word=null_word,
             trim_rule=trim_rule, sorted_vocab=sorted_vocab, batch_words=batch_words)
@@ -108,13 +110,15 @@ class FastText(Word2Vec):
     def build_vocab(self, sentences, keep_raw_vocab=False, trim_rule=None, progress_per=10000, update=False):
         if update:
             if not len(self.wv.vocab):
-                raise RuntimeError("You cannot do an online vocabulary-update of a model which has no prior vocabulary. "
+                raise RuntimeError(
+                    "You cannot do an online vocabulary-update of a model which has no prior vocabulary. "
                     "First build the vocabulary of your model with a corpus "
                     "before doing an online update.")
             self.old_vocab_len = len(self.wv.vocab)
             self.old_hash2index_len = len(self.wv.hash2index)
 
-        super(FastText, self).build_vocab(sentences, keep_raw_vocab=keep_raw_vocab, trim_rule=trim_rule, progress_per=progress_per, update=update)
+        super(FastText, self).build_vocab(
+            sentences, keep_raw_vocab=keep_raw_vocab, trim_rule=trim_rule, progress_per=progress_per, update=update)
         self.init_ngrams(update=update)
 
     def init_ngrams(self, update=False):
@@ -171,10 +175,21 @@ class FastText(Word2Vec):
 
             rand_obj = np.random
             rand_obj.seed(self.seed)
-            new_vocab_rows = rand_obj.uniform(-1.0 / self.vector_size, 1.0 / self.vector_size, (len(self.wv.vocab) - self.old_vocab_len, self.vector_size))
-            new_vocab_lockf_rows = ones((len(self.wv.vocab) - self.old_vocab_len, self.vector_size), dtype=REAL)
-            new_ngram_rows = rand_obj.uniform(-1.0 / self.vector_size, 1.0 / self.vector_size, (len(self.wv.hash2index) - self.old_hash2index_len, self.vector_size))
-            new_ngram_lockf_rows = ones((len(self.wv.hash2index) - self.old_hash2index_len, self.vector_size), dtype=REAL)
+            new_vocab_rows = rand_obj.uniform(
+                -1.0 / self.vector_size, 1.0 / self.vector_size,
+                (len(self.wv.vocab) - self.old_vocab_len, self.vector_size))
+            new_vocab_lockf_rows = ones(
+                (len(self.wv.vocab) - self.old_vocab_len,
+                self.vector_size),
+                dtype=REAL)
+            new_ngram_rows = rand_obj.uniform(
+                -1.0 / self.vector_size, 1.0 / self.vector_size,
+                (len(self.wv.hash2index) - self.old_hash2index_len,
+                self.vector_size))
+            new_ngram_lockf_rows = ones(
+                (len(self.wv.hash2index) - self.old_hash2index_len,
+                self.vector_size),
+                dtype=REAL)
 
             self.wv.syn0_vocab = vstack([self.wv.syn0_vocab, new_vocab_rows])
             self.syn0_vocab_lockf = vstack([self.syn0_vocab_lockf, new_vocab_lockf_rows])
