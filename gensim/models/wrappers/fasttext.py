1412f577d5cdb2102ea53b44f237e69852d793e2
manneshiva <manneshiva+github@gmail.com>
2017-12-04 14:05:20 +0530

    raises error when calling `load_word2vec_format`

--- gensim/models/wrappers/fasttext.py
+++ gensim/models/wrappers/fasttext.py
@@ -137,6 +137,12 @@ class FastTextKeyedVectors(KeyedVectors):
             char_ngrams = compute_ngrams(word, self.min_n, self.max_n)
             return any(ng in self.ngrams for ng in char_ngrams)
 
+    @classmethod
+    def load_word2vec_format(cls, fname, fvocab=None, binary=False, encoding='utf8', unicode_errors='strict',
+                             limit=None, datatype=REAL):
+        """Not suppported. Use gensim.models.KeyedVectors.load_word2vec_format instead."""
+        raise NotImplementedError("Not supported. Use gensim.models.KeyedVectors.load_word2vec_format instead.")
+
 
 class FastText(Word2Vec):
     """
