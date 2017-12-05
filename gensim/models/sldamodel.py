c553b52a6d18090cc682c9c48593b694cfd486ea
Sourav <ssouravsingh12@gmail.com>
2017-12-05 20:43:17 +0530

    Remove sLDA state class

--- gensim/models/sldamodel.py
+++ gensim/models/sldamodel.py
@@ -12,6 +12,7 @@ from collections import defaultdict
 from gensim import interfaces, utils, matutils
 from gensim.matutils import dirichlet_expectation
 from gensim.models import basemodel, CoherenceModel
+from gensim.models.ldamodel import LdaState as state
 
 try:
     from scipy.special import logsumexp
@@ -42,82 +43,6 @@ def update_dir_prior(prior, N, logphat, rho):
         logger.warning("updated prior not positive")
 
     return prior
-
-class LdaState(utils.SaveLoad):
-    """
-    Encapsulate information for distributed computation of LdaModel objects.
-    Objects of this class are sent over the network, so try to keep them lean to
-    reduce traffic.
-    """
-
-    def __init__(self, eta, shape, dtype=np.float32):
-        self.eta = eta.astype(dtype, copy=False)
-        self.sstats = np.zeros(shape, dtype=dtype)
-        self.numdocs = 0
-        self.dtype = dtype
-
-    def reset(self):
-        self.sstats[:] = 0.0
-        self.numdocs = 0
-
-    def merge(self, other):
-        """
-        Merge the result of an E step from one node with that of another node
-        (summing up sufficient statistics).
-        The merging is trivial and after merging all cluster nodes, we have the
-        exact same result as if the computation was run on a single node (no
-        approximation).
-        """
-        self.sstats += other.sstats
-        self.numdocs += other.numdocs
-
-    def blend(self, rhot, other, targetsize=None):
-        """
-        Given sLdaState `other`, merge it with the current state. Stretch both to
-        `targetsize` documents before merging, so that they are of comparable
-        magnitude.
-        Merging is done by average weighting: in the extremes, `rhot=0.0` means
-        `other` is completely ignored; `rhot=1.0` means `self` is completely ignored.
-        This procedure corresponds to the stochastic gradient update from Hoffman
-        et al., algorithm 2 (eq. 14).
-        """
-        assert other is not None
-        if targetsize is None:
-            targetsize = self.numdocs
-
-        # stretch the current model's expected n*phi counts to target size
-        if self.numdocs == 0 or targetsize == self.numdocs:
-            scale = 1.0
-        else:
-            scale = 1.0 * targetsize / self.numdocs
-        self.sstats *= (1.0 - rhot) * scale
-
-        # stretch the incoming n*phi counts to target size
-        if other.numdocs == 0 or targetsize == other.numdocs:
-            scale = 1.0
-        else:
-            logger.info("merging changes from %i documents into a model of %i documents", other.numdocs, targetsize)
-            scale = 1.0 * targetsize / other.numdocs
-        self.sstats += rhot * scale * other.sstats
-
-        self.numdocs = targetsize
-
-    def blend2(self, rhot, other, targetsize=None):
-        """
-        Alternative, more simple blend.
-        """
-        assert other is not None
-        if targetsize is None:
-            targetsize = self.numdocs
-
-        self.sstats += other.sstats
-        self.numdocs = targetsize
-
-    def get_lambda(self):
-        return self.eta + self.sstats
-
-    def get_Elogbeta(self):
-        return dirichlet_expectation(self.get_lambda())
 
 class SLdaModel(interfaces.TransformationABC, basemodel.BaseTopicModel):
 
@@ -226,7 +151,7 @@ def init_dir_prior(self, prior, name):
 
 
     def sync_state(self):
-        self.expElogbeta = np.exp(self.state.get_Elogbeta())
+        self.expElogbeta = np.exp(state.get_Elogbeta())
         assert self.expElogbeta.dtype == self.dtype
 
     def clear(self):
@@ -253,15 +178,15 @@ def init_dir_prior(self, prior, name):
     def do_mstep(self, rho, other, extra_pass=False):
         logger.debug("updating topics")
         diff = np.log(self.expElogbeta)
-        self.state.blend(rho, other)
-        diff -= self.state.get_Elogbeta()
+        state.blend(rho, other)
+        diff -= state.get_Elogbeta()
         self.sync_state()
 
         self.print_topics(5)
         logger.info("topic diff=%f, rho=%f", np.mean(np.abs(diff)), rho)
 
         if self.optimize_eta:
-            self.update_eta(self.state.get_lambda(), rho)
+            self.update_eta(state.get_lambda(), rho)
 
         if not extra_pass:
             self.num_updates += other.numdocs
