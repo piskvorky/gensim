7682b4c3668a73a40231a5085398a4953498f0c0
Andy Craze <accraze@gmail.com>
2017-12-04 20:49:08 -0800

    returning correct dtype from unitvec
    matutils.unitvec currently returns a unitvector of a different dtype from the input vector if the input dtype isn't np.float.
    
    we should make the return type consistent with the input type.
    
    fixes #1722

---
+++ gensim/test/test_matutils.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU LGPL v2.1 - http://www.gnu.org/licenses/lgpl.html
+
+"""
+Automated tests for checking various matutils functions.
+"""
+
+import logging
+import unittest
+
+import numpy as np
+
+from gensim import matutils
+
+
+class TestMatutils(unittest.TestCase):
+    def test_unitvec(self):
+        input_vector = np.random.uniform(size=(100,)).astype(np.float32)
+        unit_vector = matutils.unitvec(input_vector)
+        self.assertEqual(input_vector.dtype, unit_vector.dtype)
+        
+
+if __name__ == '__main__':
+    logging.root.setLevel(logging.WARNING)
+    unittest.main()
