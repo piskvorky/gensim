d7209f4ea82ddc19b6a49d7ca7871c45f4e44397
manneshiva <manneshiva+github@gmail.com>
2017-12-13 10:26:24 +0530

    adds public interfaces

--- gensim/models/base_any2vec.py
+++ gensim/models/base_any2vec.py
@@ -2,71 +2,157 @@
 # -*- coding: utf-8 -*-
 
 from abc import ABCMeta, abstractmethod
+from gensim import utils
 
 
-class BaseVocabBuilder(object):
-    """Base class to handle building and updating vocabulary (of any entity)."""
+# Public Interfaces
+class BaseAny2VecModel(utils.SaveLoad):
 
-    __metaclass__ = ABCMeta
+    def __init__(self, data_iterator, **kwargs):
+        """Initialize model parameters."""
+        raise NotImplementedError
 
-    @abstractmethod
-    def scan_vocab(self, entites, **kwargs):
-        """Do an initial scan of all entities appearing in iterator."""
-        pass
+    def train(self, data_iterator, **kwargs):
+        """Handles multiworker training."""
+        raise NotImplementedError
 
-    def reset_weights(self):
-        """Reset all projection weights to an initial (untrained) state, but keep the existing vocabulary."""
-        return
+    def build_vocab(self, data_iterator, **kwargs):
+        """Scan through the vocab and create/update vocabulary.
+        Should also initialize/reset vevtors for new vocab entities."""
+        raise NotImplementedError
 
-    def update_weights(self):
-        """Copy all the existing weights, and reset the weights for the newly added vocabulary."""
-        return
+    @classmethod
+    def load(cls, *args, **kwargs):
+        model = super(BaseAny2VecModel, cls).load(*args, **kwargs)
+        return model
 
-    def sort_vocab(self):
-        """Sort the vocabulary so the most frequent words have the lowest indexes."""
-        return
+    def save(self, *args, **kwargs):
+        super(BaseAny2VecModel, self).save(*args, **kwargs)
 
-    def scale_vocab(self, **kwargs):
-        """Apply vocabulary settings for `min_count` (discarding less-frequent entities)
-        and `sample` (controlling the downsampling of more-frequent entities).
+
+class BaseKeyedVectors(utils.SaveLoad):
+
+    def save(self, *args, **kwargs):
+        super(BaseKeyedVectors, self).save(*args, **kwargs)
+
+    @classmethod
+    def load(cls, *args, **kwargs):
+        return super(BaseKeyedVectors, cls).load(*args, **kwargs)
+
+    def similarity(self, e1, e2):
         """
-        return
+        Compute similarity between vectors of two input entities (words, documents, sentences etc.).
+        To be implemented by child class.
+        """
+        raise NotImplementedError
 
-    def finalize_vocab(self, **kwargs):
-        """Initialize model weights based on final vocabulary settings."""
-        return
+    def most_similar(self, *args, **kwargs):
+        return NotImplementedError
 
-    def build_vocab(self, entites, **kwargs):
-        """Build vocabulary. Internally calls `scan_vocab`, `scale_vocab` and `finalize_vocab`."""
-        return
+    def distance(self, e1, e2):
+        """
+        Compute distance between vectors of two input words.
+        To be implemented by child class.
+        """
+        raise NotImplementedError
+
+    def distances(self, entity_or_vector, other_entities=()):
+        """
+        Compute distances from given entity or vector to all words in `other_entity`.
+        If `other_entities` is empty, return distance between `entity_or_vectors` and all entities in vocab.
+        To be implemented by child class.
+        """
+        raise NotImplementedError
+
+    def get_vector(self, entity):
+        """
+        Accept a single entity as input.
+        Returns the word's representations in vector space, as a 1D numpy array.
+        """
+        raise NotImplementedError
+
+    def most_similar_to_given(self, e1, entities_list):
+        """Return the entity from entities_list most similar to e1."""
+        raise NotImplementedError
+
+    def words_closer_than(self, e1, e2):
+        """
+        Returns all words that are closer to `w1` than `w2` is to `w1`.
+        """
+        raise NotImplementedError
+
+    def rank(self, e1, e2):
+        """
+        Rank of the distance of `w2` from `w1`, in relation to distances of all words from `w1`.
+        """
+        raise NotImplementedError
+
+# class BaseVocabBuilder(object):
+#     """Base class to handle building and updating vocabulary (of any entity)."""
+
+#     __metaclass__ = ABCMeta
+
+#     @abstractmethod
+#     def scan_vocab(self, entites, **kwargs):
+#         """Do an initial scan of all entities appearing in iterator."""
+#         pass
+
+#     def reset_weights(self):
+#         """Reset all projection weights to an initial (untrained) state, but keep the existing vocabulary."""
+#         return
+
+#     def update_weights(self):
+#         """Copy all the existing weights, and reset the weights for the newly added vocabulary."""
+#         return
+
+#     def sort_vocab(self):
+#         """Sort the vocabulary so the most frequent words have the lowest indexes."""
+#         return
+
+#     def scale_vocab(self, **kwargs):
+#         """Apply vocabulary settings for `min_count` (discarding less-frequent entities)
+#         and `sample` (controlling the downsampling of more-frequent entities).
+#         """
+#         return
+
+#     def finalize_vocab(self, **kwargs):
+#         """Initialize model weights based on final vocabulary settings."""
+#         return
+
+#     def build_vocab(self, entites, **kwargs):
+#         """Build vocabulary. Internally calls `scan_vocab`, `scale_vocab` and `finalize_vocab`."""
+#         return
 
 
-class BaseVectorTrainer(object):
-    """Base class for training any2vec model. This handles feeding data into queues and
-    multi workers learning of vectors.
-    """
-    __metaclass__ = ABCMeta
+# class BaseVectorTrainer(object):
+#     """Base class for training any2vec model. This handles feeding data into queues and
+#     multi workers learning of vectors.
+#     """
+#     __metaclass__ = ABCMeta
 
-    @abstractmethod
-    def _do_train_job(self, *args):
-        """Train single batch of entities."""
-        return
+#     @abstractmethod
+#     def _do_train_job(self, *args):
+#         """Train single batch of entities."""
+#         return
 
-    def train(self, *ars, **kwargs):
-        """Provide implementation for learning vectors using job producer and `worker_loop`."""
-        return
+#     def train(self, *ars, **kwargs):
+#         """Provide implementation for learning vectors using job producer and `worker_loop`."""
+#         return
 
 
-class BaseWord2VecTypeModel(BaseVocabBuilder, BaseVectorTrainer):
-    """Base Class for all "Word2Vec like" algorithms -- which use sg, cbow architecture with neg/hs loss.
-    Should contain all repeated/reused code for presently implemented algorithms -- Doc2Vec, FastText, Word2Vec.
-    """
-    def __init__(self, *args, **kwargs):
-        """Initialize common parameters."""
-        return
+# class BaseWord2VecTypeModel(BaseVocabBuilder, BaseVectorTrainer):
+#     """Base Class for all "Word2Vec like" algorithms -- which use sg, cbow architecture with neg/hs loss.
+#     Should contain all repeated/reused code for presently implemented algorithms -- Doc2Vec, FastText, Word2Vec.
+#     """
+#     def __init__(self, *args, **kwargs):
+#         """Initialize common parameters."""
+#         return
 
-    def train_sg_pair(*args, **kwagrs):
-        return
+#     def train_sg_pair(*args, **kwagrs):
+#         return
 
-    def train_cbow_pair(*args, **kwagrs):
-        return
+#     def train_cbow_pair(*args, **kwagrs):
+#         return
+
+
+
