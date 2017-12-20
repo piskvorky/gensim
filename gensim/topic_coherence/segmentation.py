8a07deeb8bb2626ca6b8048b7363a55c693bddb5
ivan <menshikh.iv@gmail.com>
2017-12-20 05:52:14 +0500

    fix segmentation

--- gensim/topic_coherence/segmentation.py
+++ gensim/topic_coherence/segmentation.py
@@ -12,31 +12,31 @@ logger = logging.getLogger(__name__)
 
 
 def s_one_pre(topics):
-    """Performs s_one_pre segmentation on a list of topics.
+    """Performs segmentation on a list of topics.
 
     Notes
     -----
-    s_one_pre segmentation is defined as
-    :math:`s_{pre} = {(W', W^{*}) | W' = w_{i}; W^{*} = {w_j}; w_{i}, w_{j} \in W; i > j}`
+    Segmentation is defined as
+    :math:`s_{pre} = {(W', W^{*}) | W' = w_{i}; W^{*} = {w_j}; w_{i}, w_{j} \in W; i > j}`.
 
     Parameters
     ----------
     topics : list of np.array
-        list of topics obtained from an algorithm such as LDA. Is a list such as
-        [array([ 9, 10, 11]), array([ 9, 10,  7]), ...]
+        list of topics obtained from an algorithm such as LDA.
 
     Returns
     -------
-    list of list of (str, str).
+    list of list of (int, int)
         :math:`(W', W^{*})` for all unique topic ids.
 
     Examples
     --------
     >>> import numpy as np
     >>> from gensim.topic_coherence import segmentation
+    >>>
     >>> topics = [np.array([1, 2, 3]), np.array([4, 5, 6])]
-    >>> # should be [[(2, 1), (3, 1), (3, 2)], [(5, 4), (6, 4), (6, 5)]]
     >>> segmentation.s_one_pre(topics)
+    [[(2, 1), (3, 1), (3, 2)], [(5, 4), (6, 4), (6, 5)]]
 
     """
     s_one_pre_res = []
@@ -52,28 +52,29 @@ def s_one_pre(topics):
 
 
 def s_one_one(topics):
-    """Perform s_one_one segmentation on a list of topics.
-    s_one_one segmentation is defined as
-    :math:`s_{one} = {(W', W^{*}) | W' = {w_i}; W^{*} = {w_j}; w_{i}, w_{j} \in W; i != j}` #TODO: neq - doesn't work
+    """Perform segmentation on a list of topics.
+    Segmentation is defined as
+    :math:`s_{one} = {(W', W^{*}) | W' = {w_i}; W^{*} = {w_j}; w_{i}, w_{j} \in W; i \\neq j}`.
 
     Parameters
     ----------
-    topics : list of np.array
+    topics : list of `numpy.ndarray`
         List of topics obtained from an algorithm such as LDA.
-        Is a list such as [array([ 9, 10, 11]), array([ 9, 10,  7]), ...].
 
     Returns
     -------
-    list of list of (str, str).
+    list of list of (int, int).
         :math:`(W', W^{*})` for all unique topic ids.
 
     Examples
     -------
     >>> import numpy as np
     >>> from gensim.topic_coherence import segmentation
+    >>>
     >>> topics = [np.array([1, 2, 3]), np.array([4, 5, 6])]
-    >>> # should be [[(1, 2), (1, 3), (2, 1), (2, 3), (3, 1), (3, 2)], [(4, 5), (4, 6), (5, 4), (5, 6), (6, 4), (6, 5)]]
-    >>> segmentation.s_one_pre(topics)
+    >>> segmentation.s_one_one(topics)
+    [[(1, 2), (1, 3), (2, 1), (2, 3), (3, 1), (3, 2)], [(4, 5), (4, 6), (5, 4), (5, 6), (6, 4), (6, 5)]]
+
     """
     s_one_one_res = []
 
@@ -92,26 +93,27 @@ def s_one_one(topics):
 
 def s_one_set(topics):
     """Perform s_one_set segmentation on a list of topics.
-    s_one_set segmentation is defined as :math:`s_{set} = {(W', W^{*}) | W' = {w_i}; w_{i} \in W; W^{*} = W}`
+    Segmentation is defined as
+    :math:`s_{set} = {(W', W^{*}) | W' = {w_i}; w_{i} \in W; W^{*} = W}`
 
     Parameters
     ----------
-    topics : list of np.array
-        List of topics obtained from an algorithm such as LDA. Is a list such as
-        [array([ 9, 10, 11]), array([ 9, 10,  7]), ...].
+    topics : list of `numpy.ndarray`
+        List of topics obtained from an algorithm such as LDA.
 
     Returns
     -------
-    list of list of (str, str).
+    list of list of (int, int).
         :math:`(W', W^{*})` for all unique topic ids.
 
     Examples
     --------
     >>> import numpy as np
     >>> from gensim.topic_coherence import segmentation
+    >>>
     >>> topics = [np.array([9, 10, 7])]
-    >>> # should be [[(9, array([ 9, 10,  7])), (10, array([ 9, 10,  7])), (7, array([ 9, 10,  7]))]]
     >>> segmentation.s_one_set(topics)
+    [[(9, array([ 9, 10,  7])), (10, array([ 9, 10,  7])), (7, array([ 9, 10,  7]))]]
 
     """
     s_one_set_res = []
