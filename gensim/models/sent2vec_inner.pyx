59c1cc6ff14213e9cbd8c1bb6d19b36a6fadf292
Prerna Kashyap <prnkshp@gmail.com>
2017-12-20 20:12:42 +0530

    Updating sent2vec_inner.pyx

--- gensim/models/sent2vec_inner.pyx
+++ gensim/models/sent2vec_inner.pyx
@@ -8,9 +8,8 @@ import cython
 import numpy as np
 from numpy import zeros, float32 as REAL
 cimport numpy as np
-#from libc.math cimport log, exp
-from libc.string cimport memset
-from libc.stdlib cimport calloc, free
+from libc.stdlib cimport rand, RAND_MAX, srand
+from libcpp.vector cimport vector
 
 # scipy <= 0.15
 try:
@@ -36,8 +35,7 @@ cdef float negative_sampling(const int target, const float lr, float *grad, floa
                               const int vector_size, const int neg, int *negatives,
                               int *negpos, const int negatives_len)nogil:
 
-    cdef float loss
-    loss = <float> 0.0
+    cdef float loss = <float> 0.0
     cdef int label_true, label_false
     label_true = <int> 1
     label_false = <int> 0
@@ -52,6 +50,7 @@ cdef float negative_sampling(const int target, const float lr, float *grad, floa
 
 
 cdef float sigmoid(const float val)nogil:
+
     if val < -MAX_EXP:
         return 0.0
     elif val > MAX_EXP:
@@ -61,6 +60,7 @@ cdef float sigmoid(const float val)nogil:
     
 
 cdef float log(const float val)nogil:
+
     if val > 1.0:
         return 0.0
     else:
@@ -93,43 +93,146 @@ cdef int get_negative(const int target, int *negatives,
     return negative
 
 
-def update(model, context_, target_, lr_, hidden_, grad_):
+cdef float update(vector[int] &context, int target, float lr, REAL_t *hidden, REAL_t *grad,
+                  int vector_size, int *negpos, int neg, int negatives_len,
+                  REAL_t *wi, REAL_t *wo, int *negatives)nogil:
 
+    cdef float alpha = ONEF / <float>(context.size())
     cdef float loss
+    cdef int i
+
+    for i from 0 <= i < context.size():
+        our_saxpy(&vector_size, &ONEF, &wi[context[i]], &ONE, hidden, &ONE)
+    sscal(&vector_size, &alpha, hidden, &ONE)
+    loss = negative_sampling(target, lr, grad, wo, hidden, vector_size, neg, negatives, negpos, negatives_len)
+    sscal(&vector_size, &alpha, grad, &ONE)
+    for i from 0 <= i < context.size():
+        our_saxpy(&vector_size, &ONEF, grad, &ONE, &wi[context[i]], &ONE)
+    return loss
+
+
+cdef float random_uniform()nogil:
+
+    return <float> (rand()) / <float> (RAND_MAX)
+
+
+cdef int random_range(int a, int b)nogil:
+
+    return a + <int>(rand() / (RAND_MAX * b))
+
+
+cdef int get_line(vector[int] &wids, vector[int] &words, int max_line_size)nogil:
+
+    cdef int ntokens = <int> 0
+    cdef int i
+
+    for i from 0 <= i < wids.size():
+        if wids[i] < 0:
+            continue
+        ntokens += 1
+        words.push_back(wids[i])
+        if ntokens > max_line_size:
+            break
+    return ntokens
+
+
+cdef void add_ngrams_train(vector[int] &line, int n, int k, int bucket, int size)nogil:
+
+    cdef int num_discarded = 0
+    cdef vector[int] discard
+    cdef int line_size = line.size()
+    cdef int token_to_discard
+    cdef int i, j, h
+
+    for i from 0<= i < line.size():
+        discard.push_back(0)
+
+    while num_discarded < k and line_size - num_discarded > 2:
+        token_to_discard = random_range(0, line_size-1)
+        if discard[token_to_discard] == 0:
+            discard[token_to_discard] = 1
+            num_discarded += 1
+
+    for i from 0 <= i < line_size:
+        if discard[i] == 1:
+            continue
+        h = line[i]
+        for j from i + 1 <= j < line_size:
+            if j >= i + n or discard[j] == 1:
+                break
+            h = ((h * (116049371 % bucket)) % bucket + line[j]) % bucket
+            line.push_back(size + h)
+
+
+cdef (int, int, float) _do_train_job_util(vector[vector[int]] &word_ids, REAL_t *pdiscard, int max_line_size,
+                             int word_ngrams, int dropoutk, float lr, REAL_t *hidden, REAL_t *grad,
+                             int vector_size, int *negpos, int neg, int negatives_len,
+                             REAL_t *wi, REAL_t *wo, int *negatives, int bucket, int size)nogil:
+
+    cdef int local_token_count = 0
+    cdef int nexamples = 0
+    cdef float loss = <float> 0.0
+    cdef vector[int] words, context
+    cdef int i, j, ntokens_temp
+
+    for i from 0 <= i < word_ids.size():
+        ntokens_temp = get_line(word_ids[i], words, max_line_size)
+        local_token_count += ntokens_temp
+        words_size = words.size()
+        if words_size > 0:
+            for j from 0 <= j < words_size:
+                if random_uniform() > pdiscard[words[j]]:
+                    continue
+                nexamples += 1
+                context.assign(words.begin(), words.end())
+                context[j] = 0
+                add_ngrams_train(context, word_ngrams, dropoutk, bucket, size)
+                loss += update(context, words[j], lr, hidden, grad, vector_size,
+                               negpos, neg, negatives_len, wi, wo, negatives)
+                context.clear()
+        words.clear()
+
+    return local_token_count, nexamples, loss
+
+
+def _do_train_job_fast(model, sentences_, lr_, hidden_, grad_):
+
     cdef float lr = <float> lr_
     cdef int vector_size = <int> model.vector_size
-    cdef int *context = <int *> np.PyArray_DATA(context_)
-    cdef int target = <int> target_
     cdef int *negatives = <int *> np.PyArray_DATA(model.negatives)
     cdef int negpos = <int> model.negpos
     cdef int neg = <int> model.neg
     cdef REAL_t *wi = <REAL_t *> np.PyArray_DATA(model.wi)
     cdef REAL_t *wo = <REAL_t *> np.PyArray_DATA(model.wo)
     cdef int negatives_len = <int> len(model.negatives)
-    cdef int context_len = <int> len(context_)
-    #hidden_ = zeros(vector_size, dtype=REAL)
-    #grad_ = zeros(vector_size, dtype=REAL)
     cdef REAL_t *hidden = <REAL_t *> np.PyArray_DATA(hidden_)
     cdef REAL_t *grad = <REAL_t *> np.PyArray_DATA(grad_)
-    memset(hidden, 0, vector_size)
-    memset(grad, 0, vector_size)
-    #cdef REAL_t *hidden, *grad
-    #hidden = <REAL_t *>calloc(vector_size, cython.sizeof(REAL_t))
-    #grad = <REAL_t *>calloc(vector_size, cython.sizeof(REAL_t))
+    cdef REAL_t *pdiscard = <REAL_t *> np.PyArray_DATA(np.array(model.dict.pdiscard))
+    cdef int max_line_size = <int> (model.dict.max_line_size)
+    cdef int size = <int> (model.dict.size)
+    cdef int bucket = <int> (model.dict.bucket)
+    cdef int word_ngrams = <int> (model.word_ngrams)
+    cdef int dropoutk = <int> (model.dropoutk)
+    srand(model.seed)
 
-    cdef np.int32_t i
-    cdef float alpha = ONEF / <float>(context_len)
+    cdef vector[vector[int]] word_ids
+    cdef vector[int] ids
+    cdef int i, local_token_count, nexamples
+    cdef float loss
+
+    for sentence in sentences_:
+        for word in sentence:
+            h = model.dict.find(word)
+            ids.push_back(<int> model.dict.word2int[h])
+        word_ids.push_back(ids)
+        ids.clear()
 
     with nogil:
-        for i from 0 <= i < context_len:
-            our_saxpy(&vector_size, &ONEF, &wi[context[i]], &ONE, hidden, &ONE)
-        sscal(&vector_size, &alpha, hidden, &ONE)
-        loss = negative_sampling(target, lr, grad, wo, hidden, vector_size, neg, negatives, &negpos, negatives_len)
-        sscal(&vector_size, &alpha, grad, &ONE)
-        for i from 0 <= i < context_len:
-            our_saxpy(&vector_size, &ONEF, grad, &ONE, &wi[context[i]], &ONE)
-
+        local_token_count, nexamples, loss = _do_train_job_util(word_ids, pdiscard,
+                                                                max_line_size, word_ngrams,
+                                                                dropoutk, lr, hidden,
+                                                                grad, vector_size, &negpos,
+                                                                neg, negatives_len, wi, wo,
+                                                                negatives, bucket, size)
     model.negpos = negpos
-    #free(hidden)
-    #free(grad)
-    return loss
+    return local_token_count, nexamples, loss
