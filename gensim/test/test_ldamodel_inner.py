ef6478139815a5a2a4da67a0a040404df2dfc52d
arlenk <arlen.khodadadi@gmail.com>
2017-12-22 21:12:16 -0500

    add unittests for ldamodel_inner

---
+++ gensim/test/test_ldamodel_inner.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#
+# Copyright (C) 2010 Radim Rehurek <radimrehurek@seznam.cz>
+# Licensed under the GNU LGPL v2.1 - http://www.gnu.org/licenses/lgpl.html
+import logging
+import unittest
+import numpy as np
+
+try:
+    import gensim.models.ldamodel_inner as mli
+except ImportError:
+    raise unittest.SkipTest("Test requires compiled cython version of ldamodel_inner, which is not available")
+
+import gensim.matutils as matutils
+
+
+class TestLdaModelInner(unittest.TestCase):
+    def setUp(self):
+        self.random_state = np.random.RandomState()
+        self.num_runs = 10 # test functions with *num_runs* random inputs
+        self.num_topics = 100
+
+    def testLogSumExp(self):
+        # test logsumexp
+        rs = self.random_state
+
+        for dtype in [ np.float16, np.float32, np.float64 ]:
+            for i in range(self.num_runs):
+                input = rs.uniform(-1000, 1000, size=(self.num_topics,1))
+
+                known_good = matutils.logsumexp(input)
+                test_values = mli.logsumexp(input)
+
+                msg = "logsumexp failed for dtype={}".format(dtype)
+                self.assertTrue(np.allclose(known_good, test_values), msg)
+
+    def testMeanAbsoluteDifference(self):
+        # test mean_absolute_difference
+        rs = self.random_state
+
+        for dtype in [ np.float16, np.float32, np.float64 ]:
+            for i in range(self.num_runs):
+                input1 = rs.uniform(-10000, 10000, size=(self.num_topics,))
+                input2 = rs.uniform(-10000, 10000, size=(self.num_topics,))
+
+                known_good = matutils.mean_absolute_difference(input1, input2)
+                test_values = mli.mean_absolute_difference(input1, input2)
+
+                msg = "mean_absolute_difference failed for dtype={}".format(dtype)
+                self.assertTrue(np.allclose(known_good, test_values), msg)
+
+    def testDirichletExpectation(self):
+        # test dirichlet_expectation
+        rs = self.random_state
+
+        for dtype in [ np.float16, np.float32, np.float64 ]:
+            for i in range(self.num_runs):
+                # 1 dimensional case
+                input_1d = rs.uniform(.01, 10000, size=(self.num_topics,))
+                known_good = matutils.dirichlet_expectation(input_1d)
+                test_values = mli.dirichlet_expectation_1d(input_1d)
+
+                msg = "dirichlet_expectation_1d failed for dtype={}".format(dtype)
+                self.assertTrue(np.allclose(known_good, test_values), msg)
+
+                # 2 dimensional case
+                input_2d = rs.uniform(.01, 10000, size=(1, self.num_topics,))
+                known_good = matutils.dirichlet_expectation(input_2d)
+                test_values = mli.dirichlet_expectation_2d(input_2d)
+
+                msg = "dirichlet_expectation_2d failed for dtype={}".format(dtype)
+                self.assertTrue(np.allclose(known_good, test_values), msg)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
+    unittest.main()
