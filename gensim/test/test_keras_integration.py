ce0dee9355ac339654d7b039cb07e3e215d321e8
manneshiva <manneshiva+github@gmail.com>
2018-01-08 11:59:26 +0530

    renames syn0 to vectors in keras integration test

--- gensim/test/test_keras_integration.py
+++ gensim/test/test_keras_integration.py
@@ -33,13 +33,13 @@ class TestKerasWord2VecWrapper(unittest.TestCase):
         Test word2vec training.
         """
         model = self.model_cos_sim
-        self.assertTrue(model.wv.syn0.shape == (len(model.wv.vocab), 100))
-        self.assertTrue(model.syn1.shape == (len(model.wv.vocab), 100))
+        self.assertTrue(model.wv.vectors.shape == (len(model.wv.vocab), 100))
+        self.assertTrue(model.trainables.syn1.shape == (len(model.wv.vocab), 100))
         sims = model.most_similar('graph', topn=10)
         # self.assertTrue(sims[0][0] == 'trees', sims)  # most similar
 
         # test querying for "most similar" by vector
-        graph_vector = model.wv.syn0norm[model.wv.vocab['graph'].index]
+        graph_vector = model.wv.vectors_norm[model.wv.vocab['graph'].index]
         sims2 = model.most_similar(positive=[graph_vector], topn=11)
         sims2 = [(w, sim) for w, sim in sims2 if w != 'graph']  # ignore 'graph' itself
         self.assertEqual(sims, sims2)
