3281a732ef606ee42aae5334d75a08e4ddc4af47
manneshiva <manneshiva+github@gmail.com>
2018-01-09 11:18:50 +0530

    shifts epoch saver callback to an example in docstring

--- gensim/callbacks.py
+++ gensim/callbacks.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+Abstract base class to build callbacks. Callbacks are used to apply custom functions over the model
+at specific points during training (epoch start, batch end etc.). To implement a Callback, subclass
+:class: ~gensim.callbacks.Callback, look at the example below which creates a callback to save a training model
+after each epoch:
+
+>>> from gensim.test.utils import common_texts as sentences
+>>> from gensim.callbacks import Callback
+>>> from gensim.models import word2vec
+>>> class ModelEpochSaver(Callback):  # Callback to save model after every epoch
+>>>     def __init__(self, path_prefix):
+>>>         self.path_prefix = path_prefix
+>>>     def on_epoch_end(self, model):
+>>>         model.save('{}_epoch{}'.format(self.path_prefix, self.cur_epoch))
+>>>         self.cur_epoch += 1
+>>>     def on_train_begin(self, model):
+>>>         self.cur_epoch = 0
+>>> epoch_saver = ModelEpochSaver('axax')
+>>> model = word2vec.Word2Vec(sentences, iter=5, size=10, min_count=0, seed=42, callbacks=[epoch_saver])
+
+"""
+
 
 class Callback(object):
     """Abstract base class used to build new callbacks."""
@@ -25,30 +47,3 @@ class Callback(object):
 
     def on_train_end(self, model):
         pass
-
-
-class ModelEpochSaver(Callback):
-    """Callback to save model after every epoch."""
-
-    def __init__(self, path_prefix):
-        self.path_prefix = path_prefix
-
-    def on_epoch_end(self, model):
-        model.save('{}_epoch{}'.format(self.path_prefix, self.cur_epoch))
-        self.cur_epoch += 1
-
-    def on_train_begin(self, model):
-        self.cur_epoch = 0
-
-
-class LossEpochHistory(Callback):
-    """Callback to capture loss after every epoch. Currently only valid for Word2Vec"""
-
-    def __init__(self):
-        self.losses = []
-
-    def on_epoch_end(self, model):
-        self.losses.append(model.running_training_loss)
-
-    def on_train_begin(self, model):
-        model.compute_loss = True
