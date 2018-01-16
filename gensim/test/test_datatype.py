37b39f43a3a8d14bea821c9281c66219279e0246
Pushpankar <pushpankark@gmail.com>
2018-01-16 12:17:52 +0530

    Fix path error

--- gensim/test/test_datatype.py
+++ gensim/test/test_datatype.py
@@ -3,11 +3,13 @@ import unittest
 
 import numpy as np
 from gensim.models.keyedvectors import KeyedVectors
+import os
 
 
 class TestDataType(unittest.TestCase):
     def test_datatype(self):
-        kv = KeyedVectors.load_word2vec_format('test.kv.txt', datatype=np.float64)
+        path = os.path.join(os.path.dirname(__file__), 'test.kv.txt')
+        kv = KeyedVectors.load_word2vec_format(path, datatype=np.float64)
         self.assertEqual(kv['horse.n.01'][0], -0.0008546282343595379)
 
 
