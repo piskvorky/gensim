fc0617e040aa3968b995bb7023b6fc4b53a525f0
arlenk <arlen.khodadadi@gmail.com>
2018-01-17 22:27:09 -0500

    fix ordering of imports for gensim.corpora

--- gensim/corpora/__init__.py
+++ gensim/corpora/__init__.py
@@ -5,27 +5,6 @@ This package contains implementations of various streaming corpus I/O format.
 # bring corpus classes directly into package namespace, to save some typing
 from .indexedcorpus import IndexedCorpus  # noqa:F401 must appear before the other classes
 
-# following try/except block is only necessary if we need to check FAST_VERSION
-# before c-extensions have been built.  There is no non-c version of MmReader
-# so imports of MmCorpus and UciCorpus will fail if c-extensions have not been
-# built yet
-try:
-    from .mmcorpus import MmCorpus  # noqa:F401
-    from .ucicorpus import UciCorpus  # noqa:F401
-except ImportError:
-    # this means something went wrong with the c-extension compilation
-    import logging
-    logger = logging.getLogger('gensim.corpora')
-    logger.warning('No valid MmReader found, compilation must have failed')
-
-    # create a dummy classes that will prevent other imports from failing.
-    # this class will not work if actually used!
-    class MmCorpus(object):
-        pass
-
-    class UciCorpus(object):
-        pass
-    
 
 from .bleicorpus import BleiCorpus  # noqa:F401
 from .svmlightcorpus import SvmLightCorpus  # noqa:F401
@@ -35,3 +14,25 @@ from .hashdictionary import HashDictionary  # noqa:F401
 from .wikicorpus import WikiCorpus  # noqa:F401
 from .textcorpus import TextCorpus, TextDirectoryCorpus  # noqa:F401
 from .malletcorpus import MalletCorpus  # noqa:F401
+
+# following try/except block is only necessary if we need to check FAST_VERSION
+# before c-extensions have been built.  There is no non-c version of MmReader
+# so imports of MmCorpus and UciCorpus will fail if c-extensions have not been
+# built yet
+try:
+    from .mmcorpus import MmCorpus  # noqa:F401
+    from .ucicorpus import UciCorpus  # noqa:F401
+except ImportError as e:
+    # this means something went wrong with the c-extension compilation
+    import logging
+    logger = logging.getLogger('gensim.corpora')
+    logger.warning('No valid MmReader found, compilation must have failed')
+    print("here")
+    # create a dummy classes that will prevent other imports from failing.
+    # this class will not work if actually used!
+    class MmCorpus(object):
+        pass
+
+    class UciCorpus(object):
+        pass
+
