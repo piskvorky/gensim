c157d79f62ea3377b181253b489823ffe6c0ef51
Pushpankar <pushpankark@gmail.com>
2018-01-23 00:32:06 +0530

    Test other datatypes as well.

--- gensim/test/test_datatype.py
+++ gensim/test/test_datatype.py
@@ -17,12 +17,26 @@ from gensim.models.keyedvectors import KeyedVectors
 
 
 class TestDataType(unittest.TestCase):
-    def test_text(self):
+    def load_model(self, datatype):
         path = datapath('test.kv.txt')
         kv = KeyedVectors.load_word2vec_format(path, binary=False,
-                                               datatype=np.float64)
+                                               datatype=datatype)
+        return kv
+
+    def test_high_precision(self):
+        kv = self.load_model(np.float64)
         self.assertAlmostEqual(kv['horse.n.01'][0], -0.0008546282343595379)
         self.assertEqual(kv['horse.n.01'][0].dtype, np.float64)
+
+    def test_medium_precision(self):
+        kv = self.load_model(np.float32)
+        self.assertAlmostEqual(kv['horse.n.01'][0], -0.00085462822)
+        self.assertEqual(kv['horse.n.01'][0].dtype, np.float32)
+
+    def test_low_precision(self):
+        kv = self.load_model(np.float16)
+        self.assertAlmostEqual(kv['horse.n.01'][0], -0.00085449)
+        self.assertEqual(kv['horse.n.01'][0].dtype, np.float16)
 
 
 if __name__ == '__main__':
