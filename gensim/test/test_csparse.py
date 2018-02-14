25db512a3979d75a2d93632cb81d2a70eb4401c9
Brian Mingus <brian.mingus@colorado.edu>
2018-02-13 22:15:30 -0700

    Add speed and equality assertions to test

--- gensim/test/test_csparse.py
+++ gensim/test/test_csparse.py
@@ -3,22 +3,48 @@
 #
 # Copyright (C) 2010 Radim Rehurek <radimrehurek@seznam.cz>
 # Licensed under the GNU LGPL v2.1 - http://www.gnu.org/licenses/lgpl.html
-
-import logging
-import unittest
-
-import numpy, scipy
 from gensim.csparse.psparse import pmultiply
+
 from numpy.testing import assert_array_equal
+from numpy import asfortranarray
+from numpy.random import normal
+from functools import partial
+from scipy.sparse import rand
+from os import cpu_count
+from timeit import Timer
+import unittest
+import logging
+import scipy
 
 class TestCSparse(unittest.TestCase):
     def testPmultiply(self):
-        X = scipy.sparse.rand(3000, 8000, format="csc")
-        Y = numpy.asfortranarray(numpy.random.normal(0.0, 1.0, (X.shape[1], 1000)))
-        Z_np = X @ Y
-        Z = pmultiply(X, Y)
 
-        assert_array_equal(Z_np, Z)
+        def matmul(X, Y): return X @ Y
+        def pmatmul(X, Y): return pmultiply(X, Y)
+
+        X_rows, X_cols, factors = 10000, 50000, 1000
+
+        X = rand(X_rows, X_cols, format="csc")
+        Y = asfortranarray(normal(0.0, 1.0, (X.shape[1], factors)))
+
+        cpus = cpu_count()
+        
+        if cpus > 1:
+            t_matmul = Timer(partial(matmul, X, Y)).timeit(1)
+            t_pmatmul = Timer(partial(pmatmul, X, Y)).timeit(1)
+
+            assert(t_pmatmul < t_matmul)
+            
+            print(f'''cpus: {cpus}
+X.shape[0]: {X_rows}
+X.shape[1]: {X_cols}
+Y.shape[0]: {X_cols}
+Y.shape[1]: {factors}
+matmul: {t_matmul:.4} seconds
+pmatmul: {t_pmatmul:.4} seconds
+speedup: {t_matmul / t_pmatmul:.4} times faster''')
+
+        assert_array_equal(matmul(X, Y), pmatmul(X, Y))
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
