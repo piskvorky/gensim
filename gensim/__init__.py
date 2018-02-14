e337c80bee9da139c2612b36047b8771e5108976
Sharan Yalburgi <sharanyalburgi@gmail.com>
2018-02-14 13:19:46 +0000

    requested changes

--- gensim/__init__.py
+++ gensim/__init__.py
@@ -6,6 +6,16 @@ similarities within a corpus of documents.
 from gensim import parsing, matutils, interfaces, corpora, models, similarities, summarization, utils  # noqa:F401
 import logging
 
+import platform
+import sys
+import numpy
+import scipy
+import argparse
+import gensim
+import smart_open
+from gensim.models.word2vec import FAST_VERSION
+
+
 __version__ = '3.3.0'
 
 
@@ -19,3 +29,24 @@ class NullHandler(logging.Handler):
 logger = logging.getLogger('gensim')
 if len(logger.handlers) == 0:  # To ensure reload() doesn't add another one
     logger.addHandler(NullHandler())
+
+
+def package_info():
+    """Get the versions of Gensim and its dependencies, the location where Gensim is installed and platform on which the system is running.
+    
+    Returns
+    -------
+    dict 
+        Returns a dictionary containing the versions pof Gensim, Python, NumPy, SciPy and platform information.
+    """    
+    dict = {}
+    dict["platform"] = platform.platform()
+    dict["python"] = sys.version
+    dict["NumPy"] = numpy.__version__
+    dict["SciPy"] = scipy.__version__
+    dict["gensim"] = gensim.__version__, gensim.__file__
+#    dict["smart_open"] = smart_open.__version__
+    dict["fast_version"] = FAST_VERSION
+    
+    return dict
+
--- gensim/__main__.py
+++ gensim/__main__.py
@@ -1,19 +1,18 @@
-import platform
-import sys
-import numpy
-import scipy
-import gensim
 import argparse
+from gensim import package_info
 
-if __name__ == "__main__":
 
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--info", action="store_true", help="Display version of Gensim and its dpendencies")
-        opt = parser.parse_args()
-        if opt.info:
-                print(platform.platform())
-                print("Python", sys.version)
-                print("NumPy", numpy.__version__)
-                print("SciPy", scipy.__version__)
-                print("gensim", gensim.__version__)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--info", action="store_true", help="Display version of Gensim and its dpendencies")
+opt = parser.parse_args()
+
+if opt.info:
+    dict = package_info()
+    print("\nGensim ", dict["gensim"][0], " from ", dict["gensim"][1], "\n")
+    print("FAST_VERSION ",dict["fast_version"], "\n\n") 
+    print("python ", dict["python"], "\n\n")
+    print("Platform \t", dict["platform"], "\n")
+    print("NumPy \t\t",  dict["NumPy"], "\n")
+    print("SciPy \t\t",  dict["SciPy"], "\n")
 
