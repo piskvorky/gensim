64edfde1363f4d8ea9f36ad83dc868ac7b3f549b
ivan <menshikh.iv@gmail.com>
2018-02-15 19:18:18 +0500

    fix utils

--- gensim/test/utils.py
+++ gensim/test/utils.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-"""This module contains common utilities used in automated code tests for Gensim
-modules. It includes functions to get full paths to test data and files in
-temporary directories. Also it contains context manager which automatically
-delete temporary directory (and included files) after use. For your quick
-tests you may use tiny toy dataset, its dictionary and corpus which were
-used in Gensim tutorial.
+"""Module contains common utilities used in automated code tests for Gensim modules.
 
 Attributes:
 -----------
@@ -22,49 +17,45 @@ common_dictionary : :class:`~gensim.corpora.Dictionary`
 common_corpus : list of list of (int, int)
     Corpus of toy dataset.
 
+
 Examples:
 ---------
 It's easy to keep objects in temporary folder and reuse'em if needed:
 
 >>> from gensim.models import word2vec
 >>> from gensim.test.utils import get_tmpfile, common_texts
+>>>
 >>> model = word2vec.Word2Vec(common_texts, min_count=1)
 >>> temp_path = get_tmpfile('toy_w2v')
 >>> model.save(temp_path)
+>>>
 >>> new_model = word2vec.Word2Vec.load(temp_path)
->>> new_model.wv.most_similar("human", topn=1)
-[('user', 0.159118190407753)]
+>>> result = new_model.wv.most_similar("human", topn=1)
 
-Let's print first document in toy dataset and then recreate it using its corpus
-and dictionary. Order of elements might differ:
+Let's print first document in toy dataset and then recreate it using its corpus and dictionary.
 
 >>> from gensim.test.utils import common_texts, common_dictionary, common_corpus
 >>> print(common_texts[0])
 ['human', 'interface', 'computer']
->>> print([common_dictionary[i] for i, _ in common_corpus[0]])
-['computer', 'human', 'interface']
+>>> assert common_dictionary.doc2bow(common_texts[0]) == common_corpus[0]
 
-We can find our toy set in test data directory. Again, order of words might
-be different.
+We can find our toy set in test data directory.
 
->>> from gensim.test.utils import datapath, common_texts
+>>> from gensim.test.utils import datapath
+>>>
 >>> with open(datapath("testcorpus.txt")) as f:
 ...     texts = [line.strip().split() for line in f]
->>> print(texts[-1], common_texts[-1])
-['survey', 'graph', 'minors'] ['graph', 'minors', 'survey']
+>>> print(texts[0])
+['computer', 'human', 'interface']
 
 If you don't need to keep temporary objects on disk use :func:`~gensim.test.utils.temporary_file`:
 
->>> from gensim.test.utils import temporary_file, common_corpus
+>>> from gensim.test.utils import temporary_file, common_corpus, common_dictionary
+>>> from gensim.models import LdaModel
+>>>
 >>> with temporary_file("temp.txt") as tf:
->>>     lda = LdaModel(common_corpus[:-1], num_topics=5)
->>>     lda.save(tf)
->>>     lda_updated = LdaModel.load(tf)
->>>     lda_updated.update(common_corpus[:-1])
->>>     print(lda[common_corpus[-2]])
->>>     print(lda_updated[common_corpus[-2]])
-[(0, 0.050019536), (1, 0.35821638), (2, 0.050089914), (3, 0.4916386), (4, 0.050035536)]
-[(0, 0.05001102), (1, 0.29562944), (2, 0.050081477), (3, 0.5542563), (4, 0.050021794)]
+...     lda = LdaModel(common_corpus, id2word=common_dictionary, num_topics=3)
+...     lda.save(tf)
 
 """
 
@@ -79,13 +70,13 @@ module_path = os.path.dirname(__file__)  # needed because sample data files are 
 
 
 def datapath(fname):
-    """Returns full path for file `fname` in test data directory placed in this
-    module directory. Usually used to place corpus to test_data directory.
+    """Get full path for file `fname` in test data directory placed in this module directory.
+    Usually used to place corpus to test_data directory.
 
     Parameters
     ----------
     fname : str
-        Filename.
+        Name of file.
 
     Returns
     -------
@@ -96,25 +87,27 @@ def datapath(fname):
     -------
     Let's get path of test GloVe data file and check if it exits.
 
-    >>> import os
+    >>> from gensim.corpora import MmCorpus
     >>> from gensim.test.utils import datapath
-    >>> os.path.exists(datapath("test_glove.txt"))
-    True
+    >>>
+    >>> corpus = MmCorpus(datapath("testcorpus.mm"))
+    >>> for document in corpus:
+    ...     pass
+
 
     """
-
     return os.path.join(module_path, 'test_data', fname)
 
 
 def get_tmpfile(suffix):
-    """Returns full path to file `suffix` in temporary folder. This function
-    doesn't creates file. Also it may return different paths in consecutive
-    calling.
+    """Get full path to file `suffix` in temporary folder.
+    This function doesn't creates file (only generate unique name).
+    Also, it may return different paths in consecutive calling.
 
     Parameters
     ----------
     suffix : str
-        Filename.
+        Suffix of file.
 
     Returns
     -------
@@ -123,14 +116,16 @@ def get_tmpfile(suffix):
 
     Examples
     --------
-    Using this function we may get path to temporary file and use it, for
-    example, to store temporary model.
+    Using this function we may get path to temporary file and use it, for example, to store temporary model.
 
     >>> from gensim.models import LsiModel
     >>> from gensim.test.utils import get_tmpfile, common_dictionary, common_corpus
+    >>>
     >>> tmp_f = get_tmpfile("toy_lsi_model")
+    >>>
     >>> model = LsiModel(common_corpus, id2word=common_dictionary)
     >>> model.save(tmp_f)
+    >>>
     >>> loaded_model = LsiModel.load(tmp_f)
 
     """
@@ -139,14 +134,13 @@ def get_tmpfile(suffix):
 
 @contextlib.contextmanager
 def temporary_file(name=""):
-    """This context manager creates file `name` in temporary directory and
-    returns its full path. Temporary directory with included files will deleted
-    at the end of context. Note, it won't create file.
+    """This context manager creates file `name` in temporary directory and returns its full path.
+    Temporary directory with included files will deleted at the end of context. Note, it won't create file.
 
     Parameters
     ----------
     name : str
-        Filename
+        Filename.
 
     Yields
     ------
@@ -160,13 +154,17 @@ def temporary_file(name=""):
 
     >>> import os
     >>> from gensim.test.utils import temporary_file
-    >>> with temporary_file("temp.txt") as tf:
-    ...     with open(tf, "w") as f:
-    ...         f.write("my extremely useful information")
-    ...     print("Is this file exists?", os.path.exists(tf))
+    >>> with temporary_file("temp.txt") as tf, open(tf, 'w') as outfile:
+    ...     outfile.write("my extremely useful information")
+    ...     print("Is this file exists? {}".format(os.path.exists(tf)))
+    ...     print("Is this folder exists? {}".format(os.path.exists(os.path.dirname(tf))))
     Is this file exists? True
-    >>> print("Is file's folder exists now?", os.path.exists(os.path.dirname(tf)))
-    But is file's folder exists now? False
+    Is this folder exists? True
+    >>>
+    >>> print("Is this file exists? {}".format(os.path.exists(tf)))
+    Is this file exists? False
+    >>> print("Is this folder exists? {}".format(os.path.exists(os.path.dirname(tf))))
+    Is this folder exists? False
 
     """
 
