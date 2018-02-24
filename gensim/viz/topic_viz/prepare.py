2c795d8a5499448cf27c1db18bf8698c58f14460
parulsethi <parul1sethi@gmail.com>
2018-02-24 19:34:16 +0530

    calculate coordinates for doc/topic/word

--- gensim/viz/topic_viz/prepare.py
+++ gensim/viz/topic_viz/prepare.py
@@ -98,3 +98,28 @@ def js_TSNE(distributions, **kwargs):
 	dist_matrix = squareform(pdist(distributions, metric=_jensen_shannon))
 	model = TSNE(n_components=2, random_state=0, metric='precomputed', **kwargs)
 	return model.fit_transform(dist_matrix)
+
+
+def _doc_coordinates(mds, doc_topic_dists, doc_tag, doc_texts):
+	K = doc_topic_dists.shape[0]
+	mds_res = mds(doc_topic_dists)
+	assert mds_res.shape == (K, 2)
+	mds_df = pd.DataFrame({'x': mds_res[:,0], 'y': mds_res[:,1], 'docs': doc_tag, 'doc_texts':doc_texts, 'Freq': [10]*K}, index=range(1, K+1))
+	mds_df.reset_index(level=0, inplace=True)
+	return mds_df
+
+def _topic_coordinates(mds, topic_word_dists, topic_proportion):
+	K = topic_word_dists.shape[0]
+	mds_res = mds(topic_word_dists)
+	assert mds_res.shape == (K, 2)
+	mds_df = pd.DataFrame({'x': mds_res[:,0], 'y': mds_res[:,1], 'topics': range(1, K+1), 'Freq': topic_proportion * 100}, index=range(1, K+1))
+	mds_df.reset_index(level=0, inplace=True)
+	return mds_df
+
+def _word_coordinates(mds, word_topic_dists, vocab, word_proportion):
+	K = word_topic_dists.shape[0]
+	mds_res = mds(word_topic_dists)
+	assert mds_res.shape == (K, 2)
+	mds_df = pd.DataFrame({'x': mds_res[:,0], 'y': mds_res[:,1], 'vocab': vocab, 'Freq': word_proportion * 100}, index=range(1, K+1))
+	mds_df.reset_index(level=0, inplace=True)
+	return mds_df
