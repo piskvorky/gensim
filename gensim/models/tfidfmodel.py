c6109589e53c5ac2a57f95b76631689ee4307799
Timofey Yefimov <anotherbugmaster@gmail.com>
2018-02-25 15:37:28 +0300

    Fix flake8

--- gensim/models/tfidfmodel.py
+++ gensim/models/tfidfmodel.py
@@ -27,7 +27,8 @@ def resolve_weights(smartirs):
         Information Retrieval System, a mnemonic scheme for denoting tf-idf weighting
         variants in the vector space model. The mnemonic for representing a combination
         of weights takes the form ddd, where the letters represents the term weighting of the document vector.
-        for more information visit `SMART_Information_Retrieval_System <https://en.wikipedia.org/wiki/SMART_Information_Retrieval_System>`.
+        for more information visit `SMART_Information_Retrieval_System <https://en.wikipedia.org/wiki/SMART_Information
+        Retrieval_System>`.
 
     Returns
     -------
