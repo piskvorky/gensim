2ee6ab85292971ccdcbaccbff23606a25be27597
Mritunjay Mohitesh <mritunjaymohitesh@gmail.com>
2018-02-28 21:02:56 +0530

    fix 'iter' and 'size' warnings

--- gensim/sklearn_api/d2vmodel.py
+++ gensim/sklearn_api/d2vmodel.py
@@ -9,6 +9,7 @@ Scikit learn interface for gensim for easy use of gensim with scikit-learn
 Follows scikit-learn API conventions
 """
 
+import warnings
 import numpy as np
 from six import string_types
 from sklearn.base import TransformerMixin, BaseEstimator
@@ -26,10 +27,18 @@ class D2VTransformer(TransformerMixin, BaseEstimator):
     def __init__(self, dm_mean=None, dm=1, dbow_words=0, dm_concat=0, dm_tag_count=1, docvecs=None,
                  docvecs_mapfile=None, comment=None, trim_rule=None, size=100, alpha=0.025, window=5, min_count=5,
                  max_vocab_size=None, sample=1e-3, seed=1, workers=3, min_alpha=0.0001, hs=0, negative=5, cbow_mean=1,
-                 hashfxn=hash, iter=5, sorted_vocab=1, batch_words=10000):
+                 hashfxn=hash, iter=5,sorted_vocab=1, batch_words=10000 ,**kwargs):
         """
         Sklearn api for Doc2Vec model. See gensim.models.Doc2Vec and gensim.models.Word2Vec for parameter details.
         """
+        if 'iter' in kwargs:
+            warnings.warn("The parameter `iter` is deprecated, will be removed in 4.0.0, use `epochs` instead.")
+            kwargs['epochs'] = kwargs['iter']
+
+        if 'size' in kwargs:
+            warnings.warn("The parameter `size` is deprecated, will be removed in 4.0.0, use `vector_size` instead.")
+            kwargs['vector_size'] = kwargs['size']
+            
         self.gensim_model = None
         self.dm_mean = dm_mean
         self.dm = dm
@@ -42,7 +51,9 @@ class D2VTransformer(TransformerMixin, BaseEstimator):
         self.trim_rule = trim_rule
 
         # attributes associated with gensim.models.Word2Vec
+        
         self.size = size
+        self.vector_size = vector_size
         self.alpha = alpha
         self.window = window
         self.min_count = min_count
@@ -56,6 +67,7 @@ class D2VTransformer(TransformerMixin, BaseEstimator):
         self.cbow_mean = int(cbow_mean)
         self.hashfxn = hashfxn
         self.iter = iter
+        self.epochs = epochs
         self.sorted_vocab = sorted_vocab
         self.batch_words = batch_words
 
@@ -72,11 +84,11 @@ class D2VTransformer(TransformerMixin, BaseEstimator):
             documents=d2v_sentences, dm_mean=self.dm_mean, dm=self.dm,
             dbow_words=self.dbow_words, dm_concat=self.dm_concat, dm_tag_count=self.dm_tag_count,
             docvecs=self.docvecs, docvecs_mapfile=self.docvecs_mapfile, comment=self.comment,
-            trim_rule=self.trim_rule, size=self.size, alpha=self.alpha, window=self.window,
+            trim_rule=self.trim_rule, size=self.size, vector_size=self.vector_size, alpha=self.alpha, window=self.window,
             min_count=self.min_count, max_vocab_size=self.max_vocab_size, sample=self.sample,
             seed=self.seed, workers=self.workers, min_alpha=self.min_alpha, hs=self.hs,
             negative=self.negative, cbow_mean=self.cbow_mean, hashfxn=self.hashfxn,
-            iter=self.iter, sorted_vocab=self.sorted_vocab, batch_words=self.batch_words
+            iter=self.iter,epochs=self.epochs, sorted_vocab=self.sorted_vocab, batch_words=self.batch_words
         )
         return self
 
