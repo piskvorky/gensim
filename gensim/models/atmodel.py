0dbf1689fbe839f2683cba608e446bea0846c952
Sourav Singh <ssouravsingh12@gmail.com>
2018-03-09 00:07:07 +0530

    Make final edits

--- gensim/models/atmodel.py
+++ gensim/models/atmodel.py
@@ -388,9 +388,9 @@ class AuthorTopicModel(LdaModel):
         Parameters
         ----------
         expElogthetad: numpy.ndarray
-            Value of variational distribution q(theta|gamma).
+            Value of variational distribution .. math:: q(theta|gamma).
         expElogbetad: numpy.ndarray
-            Value of variational distribution q(beta|lambda).
+            Value of variational distribution .. math:: q(beta|lambda).
         
         Returns
         -------
@@ -1069,11 +1069,27 @@ class AuthorTopicModel(LdaModel):
             
         Returns
         -------
-        list of 2-tuples
+        list of (topic_id, topic_probability) as a 2-tuple
             Topic distribution of an author as a list of topic ID and its probability.
             
         Example
         -------
+        >>> import numpy as np
+        >>> from gensim.models import AuthorTopicModel
+        >>> from gensim.corpora import mmcorpus
+        >>> from gensim.test.utils import (datapath, common_dictionary as dictionary, common_corpus as corpus)
+        >>> author2doc = {
+        ...     'john': [0, 1, 2, 3, 4, 5, 6],
+        ...     'jane': [2, 3, 4, 5, 6, 7, 8],
+        ...     'jack': [0, 2, 4, 6, 8]
+        ... }
+        >>> doc2author = {
+        ...     0: ['john', 'jack'],
+        ...     1: ['john', 'jill'],
+        ...     2: ['john', 'jane', 'jack']
+        ... }
+        >>> corpus = mmcorpus.MmCorpus(datapath('testcorpus.mm'))
+        >>> model = AuthorTopicModel(corpus, author2doc=author2doc, id2word=dictionary, num_topics=4, passes=100)  # train model
         >>> author_vecs = [model.get_author_topics(author) for author in model.id2author.values()]
         >>> print(author_vecs)
         
@@ -1114,7 +1130,7 @@ class AuthorTopicModel(LdaModel):
         
         Returns
         -------
-        list of 2-tuples
+        list of (topic_id, topic_probability) as a 2-tuple
             Topic distribution for the author as a list.
         
         """
