09b5dc7a9d78a379605c87bd09cc60dcc1054c9d
Mritunjay Mohitesh <mritunjaymohitesh@gmail.com>
2018-03-12 12:28:08 +0530

    Update w2vmodel.py

--- gensim/sklearn_api/w2vmodel.py
+++ gensim/sklearn_api/w2vmodel.py
@@ -10,7 +10,6 @@ Scikit learn interface for gensim for easy use of gensim with scikit-learn
 Follows scikit-learn API conventions
 """
 
-import warnings
 import numpy as np
 import six
 from sklearn.base import TransformerMixin, BaseEstimator
@@ -24,21 +23,14 @@ class W2VTransformer(TransformerMixin, BaseEstimator):
     Base Word2Vec module
     """
 
-    def __init__(self, size=None, vector_size=100, alpha=0.025, window=5, min_count=5, max_vocab_size=None, sample=1e-3,
-                 seed=1, workers=3, min_alpha=0.0001, sg=0, hs=0, negative=5, cbow_mean=1, hashfxn=hash, iter=None,
-                 epochs=5, null_word=0, trim_rule=None, sorted_vocab=1, batch_words=10000):
+    def __init__(self, size=100, alpha=0.025, window=5, min_count=5, max_vocab_size=None, sample=1e-3, seed=1,
+                 workers=3, min_alpha=0.0001, sg=0, hs=0, negative=5, cbow_mean=1, hashfxn=hash, iter=5, null_word=0,
+                 trim_rule=None, sorted_vocab=1, batch_words=10000):
         """
         Sklearn wrapper for Word2Vec model. See gensim.models.Word2Vec for parameter details.
         """
-        if iter is not None:
-            warnings.warn("The parameter `iter` is deprecated, will be removed in 4.0.0, use `epochs` instead.")
-            epochs = iter
-        if size is not None:
-            warnings.warn("The parameter `size` is deprecated, will be removed in 4.0.0, use `vector_size` instead.")
-            vector_size = size
         self.gensim_model = None
-        self.vector_size = vector_size
-        self.size = vector_size
+        self.size = size
         self.alpha = alpha
         self.window = window
         self.min_count = min_count
@@ -52,23 +44,23 @@ class W2VTransformer(TransformerMixin, BaseEstimator):
         self.negative = negative
         self.cbow_mean = int(cbow_mean)
         self.hashfxn = hashfxn
-        self.epochs = epochs
+        self.iter = iter
         self.null_word = null_word
         self.trim_rule = trim_rule
         self.sorted_vocab = sorted_vocab
         self.batch_words = batch_words
-        
+
     def fit(self, X, y=None):
         """
         Fit the model according to the given training data.
         Calls gensim.models.Word2Vec
         """
         self.gensim_model = models.Word2Vec(
-            sentences=X, size=self.vector_size, alpha=self.alpha,
+            sentences=X, size=self.size, alpha=self.alpha,
             window=self.window, min_count=self.min_count, max_vocab_size=self.max_vocab_size,
             sample=self.sample, seed=self.seed, workers=self.workers, min_alpha=self.min_alpha,
             sg=self.sg, hs=self.hs, negative=self.negative, cbow_mean=self.cbow_mean,
-            hashfxn=self.hashfxn, iter=self.epochs, null_word=self.null_word, trim_rule=self.trim_rule,
+            hashfxn=self.hashfxn, iter=self.iter, null_word=self.null_word, trim_rule=self.trim_rule,
             sorted_vocab=self.sorted_vocab, batch_words=self.batch_words
         )
         return self
