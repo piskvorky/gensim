52fc95620e5012c18ed69318a25c4741b85a04be
Timofey Yefimov <anotherbugmaster@gmail.com>
2018-04-23 05:40:35 +0300

    Add more LDA-like API

--- gensim/models/nmf.py
+++ gensim/models/nmf.py
@@ -21,9 +21,18 @@ class Nmf(interfaces.TransformationABC, basemodel.BaseTopicModel):
 
     """
 
-    def __init__(self, corpus=None, num_topics=100, id2word=None,
-                 chunksize=2000, passes=1, lambda_=1.,
-                 kappa=1., store_r=False, max_iter=1e9):
+    def __init__(self,
+                 corpus=None,
+                 num_topics=100,
+                 id2word=None,
+                 chunksize=2000,
+                 passes=1,
+                 lambda_=1.,
+                 kappa=1.,
+                 store_r=False,
+                 max_iter=1e9,
+                 normalize=True
+                 ):
         """
 
         Parameters
@@ -43,6 +52,7 @@ class Nmf(interfaces.TransformationABC, basemodel.BaseTopicModel):
         self._H = []
         self.v_max = None
         self.max_iter = max_iter
+        self.normalize = normalize
         if store_r:
             self._R = []
         else:
@@ -68,26 +78,84 @@ class Nmf(interfaces.TransformationABC, basemodel.BaseTopicModel):
         self._B = value
 
     def get_topics(self):
-        raise NotImplementedError
+        if self.normalize:
+            return (self._W / np.sum(self._W, axis=0)).T
 
-    def __getitem__(self, vec):
-        raise NotImplementedError
+        return self._W.T
 
-    def _setup(self, X):
+    def __getitem__(self, bow, eps=None):
+        return self.get_document_topics(bow, eps)
+
+    def show_topics(self, num_topics=10, num_words=10, log=False, formatted=True):
+        """
+        Args:
+            num_topics (int): show results for first `num_topics` topics.
+                Unlike LSA, there is no natural ordering between the topics in LDA.
+                The returned `num_topics <= self.num_topics` subset of all topics is
+                therefore arbitrary and may change between two LDA training runs.
+            num_words (int): include top `num_words` with highest probabilities in topic.
+            log (bool): If True, log output in addition to returning it.
+            formatted (bool): If True, format topics as strings, otherwise return them as
+                `(word, probability)` 2-tuples.
+        Returns:
+            list: `num_words` most significant words for `num_topics` number of topics
+            (10 words for top 10 topics, by default).
+        """
+        # TODO: maybe count sparsity in some other way
+        sparsity = np.count_nonzero(self._W, axis=0)
+
+        if num_topics < 0 or num_topics >= self.num_topics:
+            num_topics = self.num_topics
+            chosen_topics = range(num_topics)
+        else:
+            num_topics = min(num_topics, self.num_topics)
+
+            sorted_topics = list(matutils.argsort(sparsity))
+            chosen_topics = sorted_topics[:num_topics // 2] + sorted_topics[-num_topics // 2:]
+
+        shown = []
+
+        topic = self.get_topics()
+        for i in chosen_topics:
+            topic_ = topic[i]
+            bestn = matutils.argsort(topic_, num_words, reverse=True)
+            topic_ = [(self.id2word[id], topic_[id]) for id in bestn]
+            if formatted:
+                topic_ = ' + '.join(['%.3f*"%s"' % (v, k) for k, v in topic_])
+
+            shown.append((i, topic_))
+            if log:
+                logger.info("topic #%i (%.3f): %s", i,sparsity[i], topic_)
+
+        return shown
+
+    def get_document_topics(self, bow, minimum_probability=None):
+        v = matutils.corpus2dense([bow], len(self.id2word), 1).T
+        h, _ = self._solveproj(v, self._W, v_max=np.inf)
+
+        if self.normalize:
+            h = h / np.sum(h)
+
+        if minimum_probability is not None:
+            h[h < minimum_probability] = 0
+
+        return h
+
+    def _setup(self, corpus):
         self._h, self._r = None, None
-        x = next(iter(X))
-        x_asarray = matutils.corpus2dense([x], len(self.id2word), 1)[:, 0]
-        m = len(x_asarray)
-        avg = np.sqrt(x_asarray.mean() / m)
+        first_doc = next(iter(corpus))
+        first_doc = matutils.corpus2dense([first_doc], len(self.id2word), 1)[:, 0]
+        m = len(first_doc)
+        avg = np.sqrt(first_doc.mean() / m)
 
-        self.n_features = len(x_asarray)
+        self.n_features = len(first_doc)
 
         self._W = np.abs(avg * halfnorm.rvs(size=(self.n_features, self.num_topics)) /
                          np.sqrt(self.num_topics))
 
         self.A = np.zeros((self.num_topics, self.num_topics))
         self.B = np.zeros((self.n_features, self.num_topics))
-        return X
+        return corpus
 
     def update(self, corpus, chunks_as_numpy=False):
         """
@@ -96,6 +164,7 @@ class Nmf(interfaces.TransformationABC, basemodel.BaseTopicModel):
         ----------
         corpus : matrix or iterator
             Matrix to factorize.
+        chunks_as_numpy: bool
         """
 
         if self.n_features is None:
@@ -138,25 +207,6 @@ class Nmf(interfaces.TransformationABC, basemodel.BaseTopicModel):
             lasttwo[1] = 0.5 * np.trace(self._W.T.dot(self._W).dot(self.A)) - \
                          np.trace(self._W.T.dot(self.B))
 
-    def transform(self, corpus, return_r=False):
-        H = []
-        if return_r:
-            R = []
-
-        W = self._W
-        for chunk in corpus:
-            v = matutils.corpus2dense([chunk], len(self.id2word), 1).T[0]
-            h, r = self._solveproj(v, W, v_max=np.inf)
-            H.append(h.copy())
-            if return_r:
-                R.append(r.copy())
-
-        H = np.stack(H, axis=-1)
-        if return_r:
-            return H, np.stack(R, axis=-1)
-        else:
-            return H
-
     def get_factor_matrices(self):
         if len(self._H) > 0:
             if len(self._H[0].shape) == 1:
@@ -190,15 +240,11 @@ class Nmf(interfaces.TransformationABC, basemodel.BaseTopicModel):
             self.v_max = v_max
         elif self.v_max is None:
             self.v_max = v.max()
-        # else:
-        #     self.v_max = np.max((self.v_max, v.max()))
-        if len(v.shape) == 2:
-            batch_size = v.shape[1]
-            rshape = (m, batch_size)
-            hshape = (n, batch_size)
-        else:
-            rshape = m,
-            hshape = n,
+
+        batch_size = v.shape[1]
+        rshape = (m, batch_size)
+        hshape = (n, batch_size)
+
         if h is None or h.shape != hshape:
             h = np.zeros(hshape)
 
