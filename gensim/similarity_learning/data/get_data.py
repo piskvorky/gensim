0f58bdfc093e3c8f95fee88ffd517a22ff8836d5
aneesh-joshi <aneeshyjoshi@gmail.com>
2018-05-28 18:13:54 +0530

    add get_data.py

--- gensim/similarity_learning/data/get_data.py
+++ gensim/similarity_learning/data/get_data.py
@@ -9,26 +9,29 @@ Currently supports:
 """
 
 # The urls and filepaths of currently supported files
-wikiqa_url, wikiqa_file = "https://download.microsoft.com/download/E/5/F/E5FCFCEE-7005-4814-853D-DAA7C66507E0/", "WikiQACorpus.zip"
+wikiqa_url, wikiqa_file = "https://download.microsoft.com/download/E/5/F/E5FCFCEE-7005-4814-853D-DAA7C66507E0/", "WikiQACorpus.zip"  # noqa
 quoraqp_url, quoraqp_file = "http://qim.ec.quoracdn.net/", "quora_duplicate_questions.tsv"
+
 
 def download(url, file):
     print("Downloading %s" % file)
     req = requests.get(url + file)
     try:
-        with open(file , "wb") as code:
+        with open(file, "wb") as code:
             code.write(req.content)
             print("Download of %s complete" % file)
     except Exception as e:
         print(str(e))
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--datafile', default='all', help='refers to the file you want to download. Options: wikiqa, quoraqp, all')
+    parser.add_argument('--datafile',
+                         default='all',
+                         help='refers to the file you want to download. Options: wikiqa, quoraqp, all')
     args = parser.parse_args()
-    
+
     if args.datafile == 'wikiqa':
         download(wikiqa_url, wikiqa_file)
     elif args.datafile == 'quoraqp':
