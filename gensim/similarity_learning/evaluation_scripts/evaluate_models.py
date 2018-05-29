2e4a5a8b94ec66d3021dc6babcec83af7996e161
aneesh-joshi <aneeshyjoshi@gmail.com>
2018-05-29 20:11:32 +0530

    add support for saving evaluaton results in a .csv

--- gensim/similarity_learning/evaluation_scripts/evaluate_models.py
+++ gensim/similarity_learning/evaluation_scripts/evaluate_models.py
@@ -24,6 +24,9 @@ $ python evaluate_models.py --model word2vec --datapath ../data/WikiQACorpus/ --
 For evaluating the TREC format file produced by MatchZoo:
 $ python evaluate_models.py  --model mz --mz_output_file predict.test.wikiqa.txtDRMM
 Note: here "predict.test.wikiqa.txtDRMM" is the file output by MZ. It has been provided in this repo as an example.
+
+For evaluating all models
+$ python evaluate_models.py --model all --mz_output_file predict.test.wikiqa.txtDRMM --result_save_path results --word_embedding_path ../evaluation_scripts/glove.6B.50d.txt --datapath ../data/WikiQACorpus/  # noqa:F401
 """
 
 
@@ -38,10 +41,13 @@ class LabeledLineSentence(object):
         for uid, line in enumerate(self.corpus):
             yield TaggedDocument(words=line.split(), tags=['SENT_%s' % uid])
 
+# list to store results from all models to be saved later
+results_list = []
+
 
 def mapk(Y_true, Y_pred):
     """Function to get Mean Average Precision(MAP) for a given set of Y_true, Y_pred
-    TODO Currently doesn't support mapping at k. Couldn't use only map as it's a 
+    TODO Currently doesn't support mapping at k. Couldn't use only map as it's a
     reserved word
 
     parameters:
@@ -127,12 +133,12 @@ def mean_ndcg(Y_true, Y_pred, k=10):
 
 def accuracy(Y_true, Y_pred):
     """Calculates accuracy as (number of correct predictions / number of predictions)
+    WARNING: TODO this definition of accuracy doesn't allow for two correct answers
     """
     n_correct = 0
     for y_pred, y_true in zip(Y_pred, Y_true):
         if (np.argmax(y_true) == np.argmax(y_pred)):
             n_correct += 1
-
     return n_correct / len(Y_true)
 
 
@@ -140,13 +146,6 @@ def cos_sim(vec1, vec2):
     """Calculates the cosine similarity of 2 vectos
     """
     return np.sum(vec1 * vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
-
-
-def write_results_to_file(eval_metrics, file_to_write):
-    """Writes the evaluated metrics in the given
-    TODO
-    """
-    raise NotImplementedError()
 
 
 def get_metric_results(Y_true, Y_pred, k_range=[1, 3, 5, 10, 20]):
@@ -158,12 +157,10 @@ def get_metric_results(Y_true, Y_pred, k_range=[1, 3, 5, 10, 20]):
     for k in k_range:
         eval_metrics["ndcg@" + str(k)] = mean_ndcg(Y_true, Y_pred, k=k)
 
-    eval_metrics["accuracy"] = accuracy(Y_true, Y_pred)
-
     return eval_metrics
 
 
-def doc2vec_eval(datapath, vec_size=20, alpha=0.025, file_to_write=None):
+def doc2vec_eval(datapath, vec_size=20, alpha=0.025):
     """Trains the doc2vec model on training data of WikiQA and then
     evaluates on test data
 
@@ -178,16 +175,13 @@ def doc2vec_eval(datapath, vec_size=20, alpha=0.025, file_to_write=None):
 
     alpha : float
         The initial learning rate.
-
-    file_to_write : string
-        where the results of the evaluation should be stored
-        ignore if it is None
     """
     # load training data
     wikiqa_train = WikiQAExtractor(os.path.join(datapath, "WikiQA-train.tsv"))
     train_data = wikiqa_train.get_preprocessed_corpus()
     lls = LabeledLineSentence(train_data)
 
+    logger.info("Building and training doc2vec model")
     model = Doc2Vec(size=vec_size,
                     alpha=alpha,
                     min_alpha=0.025,
@@ -197,6 +191,7 @@ def doc2vec_eval(datapath, vec_size=20, alpha=0.025, file_to_write=None):
     model.train(lls,
                 total_examples=model.corpus_count,
                 epochs=model.iter)
+    logger.info("Building and training of doc2vec done")
 
     # load test data
     wikiqa_test = WikiQAExtractor(os.path.join(datapath, "WikiQA-test.tsv"))
@@ -217,17 +212,17 @@ def doc2vec_eval(datapath, vec_size=20, alpha=0.025, file_to_write=None):
     Y_true = np.array(Y_true)
 
     results = get_metric_results(Y_true, Y_pred)
+    results["method"] = "d2v"
     print("Results of evaluating on doc2vec:")
     print(results)
 
-    if file_to_write is not None:
-        write_results_to_file(results, file_to_write)
+    results_list.append(results)
 
     # TODO might have to tune with validation data
     # TODO get params from user?
 
 
-def word2vec_eval(datapath, word_embedding_path, file_to_write=None):
+def word2vec_eval(datapath, word_embedding_path):
     """Averages words in a query to represent the sentence/doc
     If the word is out of vocabulary, we ignore it
 
@@ -238,10 +233,6 @@ def word2vec_eval(datapath, word_embedding_path, file_to_write=None):
 
     word_embedding_path : string
         path to the the .txt which has the Glove word embeddings
-
-    file_to_write : string
-        where the results of the evaluation should be stored
-        ignore if it is None
     """
     # load test data
     # Note: here we are not using train data to keep results consistent with
@@ -249,6 +240,7 @@ def word2vec_eval(datapath, word_embedding_path, file_to_write=None):
     wikiqa_test = WikiQAExtractor(os.path.join(datapath, "WikiQA-test.tsv"))
     test_doc_data = wikiqa_test.get_data()
 
+    logger.info("Starting building word-vec dict")
     # dict to store word-index pairs
     w2v = {}
     with open(word_embedding_path) as f:
@@ -256,6 +248,7 @@ def word2vec_eval(datapath, word_embedding_path, file_to_write=None):
             string_array = np.array(line.split()[1:])
             string_array = [float(i) for i in string_array]
             w2v[line.split()[0]] = string_array
+    logger.info("Word-vec dict build complete")
 
     def sent2vec(w2v, sentence):
         """Function to convert a sentence into an averaged vector
@@ -280,12 +273,12 @@ def word2vec_eval(datapath, word_embedding_path, file_to_write=None):
     Y_true = np.array(Y_true)
 
     results = get_metric_results(Y_true, Y_pred)
+    results["method"] = "w2v"
 
     print("Results of evaluating on word2vec:")
     print(results)
 
-    if file_to_write is not None:
-        write_results_to_file(results, file_to_write)
+    results_list.append(results)
     # TODO we can do an evaluation on the whole dataset since this is unsupervised
     # currently WikiQAExtractor cannot support multiple files. Add it.
     # call it get_unsupervised_data or something
@@ -297,17 +290,13 @@ def word2vec_eval(datapath, word_embedding_path, file_to_write=None):
     # KeyedVectors
 
 
-def mz_eval(mz_output_file, file_to_write=None):
+def mz_eval(mz_output_file):
     """Evaluates the metrics on a TREC format file output by MatchZoo
 
     parameters:
     ==========
     mz_output_file : string
         path to MatchZoo output TREC format file
-
-    file_to_write : string
-        where the results of the evaluation should be stored
-        ignore if it is None
     """
 
     with open(mz_output_file) as f:
@@ -329,12 +318,37 @@ def mz_eval(mz_output_file, file_to_write=None):
         Y_true.append(this_y_true)
 
     results = get_metric_results(Y_true, Y_pred)
+    results["method"] = "MZ"  # TODO add a way to specify the function
 
     print("Results of evaluating on mz_eval:")
     print(results)
 
-    if file_to_write is not None:
-        write_results_to_file(results, file_to_write)
+    results_list.append(results)
+
+
+def write_results_to_file(results_list, file_to_write, k_range=[1, 3, 5, 10, 20]):
+    """Writes the evaluated metrics in the given
+    """
+    k_range = ["ndcg@" + str(k) for k in k_range]
+    metric_order = ["map"] + k_range
+
+    header = "Method, "
+    for metric in metric_order:
+        header += metric + ", "
+    header += "\n"
+
+    to_write = ""
+    for result in results_list:
+        to_write += result["method"] + ", "
+        for metric in metric_order:
+            to_write += str(result[metric]) + ", "
+        to_write += "\n"
+
+    with open(file_to_write + ".csv", "w") as f:
+        f.write(header)
+        f.write(to_write)
+
+    print("Results saved in %s" % file_to_write + ".csv")
 
 
 if __name__ == '__main__':
@@ -359,14 +373,21 @@ if __name__ == '__main__':
     parser.add_argument('--mz_output_file',
                         help='path to the prediction output file made by mz')
 
+    parser.add_argument('--result_save_path',
+                        default=None,
+                        help='path to save the results to as a csv')
+
     args = parser.parse_args()
     if args.model == 'doc2vec':
         doc2vec_eval(args.datapath)
     elif args.model == 'word2vec':
         word2vec_eval(args.datapath, args.word_embedding_path)
     elif args.model == 'mz':
-        mz_eval(args.mz_output_file)
-    else:
+        mz_eval(args.mz_output_file, file_to_write=args.result_save_path)
+    elif args.model == 'all':
         doc2vec_eval(args.datapath)
         word2vec_eval(args.datapath, args.word_embedding_path)
         mz_eval(args.mz_output_file)
+
+    if args.result_save_path is not None:
+        write_results_to_file(results_list, args.result_save_path)
