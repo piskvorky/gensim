a9cf90e6dd03bac7954484dce8db2114b2d3c48e
shiki <shiki.he@hotmail.com>
2018-08-09 00:18:03 -0400

    fixed docstrings to numpy-style

--- gensim/summarization/bm25.py
+++ gensim/summarization/bm25.py
@@ -155,7 +155,24 @@ class BM25(object):
 
 
 def _get_scores(bm25, document, average_idf):
-    """helper function for retrieving bm25 scores in parallel"""
+    """Helper function for retrieving bm25 scores of given `document` in parallel
+    in relation to every item in corpus.
+    
+    Parameters
+    ----------
+    bm25 : BM25 object
+        BM25 object fitted on the corpus where documents are retrieved.
+    document : list of str
+        Document to be scored.
+    average_idf : float
+        Average idf in corpus.
+
+    Returns
+    -------
+    list of float
+        BM25 scores.
+    
+    """
     scores = []
     for index in xrange(bm25.corpus_size):
         score = bm25.get_score(document, index, average_idf)
@@ -164,7 +181,22 @@ def _get_scores(bm25, document, average_idf):
 
 
 def _effective_n_jobs(n_jobs):
-    """Determine the number of jobs which are going to run in parallel"""
+    """Determines the number of jobs can run in parallel.
+
+    Just like in sklearn, passing n_jobs=-1 means using all available
+    CPU cores.
+    
+    Parameters
+    ----------
+    n_jobs : int
+        Number of workers requested by caller.
+
+    Returns
+    -------
+    int
+        number of effective jobs
+    
+    """
     if n_jobs == 0:
         raise ValueError('n_jobs == 0 in Parallel has no meaning')
     elif n_jobs is None:
