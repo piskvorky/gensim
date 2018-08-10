3a8a915accac91a9506df4d9ad7615d181c444fe
persiyanov <persiyanov@yandex-team.ru>
2018-08-10 16:03:04 +0300

    add MULTISTREAM_VERSION global constant

--- gensim/models/word2vec.py
+++ gensim/models/word2vec.py
@@ -334,8 +334,12 @@ except ImportError:
 
 try:
     from gensim.models.word2vec_multistream import train_epoch_sg, train_epoch_cbow
+
+    MULTISTREAM_VERSION = 1
 except ImportError:
     # multistream word2vec is not supported
+    MULTISTREAM_VERSION = -1
+
     def train_epoch_sg(model, corpus_file, offset, _cython_vocab, _cur_epoch, _expected_examples, _expected_words,
                        _work, _neu1, compute_loss):
         raise RuntimeError("Training with corpus_file argument is not supported")
