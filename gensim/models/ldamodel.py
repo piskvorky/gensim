2b20f67914c471aa5eeefe5117950a387758a70a
Laubeee <silvanlaube@hotmail.com>
2018-08-13 16:10:53 +0200

    correct assymetric -> asymmetric

--- gensim/models/ldamodel.py
+++ gensim/models/ldamodel.py
@@ -530,8 +530,8 @@ class LdaModel(interfaces.TransformationABC, basemodel.BaseTopicModel):
             If `name` == 'alpha', then the prior can be:
 
                 * an 1D array of length equal to the number of expected topics,
-                * 'asymmetric': Uses a fixed normalized assymetric prior of `1.0 / topicno`.
-                * 'auto': Learns an assymetric prior from the corpus.
+                * 'asymmetric': Uses a fixed normalized asymmetric prior of `1.0 / topicno`.
+                * 'auto': Learns an asymmetric prior from the corpus.
         name : {'alpha', 'eta'}
             Whether the `prior` is parameterized by the alpha vector (1 parameter per topic)
             or by the eta (1 parameter per unique term in the vocabulary).
