234180a1342b16fd66e313705e23f427a0d0c9dc
Denis Kataev <denis.a.kataev+git@gmail.com>
2018-09-20 17:46:05 +0500

    Indentation is not a multiple of four

--- gensim/scripts/segment_wiki.py
+++ gensim/scripts/segment_wiki.py
@@ -28,15 +28,15 @@ How to use
     >>>
     >>> # iterate over the plain text data we just created
     >>> for line in smart_open('enwiki-latest.json.gz'):
-    >>>    # decode each JSON line into a Python dictionary object
-    >>>    article = json.loads(line)
+    >>>     # decode each JSON line into a Python dictionary object
+    >>>     article = json.loads(line)
     >>>
-    >>>    # each article has a "title", a mapping of interlinks and a list of "section_titles" and "section_texts".
-    >>>    print("Article title: %s" % article['title'])
-    >>>    print("Interlinks: %s" + article['interlinks'])
-    >>>    for section_title, section_text in zip(article['section_titles'], article['section_texts']):
-    >>>        print("Section title: %s" % section_title)
-    >>>        print("Section text: %s" % section_text)
+    >>>     # each article has a "title", a mapping of interlinks and a list of "section_titles" and "section_texts".
+    >>>     print("Article title: %s" % article['title'])
+    >>>     print("Interlinks: %s" + article['interlinks'])
+    >>>     for section_title, section_text in zip(article['section_titles'], article['section_texts']):
+    >>>         print("Section title: %s" % section_title)
+    >>>         print("Section text: %s" % section_text)
 
 
 Notes
