4a96eaba3b113b5d2ca93c1cd3b18124fbbf2592
jeni Shah <jenishah@users.noreply.github.com>
2018-10-03 15:40:09 +0530

    modify phrasegrams

--- gensim/models/phrases.py
+++ gensim/models/phrases.py
@@ -772,7 +772,7 @@ class Phraser(SentenceAnalyzer, PhrasesTransformation):
         for bigram, score in phrases_model.export_phrases(corpus, self.delimiter, as_tuples=True):
             if bigram in self.phrasegrams:
                 logger.info('Phraser repeat %s', bigram)
-            self.phrasegrams[bigram] = (phrases_model.vocab[self.delimiter.join(bigram)], score)
+            self.phrasegrams[bigram] = score
             count += 1
             if not count % 50000:
                 logger.info('Phraser added %i phrasegrams', count)
@@ -815,7 +815,7 @@ class Phraser(SentenceAnalyzer, PhrasesTransformation):
 
         """
         try:
-            return self.phrasegrams[tuple(components)][1]
+            return self.phrasegrams[tuple(components)]
         except KeyError:
             return -1
 
