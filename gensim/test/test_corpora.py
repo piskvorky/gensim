6eafa239183b6f484825781ffa3a610afe8f32de
aquatiko <rohit970819@gmail.com>
2018-10-27 16:35:20 +0530

    svmlightcorpus.py: Add sequence serialization of corpus
    Current version of serialization support only lists, but this
    adds support for any type of sequence.
    
    Closes: https://github.com/RaRe-Technologies/gensim/issues/2113

--- gensim/test/test_corpora.py
+++ gensim/test/test_corpora.py
@@ -23,7 +23,7 @@ from gensim.corpora import (bleicorpus, mmcorpus, lowcorpus, svmlightcorpus,
                             ucicorpus, malletcorpus, textcorpus, indexedcorpus, wikicorpus)
 from gensim.interfaces import TransformedCorpus
 from gensim.utils import to_unicode
-from gensim.test.utils import datapath, get_tmpfile
+from gensim.test.utils import datapath, get_tmpfile, common_corpus
 
 
 class DummyTransformer(object):
@@ -381,6 +381,17 @@ class TestSvmLightCorpus(CorpusTestCase):
     def setUp(self):
         self.corpus_class = svmlightcorpus.SvmLightCorpus
         self.file_extension = '.svmlight'
+
+    def test_serialization(self):
+        path = get_tmpfile("svml.corpus")
+        labels = [1] * len(common_corpus)
+        second_corpus = [(0, 1.0), (3, 1.0), (4, 1.0), (5, 1.0), (6, 1.0), (7, 1.0)]
+        self.corpus_class.serialize(path, common_corpus, labels=labels)
+        serialized_corpus = self.corpus_class(path)
+        self.assertEqual(serialized_corpus[1], second_corpus)
+        self.corpus_class.serialize(path, common_corpus, labels=np.array(labels))
+        serialized_corpus = self.corpus_class(path)
+        self.assertEqual(serialized_corpus[1], second_corpus)
 
 
 class TestBleiCorpus(CorpusTestCase):
