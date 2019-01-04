bf8355370bae8bbcf6ebed3b6ecd2e04e8e610b6
Ivan Menshikh <menshikh.iv@gmail.com>
2019-01-04 17:18:45 +0500

    try to use different path

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -17,7 +17,7 @@ import sys
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- General configuration -----------------------------------------------------
 
