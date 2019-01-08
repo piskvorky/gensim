4d58a31b869e4a34f0a89c291d683f42960192ba
Christos Karatsalos <ckarats@gmail.com>
2019-01-08 23:49:13 +0200

    Update keyedvectors.py

--- gensim/models/keyedvectors.py
+++ gensim/models/keyedvectors.py
@@ -1175,16 +1175,12 @@ class WordEmbeddingsKeyedVectors(BaseKeyedVectors):
 
     def evaluate_word_analogies(self, analogies, restrict_vocab=300000, case_insensitive=True, dummy4unknown=False, topk=1, method='3CosAdd'):
         """Compute performance of the model on an analogy test set.
-
         This is modern variant of :meth:`~gensim.models.keyedvectors.WordEmbeddingsKeyedVectors.accuracy`, see
         `discussion on GitHub #1935 <https://github.com/RaRe-Technologies/gensim/pull/1935>`_.
-
         The accuracy is reported (printed to log and returned as a score) for each section separately,
         plus there's one aggregate summary at the end.
-
         This method corresponds to the `compute-accuracy` script of the original C word2vec.
         See also `Analogy (State of the art) <https://aclweb.org/aclwiki/Analogy_(State_of_the_art)>`_.
-
         Parameters
         ----------
         analogies : str
@@ -1202,14 +1198,14 @@ class WordEmbeddingsKeyedVectors(BaseKeyedVectors):
         dummy4unknown : bool, optional
             If True - produce zero accuracies for 4-tuples with out-of-vocabulary words.
             Otherwise, these tuples are skipped entirely and not used in the evaluation.
-
         Returns
         -------
         score : float
-            The overall evaluation score on the entire evaluation set		
-        (float, list of dict of (str, (str, str, str))
-            Overall evaluation score and full lists of correct and incorrect predictions divided by sections.
-
+            The overall evaluation score on the entire evaluation set
+        sections : list of dict of {str : str or list of tuple of (str, str, str, str)}
+            Results broken down by each section of the evaluation set. Each dict contains the name of the section
+            under the key 'section', and lists of correctly and incorrectly predicted 4-tuples of words under the
+            keys 'correct' and 'incorrect'.
         """
         ok_vocab = [(w, self.vocab[w]) for w in self.index2word[:restrict_vocab]]
         ok_vocab = {w.upper(): v for w, v in reversed(ok_vocab)} if case_insensitive else dict(ok_vocab)
