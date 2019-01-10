39e884481dd8e3abb667d1b4c999b1d82af4fc5b
Michael Penkov <misha.penkov@gmail.com>
2019-01-10 10:38:09 +0900

    review response: explain how expected values were generated

--- gensim/test/test_utils.py
+++ gensim/test/test_utils.py
@@ -263,8 +263,39 @@ class TestSaveAsLineSentence(unittest.TestCase):
             self.assertEqual(sentences, ref_sentences)
 
 
+def hash_main(alg):
+    """Generate hash values for test from standard input."""
+    import sys
+    import six
+
+    assert six.PY3, 'this only works under Py3'
+
+    hashmap = {
+        'py': gensim.models.utils_any2vec._ft_hash_py,
+        'py_broken': gensim.models.utils_any2vec._ft_hash_py_broken,
+        'cy': gensim.models.utils_any2vec._ft_hash_py,
+        'cy_broken': gensim.models.utils_any2vec._ft_hash_py_broken,
+    }
+    try:
+        fun = hashmap[alg]
+    except KeyError:
+        raise KeyError('invalid alg: %r expected one of %r' % (alg, sorted(hashmap)))
+
+    for line in sys.stdin:
+        for word in line.rstrip().split(' '):
+            print('u%r: %r,' % (word, fun(word)))
+
+
 class HashTest(unittest.TestCase):
     def setUp(self):
+        #
+        # I obtained these expected values using:
+        #
+        # $ echo word1 ... wordN | python -c 'from gensim.test.test_utils import hash_main;hash_main("alg")'  # noqa: E501
+        #
+        # where alg is one of py, py_broken, cy, cy_broken.
+
+        #
         self.expected = {
             u'команда': 1725507386,
             u'маленьких': 3011324125,
@@ -272,7 +303,14 @@ class HashTest(unittest.TestCase):
             u'возит': 4225261911,
             u'грузы': 1301826944,
             u'всех': 706328732,
-            u'быстрей': 1379730754
+            u'быстрей': 1379730754,
+            u'mysterious': 1903186891,
+            u'asteroid': 1988297200,
+            u'odyssey': 310195777,
+            u'introduction': 2848265721,
+            u'北海道': 4096045468,
+            u'札幌': 3909947444,
+            u'西区': 3653372632,
         }
         self.expected_broken = {
             u'команда': 962806708,
@@ -282,6 +320,13 @@ class HashTest(unittest.TestCase):
             u'грузы': 3674544745,
             u'всех': 3931012458,
             u'быстрей': 822471432,
+            u'mysterious': 1903186891,
+            u'asteroid': 1988297200,
+            u'odyssey': 310195777,
+            u'introduction': 2848265721,
+            u'北海道': 4017049120,
+            u'札幌': 1706980764,
+            u'西区': 1113327900,
         }
 
     def test_python(self):
