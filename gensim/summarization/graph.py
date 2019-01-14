008d5cb6b7223eec1a9557f4c93347668ddae621
horpto <__singleton__@hackerdom.ru>
2019-01-14 07:18:52 +0500

    refactor graph - remove unnecessary parts

--- gensim/summarization/graph.py
+++ gensim/summarization/graph.py
@@ -49,6 +49,11 @@ class IGraph(object):
     __metaclass__ = ABCMeta
 
     @abstractmethod
+    def __len__(self):
+        """Returns number of nodes in graph"""
+        pass
+
+    @abstractmethod
     def nodes(self):
         """Returns all nodes of graph.
 
@@ -107,7 +112,7 @@ class IGraph(object):
         pass
 
     @abstractmethod
-    def add_node(self, node, attrs=None):
+    def add_node(self, node):
         """Adds given node to the graph.
 
         Note
@@ -119,14 +124,12 @@ class IGraph(object):
         ----------
         node : hashable
             Given node
-        attrs : list, optional
-            Node attributes specified as (attribute, value)
 
         """
         pass
 
     @abstractmethod
-    def add_edge(self, edge, wt=1, label='', attrs=None):
+    def add_edge(self, edge, wt=1):
         """Adds an edge to the graph connecting two nodes. An edge, here,
         is a tuple of two nodes.
 
@@ -136,10 +139,6 @@ class IGraph(object):
             Given edge.
         wt : float, optional
             Weight of new edge.
-        label : str, optional
-            Edge label.
-        attrs : list, optional
-            Node attributes specified as (attribute, value)
 
         """
         pass
@@ -197,36 +196,18 @@ class Graph(IGraph):
 
     Attributes
     ----------
-    Graph.WEIGHT_ATTRIBUTE_NAME : str
-        Name of weight attribute in graph.
     Graph.DEFAULT_WEIGHT : float
         Weight set by default.
-    Graph.LABEL_ATTRIBUTE_NAME : str
-        Default name of attribute. Not used.
-    Graph.DEFAULT_LABEL : str
-        Label set by default. Not used.
 
     """
 
-    WEIGHT_ATTRIBUTE_NAME = "weight"
     DEFAULT_WEIGHT = 0
-
-    LABEL_ATTRIBUTE_NAME = "label"
-    DEFAULT_LABEL = ""
 
     def __init__(self):
         """Initializes object."""
-
-        # Metadata about edges
-        # Mapping: Edge -> Dict mapping, label-> str, wt->num
-        self.edge_properties = {}
-        # Key value pairs: (Edge -> Attributes)
-        self.edge_attr = {}
-
-        # Metadata about nodes
-        # Pairing: Node -> Attributes
-        self.node_attr = {}
-        # Pairing: Node -> Neighbors
+        # Pairing and metadata about edges
+        # Mapping: Node->
+        #    Dict mapping of Neighbor -> weight
         self.node_neighbors = {}
 
     def __len__(self):
@@ -248,7 +229,7 @@ class Graph(IGraph):
 
         """
         u, v = edge
-        return (u, v) in self.edge_properties and (v, u) in self.edge_properties
+        return v in self.node_neighbors[u] and u in self.node_neighbors[v]
 
     def edge_weight(self, edge):
         """Returns weight of given edge.
@@ -264,7 +245,8 @@ class Graph(IGraph):
             Edge weight.
 
         """
-        return self.get_edge_properties(edge).get(self.WEIGHT_ATTRIBUTE_NAME, self.DEFAULT_WEIGHT)
+        u, v = edge
+        return self.node_neighbors.get(u, {}).get(v, self.DEFAULT_WEIGHT)
 
     def neighbors(self, node):
         """Returns all nodes that are directly accessible from given node.
@@ -280,7 +262,7 @@ class Graph(IGraph):
             Nodes directly accessible from given `node`.
 
         """
-        return self.node_neighbors[node]
+        return list(self.node_neighbors[node])
 
     def has_node(self, node):
         """Returns whether the requested node exists.
@@ -298,7 +280,7 @@ class Graph(IGraph):
         """
         return node in self.node_neighbors
 
-    def add_edge(self, edge, wt=1, label='', attrs=None):
+    def add_edge(self, edge, wt=1):
         """Adds an edge to the graph connecting two nodes.
 
         Parameters
@@ -307,10 +289,6 @@ class Graph(IGraph):
             Given edge.
         wt : float, optional
             Weight of new edge.
-        label : str, optional
-            Edge label.
-        attrs : list, optional
-            Node attributes specified as (attribute, value).
 
         Raises
         ------
@@ -323,20 +301,15 @@ class Graph(IGraph):
             if self.has_edge(edge):
                 self.del_edge(edge)
             return
-        if attrs is None:
-            attrs = []
         u, v = edge
         if v not in self.node_neighbors[u] and u not in self.node_neighbors[v]:
-            self.node_neighbors[u].append(v)
+            self.node_neighbors[u][v] = wt
             if u != v:
-                self.node_neighbors[v].append(u)
-
-            self.add_edge_attributes((u, v), attrs)
-            self.set_edge_properties((u, v), label=label, weight=wt)
+                self.node_neighbors[v][u] = wt
         else:
             raise ValueError("Edge (%s, %s) already in graph" % (u, v))
 
-    def add_node(self, node, attrs=None):
+    def add_node(self, node):
         """Adds given node to the graph.
 
         Note
@@ -349,8 +322,6 @@ class Graph(IGraph):
         ----------
         node : hashable
             Given node.
-        attrs : list of (hashable, hashable), optional
-            Node attributes specified as (attribute, value)
 
         Raises
         ------
@@ -358,13 +329,10 @@ class Graph(IGraph):
             If `node` already exists in graph.
 
         """
-        if attrs is None:
-            attrs = []
-        if node not in self.node_neighbors:
-            self.node_neighbors[node] = []
-            self.node_attr[node] = attrs
-        else:
+        if node in self.node_neighbors:
             raise ValueError("Node %s already in graph" % node)
+
+        self.node_neighbors[node] = {}
 
     def nodes(self):
         """Returns all nodes of the graph.
@@ -386,18 +354,20 @@ class Graph(IGraph):
             Edges of graph.
 
         """
-        return [a for a in self.edge_properties]
+        return list(self.iter_edges())
 
     def iter_edges(self):
         """Returns iterator of all edges of the graph.
 
-        Returns
+        Yields
         -------
-        iterator of (hashable, hashable)
+        (hashable, hashable)
             Edges of graph.
 
         """
-        return iter(self.edge_properties)
+        for u in self.node_neighbors:
+            for v in self.node_neighbors[u]:
+                yield (u, v)
 
     def del_node(self, node):
         """Removes given node and its edges from the graph.
@@ -408,98 +378,10 @@ class Graph(IGraph):
             Given node.
 
         """
-        for each in list(self.neighbors(node)):
+        for each in self.neighbors(node):
             if each != node:
                 self.del_edge((each, node))
         del self.node_neighbors[node]
-        del self.node_attr[node]
-
-    def get_edge_properties(self, edge):
-        """Returns properties of given given edge. If edge doesn't exist
-        empty dictionary will be returned.
-
-        Parameters
-        ----------
-        edge : (hashable, hashable)
-            Given edge.
-
-        Returns
-        -------
-        dict
-            Properties of graph.
-
-        """
-        return self.edge_properties.get(edge, {})
-
-    def add_edge_attributes(self, edge, attrs):
-        """Adds attributes `attrs` to given edge, order of nodes in edge doesn't matter.
-
-        Parameters
-        ----------
-        edge : (hashable, hashable)
-            Given edge.
-        attrs : list
-            Provided attributes to add.
-
-        """
-        for attr in attrs:
-            self.add_edge_attribute(edge, attr)
-
-    def add_edge_attribute(self, edge, attr):
-        """Adds attribute `attr` to given edge, order of nodes in edge doesn't matter.
-
-        Parameters
-        ----------
-        edge : (hashable, hashable)
-            Given edge.
-
-        attr : object
-            Provided attribute to add.
-
-        """
-        self.edge_attr[edge] = self.edge_attributes(edge) + [attr]
-
-        if edge[0] != edge[1]:
-            self.edge_attr[(edge[1], edge[0])] = self.edge_attributes((edge[1], edge[0])) + [attr]
-
-    def edge_attributes(self, edge):
-        """Returns attributes of given edge.
-
-        Note
-        ----
-        In case of non existing edge returns empty list.
-
-        Parameters
-        ----------
-        edge : (hashable, hashable)
-            Given edge.
-
-        Returns
-        -------
-        list
-            Attributes of given edge.
-
-        """
-        try:
-            return self.edge_attr[edge]
-        except KeyError:
-            return []
-
-    def set_edge_properties(self, edge, **properties):
-        """Adds `properties` to given edge, order of nodes in edge doesn't matter.
-
-        Parameters
-        ----------
-        edge : (hashable, hashable)
-            Given edge.
-
-        properties : dict
-            Properties to add.
-
-        """
-        self.edge_properties.setdefault(edge, {}).update(properties)
-        if edge[0] != edge[1]:
-            self.edge_properties.setdefault((edge[1], edge[0]), {}).update(properties)
 
     def del_edge(self, edge):
         """Removes given edges from the graph.
@@ -511,26 +393,6 @@ class Graph(IGraph):
 
         """
         u, v = edge
-        self.node_neighbors[u].remove(v)
-        self.del_edge_labeling((u, v))
+        del self.node_neighbors[u][v]
         if u != v:
-            self.node_neighbors[v].remove(u)
-            self.del_edge_labeling((v, u))
-
-    def del_edge_labeling(self, edge):
-        """Removes attributes and properties of given edge.
-
-        Parameters
-        ----------
-        edge : (hashable, hashable)
-            Given edge.
-
-        """
-        keys = [edge, edge[::-1]]
-
-        for key in keys:
-            for mapping in [self.edge_properties, self.edge_attr]:
-                try:
-                    del mapping[key]
-                except KeyError:
-                    pass
+            del self.node_neighbors[v][u]
