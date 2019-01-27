36478227ec57700c1ef33cf743e8545adcb23437
Amr Keleg <amr_mohamed@live.com>
2019-01-27 20:10:43 +0200

    Rephrase the `poincare` documentation edit

--- gensim/models/poincare.py
+++ gensim/models/poincare.py
@@ -1383,7 +1383,9 @@ class PoincareRelations(object):
         ----------
         file_path : str
             Path to file containing a pair of nodes (a relation) per line, separated by `delimiter`.
-            To express u is v realtions, the lines should take the form `u` delimeter `v`.
+            Since the relations are asymmetric, the order of `u` and `v` nodes in each pair matters.
+            To express a "u is v" relation, the lines should take the form `u delimeter v`.
+            e.g: `kangaroo	mammal` is a tab-delimited line expressing a "`kangaroo is a mammal`" relation.
         encoding : str, optional
             Character encoding of the input file.
         delimiter : str, optional
