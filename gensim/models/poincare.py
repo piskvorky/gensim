b6d2653fb7363e643ff7d0486d3d6d4f46f75bbe
Amr Keleg <amr_mohamed@live.com>
2019-01-28 14:02:04 +0200

    Improve the example relation in `poincare` documentation

--- gensim/models/poincare.py
+++ gensim/models/poincare.py
@@ -1385,7 +1385,9 @@ class PoincareRelations(object):
             Path to file containing a pair of nodes (a relation) per line, separated by `delimiter`.
             Since the relations are asymmetric, the order of `u` and `v` nodes in each pair matters.
             To express a "u is v" relation, the lines should take the form `u delimeter v`.
-            e.g: https://github.com/RaRe-Technologies/gensim/blob/master/gensim/test/test_data/poincare_hypernyms.tsv
+            e.g: `kangaroo	mammal` is a tab-delimited line expressing a "`kangaroo is a mammal`" relation.
+            `gensim/test/test_data/poincare_hypernyms.tsv
+            <https://github.com/RaRe-Technologies/gensim/blob/master/gensim/test/test_data/poincare_hypernyms.tsv>`_.
         encoding : str, optional
             Character encoding of the input file.
         delimiter : str, optional
