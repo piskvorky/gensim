2ae2005d0aa4c58a7613ebbc645e32742f1675d9
horpto <__singleton__@hackerdom.ru>
2019-01-30 18:37:18 +0500

    fix issues of new version of flake8

--- gensim/similarities/__init__.py
+++ gensim/similarities/__init__.py
@@ -3,6 +3,14 @@ This package contains implementations of pairwise similarity queries.
 """
 
 # bring classes directly into package namespace, to save some typing
-from .docsim import Similarity, MatrixSimilarity, SparseMatrixSimilarity, SoftCosineSimilarity, WmdSimilarity  # noqa:F401
-from .termsim import TermSimilarityIndex, UniformTermSimilarityIndex, SparseTermSimilarityMatrix  # noqa:F401
+from .docsim import (
+    Similarity,
+    MatrixSimilarity,
+    SparseMatrixSimilarity,
+    SoftCosineSimilarity,
+    WmdSimilarity)  # noqa:F401
+from .termsim import (
+    TermSimilarityIndex,
+    UniformTermSimilarityIndex,
+    SparseTermSimilarityMatrix)  # noqa:F401
 from .levenshtein import LevenshteinSimilarityIndex  # noqa:F401
