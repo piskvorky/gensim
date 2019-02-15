b453709b93ba74ec252c889f72bcfc41aa6282ae
Michael Penkov <misha.penkov@gmail.com>
2019-02-15 10:05:41 +0900

    do proper tokenization in bug.py

--- bug.py
+++ bug.py
@@ -8,6 +8,6 @@ logging.basicConfig(level=logging.INFO)
 
 path = datapath('alldata-id-10.txt')
 with smart_open.smart_open(path, 'r', encoding='utf-8') as fin:
-    sentences = [l.rstrip() for l in fin]
+    sentences = [list(tokenize(l)) for l in fin]
 model = FastText(sentences=sentences, sg=1, hs=1, min_n=4, max_n=6)
 print(model)
