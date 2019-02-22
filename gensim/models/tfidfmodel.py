8d2bd9945be6dda28142658f28c4944ad9de74e5
Amr Keleg <amr_mohamed@live.com>
2019-02-22 22:25:14 +0200

    Fix the off-by-one bug in the TFIDF model.
    Fixes #2375.
    Use len to compute the number of features.
    Since the ids are zero-indexed, Using max causes an off-by-one bug.

--- gensim/models/tfidfmodel.py
+++ gensim/models/tfidfmodel.py
@@ -390,7 +390,7 @@ class TfidfModel(interfaces.TransformationABC):
         self.num_nnz = numnnz
         self.dfs = dfs
         # and finally compute the idf weights
-        n_features = max(dfs) if dfs else 0
+        n_features = len(dfs) if dfs else 0
         logger.info(
             "calculating IDF weights for %i documents and %i features (%i matrix non-zeros)",
             self.num_docs, n_features, self.num_nnz
