4d08a17c90dec4fe21331e4ab5c51d4d5f6f90b9
Vít Novotný <witiko@mail.muni.cz>
2019-03-18 02:39:15 +0100

    Use ndarray.size instead of len(ndarray) in matutils.unitvec

--- gensim/matutils.py
+++ gensim/matutils.py
@@ -733,7 +733,10 @@ def unitvec(vec, norm='l2', return_norm=False):
         if norm == 'l1':
             veclen = np.sum(np.abs(vec))
         if norm == 'l2':
-            veclen = blas_nrm2(vec) if len(vec) else 0.0
+            if vec.size == 0:
+                veclen = 0.0
+            else:
+                veclen = blas_nrm2(vec)
         if veclen > 0.0:
             if np.issubdtype(vec.dtype, np.integer):
                 vec = vec.astype(np.float)
