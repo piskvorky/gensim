a0b1c894c6dd4753a17b36d13c4e1b9733ef3334
Shiv Dhar <shivdhar@gmail.com>
2019-08-09 19:01:29 +0530

    Correct grammar in docs

--- gensim/scripts/glove2word2vec.py
+++ gensim/scripts/glove2word2vec.py
@@ -13,7 +13,7 @@ number of vectors and its dimension which is only difference regard to GloVe.
 Notes
 -----
 
-GloVe format (real example can be founded `on Stanford size <https://nlp.stanford.edu/projects/glove/>`_) ::
+GloVe format (real example can be found `on Stanford site <https://nlp.stanford.edu/projects/glove/>`_) ::
 
     word1 0.123 0.134 0.532 0.152
     word2 0.934 0.412 0.532 0.159
@@ -22,7 +22,7 @@ GloVe format (real example can be founded `on Stanford size <https://nlp.stanfor
     word9 0.334 0.241 0.324 0.188
 
 
-Word2Vec format (real example can be founded `on w2v old repository <https://code.google.com/archive/p/word2vec/>`_) ::
+Word2Vec format (real example can be found `on w2v old repository <https://code.google.com/archive/p/word2vec/>`_) ::
 
     9 4
     word1 0.123 0.134 0.532 0.152
