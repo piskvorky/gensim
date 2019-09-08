511eaa5360dedb7693237734d1a5f9ecc77bd30e
sezanzeb <proxima@hip70890b.de>
2019-09-08 19:32:48 +0200

    fixed tests locally

--- gensim/test/test_ensemblelda.py
+++ gensim/test/test_ensemblelda.py
@@ -14,7 +14,7 @@ import unittest
 import numpy as np
 from copy import deepcopy
 
-from gensim.models import EnsembleLda, LdaMulticore
+from gensim.models import EnsembleLda, LdaMulticore, LdaModel
 from gensim.test.utils import datapath, get_tmpfile, common_corpus, common_dictionary
 
 num_topics = 2
