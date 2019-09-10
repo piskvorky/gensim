c57150fb1bbfa37d6999429539fa47b312026d9d
Michael Penkov <m@penkov.dev>
2019-09-10 19:33:07 +0900

    more fixes to get docs building under tox

--- setup.py
+++ setup.py
@@ -388,7 +388,7 @@ setup(
         'distributed': distributed_env,
         'test-win': win_testenv,
         'test': linux_testenv,
-        'docs': linux_testenv + distributed_env + ['sphinx', 'sphinxcontrib-napoleon', 'plotly', 'pattern <= 2.6', 'sphinxcontrib.programoutput'],
+        'docs': linux_testenv + distributed_env + ['sphinx', 'sphinxcontrib-napoleon', 'plotly', 'sphinxcontrib.programoutput', 'annoy'],
     },
 
     include_package_data=True,
