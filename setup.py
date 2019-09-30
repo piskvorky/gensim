d5b82fb3f54e963c45143f0744ca439faa43ba56
SanyuktaSaha <36417473+SanyuktaSaha@users.noreply.github.com>
2019-10-01 01:33:09 +0530

    update setup.py
    removed additional commas and spaces

--- setup.py
+++ setup.py
@@ -88,17 +88,18 @@ def build_extension(self, ext):
     # the following is needed to be able to add numpy's include dirs... without
     # importing numpy directly in this script, before it's actually installed!
     # http://stackoverflow.com/questions/19919905/how-to-bootstrap-numpy-installation-in-setup-py
-    def finalize_options(self):
-        build_ext.finalize_options(self)
+def finalize_options(self):
+    
+    build_ext.finalize_options(self)
         # Prevent numpy from thinking it is still in its setup process:
         # https://docs.python.org/2/library/__builtin__.html#module-__builtin__
-        if isinstance(__builtins__, dict):
-            __builtins__["__NUMPY_SETUP__"] = False
-        else:
-            __builtins__.__NUMPY_SETUP__ = False
+    if isinstance(__builtins__, dict):
+        __builtins__["__NUMPY_SETUP__"] = False
+    else:
+         __builtins__.__NUMPY_SETUP__ = False
 
-        import numpy
-        self.include_dirs.append(numpy.get_include())
+    import numpy
+    self.include_dirs.append(numpy.get_include())
 
 model_dir = os.path.join(os.path.dirname(__file__), 'gensim', 'models')
 gensim_dir = os.path.join(os.path.dirname(__file__), 'gensim')
