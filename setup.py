a7713aa398c755aaf6c13c3094743913cc84e59d
Michael Penkov <m@penkov.dev>
2019-10-10 15:47:17 +0900

    Disable Py2.7 builds under Travis, CircleCI and AppVeyor (#2601)
    * Disable Py2.7 builds under Travis and AppVeyor
    
    * use Py3.7.4 image under CircleCI
    
    * tweak circleci config.yml
    
    * patch tox.ini
    
    * more fixes to get docs building under tox
    
    * s/python3.7/python3/
    
    * delay annoy ImportError until actual use
    
    * bring back Pattern
    
    * simplify invokation of pip command
    
    * add install_numpy_scipy.py
    
    * fixup
    
    * use sys.executable
    
    * adjust version in install_wheels.py
    
    * adjust travis.yml
    
    * adjust version in install_wheels.py back
    
    * add logging statements
    
    * use version_info instead of sys.version
    
    * fixup

--- setup.py
+++ setup.py
@@ -272,7 +272,6 @@ if sys.version_info < (3, 7):
     linux_testenv.extend([
         'tensorflow <= 1.3.0',
         'keras >= 2.0.4, <= 2.1.4',
-        'annoy',
     ])
 
 if (3, 0) < sys.version_info < (3, 7):
@@ -282,8 +281,7 @@ docs_testenv = linux_testenv + distributed_env + [
     'sphinx',
     'sphinxcontrib-napoleon',
     'plotly',
-# Pattern's version is specified to install Pattern 3.6, which adds python3 support
-    'Pattern >= 3.6',
+    'Pattern >= 3.6',  # Need 3.6 or later for Py3 support
     'sphinxcontrib.programoutput',
 ]
 #
