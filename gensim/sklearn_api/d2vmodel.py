92bea7bc264dda7a14853ba96eccaf573a52cbb4
Hiyorimi <playittodeath@gmail.com>
2019-10-14 21:08:26 +0300

    Added _get_first method to get rid of list copy

--- gensim/sklearn_api/d2vmodel.py
+++ gensim/sklearn_api/d2vmodel.py
@@ -145,6 +145,11 @@ class D2VTransformer(TransformerMixin, BaseEstimator):
         self.sorted_vocab = sorted_vocab
         self.batch_words = batch_words
 
+    def _get_first(some_iterable):
+        """Returns first element of some iterable."""
+        for elem in some_iterable:
+            return elem
+
     def fit(self, X, y=None):
         """Fit the model according to the given training data.
 
@@ -159,7 +164,7 @@ class D2VTransformer(TransformerMixin, BaseEstimator):
             The trained model.
 
         """
-        if isinstance([i for i in X[:1]][0], doc2vec.TaggedDocument):
+        if isinstance(self._get_first(X), doc2vec.TaggedDocument):
             d2v_sentences = X
         else:
             d2v_sentences = [doc2vec.TaggedDocument(words, [i]) for i, words in enumerate(X)]
