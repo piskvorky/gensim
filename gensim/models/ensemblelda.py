ad97ead41fd999b280ea92ae008f398050d3a279
Michael Penkov <m@penkov.dev>
2019-11-09 21:58:42 +0900

    fix trailing whitespace

--- gensim/models/ensemblelda.py
+++ gensim/models/ensemblelda.py
@@ -1224,9 +1224,9 @@ class CBDBSCAN():
             """
             neighbors_sorted = sorted(
                 [
-                    (distance, index) 
+                    (distance, index)
                     for index, distance in enumerate(amatrix_copy[topic_index])
-                ], 
+                ],
                 key=lambda x: x[0],
             )
             neighboring_topic_indices = [index for distance, index in neighbors_sorted if distance < self.eps]
