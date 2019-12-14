46b7cf6d4f2f098da4e16d86498942fe0df5d401
sezanzeb <to.213692@protonmail.ch>
2019-12-14 11:51:13 +0100

    added spec for inference

--- gensim/test/test_ensemblelda.py
+++ gensim/test/test_ensemblelda.py
@@ -7,6 +7,9 @@
 Automated tests for checking the EnsembleLda Class
 """
 
+import sys
+sys.path = ['/mnt/data/Code/ensemble_lda_repo/gensim'] + sys.path
+
 import logging
 import unittest
 
@@ -340,6 +343,16 @@ class TestModel(unittest.TestCase):
 
         # same random state, hence topics should be still similar
         np.testing.assert_allclose(new_eLDA.get_topics(), new_eLDA_mu.get_topics(), rtol=1e-05)
+
+    def test_inference(self):
+        import numpy as np
+        # get the most likely token id from topic 0
+        max_id = np.argmax(self.eLDA.get_topics()[0,:])
+        self.assertGreater(self.eLDA.classic_model_representation.iterations, 0)
+        # topic 0 should be dominant in the inference.
+        # the difference between the probabilities should be significant and larger than 0.3
+        infered = self.eLDA[[(max_id, 1)]]
+        self.assertGreater(infered[0][1] - 0.3, infered[1][1])
 
 
 if __name__ == '__main__':
