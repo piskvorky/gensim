12897cb9d2ef067b312aab58b517d8f91544e861
Michael Penkov <m@penkov.dev>
2020-01-01 19:10:37 +0900

    Fix appveyor builds (#2706)
    * move install_wheels script
    
    * git add continuous_integration/check_wheels.py
    
    * bump versions for numpy and scipy
    
    * update old requirements.txt
    
    * add file header
    
    * get rid of install_wheels.py hack
    
    * fixup: update travis.yml
    
    * Update continuous_integration/check_wheels.py
    
    Co-Authored-By: Radim Řehůřek <me@radimrehurek.com>
    
    * Update continuous_integration/check_wheels.py
    
    Co-Authored-By: Radim Řehůřek <me@radimrehurek.com>
    
    Co-authored-by: Radim Řehůřek <me@radimrehurek.com>

---
+++ continuous_integration/check_wheels.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 RaRe Technologies s.r.o.
+# Licensed under the GNU LGPL v2.1 - http://www.gnu.org/licenses/lgpl.html
+"""Print available wheels for a particular Python package."""
+import re
+import sys
+
+import requests
+
+def to_int(value):
+    value = ''.join((x for x in value if x.isdigit()))
+    try:
+        return int(value)
+    except Exception:
+        return 0
+
+
+def to_tuple(version):
+    return tuple(to_int(x) for x in version.split('.'))
+
+
+def main():
+    project = sys.argv[1]
+    json = requests.get('https://pypi.org/pypi/%s/json' % project).json()
+    for version in sorted(json['releases'], key=to_tuple):
+        print(version)
+        wheel_packages = [
+            p for p in json['releases'][version]
+            if p['packagetype'] == 'bdist_wheel'
+        ]
+        for p in wheel_packages:
+            print('    %(python_version)s %(filename)s' % p)
+
+
+if __name__ == '__main__':
+    main()
