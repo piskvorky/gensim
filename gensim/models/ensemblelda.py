591cf77bffddf35ad1badae408b4ac007a8b042b
sezanzeb <to.213692@protonmail.ch>
2020-02-06 21:37:50 +0100

    a bunch of reviews

--- gensim/models/ensemblelda.py
+++ gensim/models/ensemblelda.py
@@ -113,6 +113,8 @@ from gensim.utils import SaveLoad
 
 logger = logging.getLogger(__name__)
 
+print('####')
+
 
 class EnsembleLda(SaveLoad):
     """Ensemble Latent Dirichlet Allocation (eLDA), a method of training a topic model ensemble.
@@ -274,6 +276,8 @@ class EnsembleLda(SaveLoad):
     def get_topic_model_class(self):
         """Get the class that is used for :meth:`gensim.models.EnsembleLda.generate_gensim_representation`."""
         if self.topic_model_class is None:
+            instruction = 'Try setting topic_model_class manually to what the individual models were based on, ' \
+                'e.g. LdaMulticore.'
             try:
                 module = importlib.import_module(self.topic_model_module_string)
                 self.topic_model_class = getattr(module, self.topic_model_class_string)
@@ -281,12 +285,12 @@ class EnsembleLda(SaveLoad):
                 del self.topic_model_class_string
             except ModuleNotFoundError:
                 logger.error('Could not import the "{}" module in order to provide the "{}" class as '
-                    '"topic_model_class" attribute. Try setting this manually instead after loading.'
-                    .format(self.topic_model_class_string, self.topic_model_class_string))
+                    '"topic_model_class" attribute. {}'
+                    .format(self.topic_model_class_string, self.topic_model_class_string, instruction))
             except AttributeError:
                 logger.error('Could not import the "{}" class from the "{}" module in order to set the '
-                    '"topic_model_class" attribute. Try setting this manually instead after loading.'
-                    .format(self.topic_model_class_string, self.topic_model_module_string))
+                    '"topic_model_class" attribute. {}'
+                    .format(self.topic_model_class_string, self.topic_model_module_string, instruction))
         return self.topic_model_class
 
     def save(self, *args, **kwargs):
@@ -361,7 +365,7 @@ class EnsembleLda(SaveLoad):
         # to generate the proper sstats for the new gensim model:
         # transform to dimensionality of stable_topics. axis=1 is summed
         eta_sum = 0
-        if int == type(eta) or float == type(eta):
+        if isinstance(eta, (int, float)):
             eta_sum = [eta * len(stable_topics[0])] * num_stable_topics
         else:
             if len(eta.shape) == 1:  # [e1, e2, e3]
