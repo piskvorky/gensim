eefe9ab87ae1b0206f0c7ff7adc296961dada6bc
Radim Řehůřek <radimrehurek@seznam.cz>
2020-09-13 23:36:05 +0200

    fix docs

--- gensim/models/word2vec.py
+++ gensim/models/word2vec.py
@@ -44,7 +44,7 @@ Initialize a model with e.g.:
     >>>
     >>> path = get_tmpfile("word2vec.model")
     >>>
-    >>> model = Word2Vec(common_texts, size=100, window=5, min_count=1, workers=4)
+    >>> model = Word2Vec(common_texts, vector_size=100, window=5, min_count=1, workers=4)
     >>> model.save("word2vec.model")
 
 The training is streamed, meaning `sentences` can be a generator, reading input data
