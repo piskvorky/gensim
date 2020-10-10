b2a5a0dbe9bca24244ec70afd5977c6c0fbd9948
Gordon Mohr <gojogit@gmail.com>
2020-10-09 18:06:44 -0700

    double-backticks
    Co-authored-by: Michael Penkov <m@penkov.dev>

--- gensim/models/word2vec.py
+++ gensim/models/word2vec.py
@@ -953,7 +953,7 @@ class Word2Vec(utils.SaveLoad):
         Parameters
         ----------
         corpus_iterable : iterable of list of str
-            The `corpus_iterable` can be simply a list of lists of tokens, but for larger corpora,
+            The ``corpus_iterable`` can be simply a list of lists of tokens, but for larger corpora,
             consider an iterable that streams the sentences directly from disk/network, to limit RAM usage.
             See :class:`~gensim.models.word2vec.BrownCorpus`, :class:`~gensim.models.word2vec.Text8Corpus`
             or :class:`~gensim.models.word2vec.LineSentence` in :mod:`~gensim.models.word2vec` module for such examples.
