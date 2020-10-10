cd9df302047520942791e3db3bebeae06f993428
horpto <__Singleton__@hackerdom.ru>
2020-10-10 20:40:36 +0300

    take out common part of expression in ATModel

--- gensim/models/atmodel.py
+++ gensim/models/atmodel.py
@@ -486,9 +486,10 @@ class AuthorTopicModel(LdaModel):
 
                 # Update gamma.
                 # phi is computed implicitly below,
+                dot = np.dot(cts / phinorm, expElogbetad.T)
                 for ai, a in enumerate(authors_d):
                     tilde_gamma[ai, :] = self.alpha + len(self.author2doc[self.id2author[a]])\
-                        * expElogthetad[ai, :] * np.dot(cts / phinorm, expElogbetad.T)
+                        * expElogthetad[ai, :] * dot
 
                 # Update gamma.
                 # Interpolation between document d's "local" gamma (tilde_gamma),
