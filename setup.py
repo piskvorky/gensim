aa8849dae46880af52601622720066015a020f6f
Michael Penkov <misha.penkov@gmail.com>
2021-01-11 21:53:20 +0900

    bump Cython version to 0.29.21

--- setup.py
+++ setup.py
@@ -325,7 +325,7 @@ NUMPY_STR = 'numpy >= 1.11.3'
 # to build with any sane version of Cython, so we should update this pin
 # periodically.
 #
-CYTHON_STR = 'Cython==0.29.14'
+CYTHON_STR = 'Cython==0.29.21'
 
 install_requires = [
     NUMPY_STR,
