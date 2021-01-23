43e94d6bd50a5e85a47aedfd4320775bd77393b3
sezanzeb <to.213692@protonmail.ch>
2021-01-23 09:45:26 +0100

    Clarify random state in ldamulticore.py (#2953)
    * updated docstring
    
    * updated docstring 2
    
    * updated docstring 3
    
    * rewrite random_state comment
    
    Co-authored-by: Radim Řehůřek <me@radimrehurek.com>
    
    * updated docstring 5
    
    Co-authored-by: Radim Řehůřek <me@radimrehurek.com>

--- gensim/models/ldamulticore.py
+++ gensim/models/ldamulticore.py
@@ -160,6 +160,7 @@ class LdaMulticore(LdaModel):
             Topics with a probability lower than this threshold will be filtered out.
         random_state : {np.random.RandomState, int}, optional
             Either a randomState object or a seed to generate one. Useful for reproducibility.
+            Note that results can still vary due to non-determinism in OS scheduling of the worker processes.
         minimum_phi_value : float, optional
             if `per_word_topics` is True, this represents a lower bound on the term probabilities.
         per_word_topics : bool
