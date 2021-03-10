ccc35ba618929f1ea991cff84129bb2a54de66b1
Kieran W <41634689+FredHappyface@users.noreply.github.com>
2021-03-10 13:06:19 +0000

    Update gensim/similarities/__init__.py
    Co-authored-by: Michael Penkov <m@penkov.dev>

--- gensim/similarities/__init__.py
+++ gensim/similarities/__init__.py
@@ -10,7 +10,7 @@ except ImportError:
     warnings.warn(
         "The gensim.similarities.levenshtein submodule is disabled, because the optional "
         "Levenshtein package <https://pypi.org/project/python-Levenshtein/> is unavailable. "
-        "Install Levenhstein (e.g. `pip install Levenshtein`) to suppress this warning."
+        "Install Levenhstein (e.g. `pip install python-Levenshtein`) to suppress this warning."
     )
     LevenshteinSimilarityIndex = None
 else:
