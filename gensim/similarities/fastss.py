b6755077bb987a4e24a0dab7eb62d8e487446532
Radim Řehůřek <radimrehurek@seznam.cz>
2021-05-16 23:05:26 +0200

    clean up FastSS

--- gensim/similarities/fastss.py
+++ gensim/similarities/fastss.py
@@ -1,82 +1,58 @@
-"""Create and query FastSS index for string simirality search.
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 Radim Rehurek <radimrehurek@seznam.cz>
+# Licensed under the GNU LGPL v2.1 - http://www.gnu.org/licenses/lgpl.html
+# Code adapted from TinyFastSS (public domain), https://github.com/fujimotos/TinyFastSS
 
-Command-line usage:
-
-  Create a new index from dictionary:
-
-    python -m fastss -c index.dat <file>
-
-  Query an index file:
-
-    python -m fastss -q index.dat <query>
-
-Create mode options:
-
-  --maxdist  <N> maximum edit distance for the index (default: 2)
-  --encoding <S> the encoding of the dictionary file.
-
-Note:
-
-  For creating an index, you need to pass a dictionary file which
-  contains a list of words in a one-word-per-line manner. If <file>
-  argument is omitted, it tries to read from stdin.
-"""
+"""Create and query FastSS index for fast approximate string similarity search."""
 
 import struct
 import itertools
-
-
-#
-# Constants
 
 ENCODING = 'utf-8'
 DELIMITER = b'\x00'
 
 
-#
-# Utils
-
 def editdist(s1, s2):
-    r"""Return the Levenshtein distance between two strings.
+    """Return the Levenshtein distance between two strings.
 
     >>> editdist('aiu', 'aie')
     1
     """
-
     matrix = {}
 
-    for i in range(len(s1) + 1):
+    for i in range(len(s1)+1):
         matrix[(i, 0)] = i
-    for j in range(len(s2) + 1):
+    for j in range(len(s2)+1):
         matrix[(0, j)] = j
 
-    for i in range(1, len(s1) + 1):
-        for j in range(1, len(s2) + 1):
-            if s1[i - 1] == s2[j - 1]:
-                matrix[(i, j)] = matrix[(i - 1, j - 1)]
+    for i in range(1, len(s1)+1):
+        for j in range(1, len(s2)+1):
+            if s1[i-1] == s2[j-1]:
+                matrix[(i, j)] = matrix[(i-1, j-1)]
             else:
                 matrix[(i, j)] = min(
-                    matrix[(i - 1, j)],
-                    matrix[(i, j - 1)],
-                    matrix[(i - 1, j - 1)]
+                    matrix[(i-1, j)],
+                    matrix[(i, j-1)],
+                    matrix[(i-1, j-1)]
                 ) + 1
 
     return matrix[(i, j)]
 
 
 def indexkeys(word, max_dist):
-    r"""Return the set of index keys ("variants") of a word.
+    """Return the set of index keys ("variants") of a word.
 
     >>> indexkeys('aiu', 1)
     {'aiu', 'iu', 'au', 'ai'}
     """
-
     res = set()
     wordlen = len(word)
     limit = min(max_dist, wordlen) + 1
 
     for dist in range(limit):
-        variants = itertools.combinations(word, wordlen - dist)
+        variants = itertools.combinations(word, wordlen-dist)
 
         for variant in variants:
             res.add(''.join(variant))
@@ -85,7 +61,7 @@ def indexkeys(word, max_dist):
 
 
 def int2byte(i):
-    r"""Encode a positive int (<= 256) into a 8-bit byte.
+    """Encode a positive int (<= 256) into a 8-bit byte.
 
     >>> int2byte(1)
     b'\x01'
@@ -94,7 +70,7 @@ def int2byte(i):
 
 
 def byte2int(b):
-    r"""Decode a 8-bit byte into an integer.
+    """Decode a 8-bit byte into an integer.
 
     >>> byte2int(b'\x01')
     1
@@ -103,9 +79,9 @@ def byte2int(b):
 
 
 def set2bytes(s):
-    r"""Serialize a set of unicode strings into bytes.
+    """Serialize a set of unicode strings into bytes.
 
-    >>> set2byte({u'a', u'b', u'c'})
+    >>> set2byte({u'a', u'b', u'c')
     b'a\x00b\x00c'
     """
     lis = []
@@ -116,7 +92,7 @@ def set2bytes(s):
 
 
 def bytes2set(b):
-    r"""Deserialize bytes into a set of unicode strings.
+    """Deserialize bytes into a set of unicode strings.
 
     >>> int2byte(b'a\x00b\x00c')
     {u'a', u'b', u'c'}
@@ -128,27 +104,16 @@ def bytes2set(b):
     return set(bword.decode(ENCODING) for bword in lis)
 
 
-#
-# FastSS class
-
 class FastSS:
-    def __init__(self, max_dist):
-        r"""Open an FastSS index.
+    """Open a FastSS index."""
 
-        max_dist: the uppser threshold of edit distance for the index. Only
-                  effective when creating a new index file.
-        """
+    def __init__(self, max_dist=2):
+        """max_dist: the upper threshold of edit distance of works from the index."""
         self.db = {}
         self.max_dist = max_dist
 
     def __str__(self):
-        return "%s<max_dist=%s, dictionary=%i" % (self.__class__.__name__, self.max_dist, len(self.db), )
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, type, value, traceback):
-        self.close()
+        return "%s<max_dist=%s, db_size=%i>" % (self.__class__.__name__, self.max_dist, len(self.db), )
 
     def __contains__(self, word):
         bkey = word.encode(ENCODING)
@@ -156,10 +121,8 @@ class FastSS:
             return word in bytes2set(self.db[bkey])
         return False
 
-    def close(self):
-        pass
-
     def add(self, word):
+        """Add a string to the index."""
         for key in indexkeys(word, self.max_dist):
             bkey = key.encode(ENCODING)
             wordset = {word}
@@ -169,11 +132,12 @@ class FastSS:
 
             self.db[bkey] = set2bytes(wordset)
 
-    def query(self, word, max_dist):
-        res = {d: [] for d in range(max_dist + 1)}
+    def query(self, word):
+        """Find all words from the index that are within max_dist of `word`."""
+        res = {d: [] for d in range(self.max_dist+1)}
         cands = set()
 
-        for key in indexkeys(word, max_dist):
+        for key in indexkeys(word, self.max_dist):
             bkey = key.encode(ENCODING)
 
             if bkey in self.db:
@@ -181,7 +145,7 @@ class FastSS:
 
         for cand in cands:
             dist = editdist(word, cand)
-            if dist <= max_dist:
+            if dist <= self.max_dist:
                 res[dist].append(cand)
 
         return res
