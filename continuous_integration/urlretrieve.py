b16fd0ca141d0f3f52296729a569fa5680a0dc61
Michael Penkov <m@penkov.dev>
2022-02-27 19:17:29 +0900

    argh, no f-strings

--- continuous_integration/urlretrieve.py
+++ continuous_integration/urlretrieve.py
@@ -2,7 +2,7 @@
 # It only achieves the desired effect under Py3.7 and above.
 import sys
 if sys.version_info[:2] < (3, 7):
-    print(f"this script is a no-op for python version {sys.version}")
+    print("this script is a no-op for %s" % sys.version)
     sys.exit(0)
 from urllib.request import urlretrieve
 urlretrieve(sys.argv[1], sys.argv[2])
