b64fdc0d4477d9965eb1fc517d4425e275ffbc2f
Michael Penkov <m@penkov.dev>
2022-03-31 12:00:48 -0700

    update hijack_pr.py

--- release/hijack_pr.py
+++ release/hijack_pr.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """Hijack a PR to add commits as a maintainer.
 
 This is a two-step process:
@@ -14,6 +15,17 @@ import sys
 
 import smart_open
 
+def check_output(command):
+    return subprocess.check_output(command).strip().decode('utf-8')
+
+
+if sys.argv[1] == "push":
+    command = "git rev-parse --abbrev-ref HEAD@{upstream}".split()
+    remote, remote_branch = check_output(command).split('/')
+    current_branch = check_output(['git', 'branch', '--show-current'])
+    check_output(['git', 'push', remote, f'{current_branch}:{remote_branch}'])
+    sys.exit(0)
+
 prid = int(sys.argv[1])
 url = f"https://api.github.com/repos/RaRe-Technologies/gensim/pulls/{prid}"
 with smart_open.open(url) as fin:
@@ -22,7 +34,7 @@ with smart_open.open(url) as fin:
 user = prinfo['head']['user']['login']
 ssh_url = prinfo['head']['repo']['ssh_url']
 
-remotes = subprocess.check_output(['git', 'remote']).strip().decode('utf-8').split('\n')
+remotes = check_output(['git', 'remote']).split('\n')
 if user not in remotes:
     subprocess.check_call(['git', 'remote', 'add', user, ssh_url])
 
@@ -30,4 +42,14 @@ subprocess.check_call(['git', 'fetch', user])
 
 ref = prinfo['head']['ref']
 subprocess.check_call(['git', 'checkout', f'{user}/{ref}'])
-subprocess.check_call(['git', 'switch', '-c', f'{ref}'])
+
+#
+# Prefix the local branch name with the user to avoid naming clashes with
+# existing branches, e.g. develop
+#
+subprocess.check_call(['git', 'switch', '-c', f'{user}_{ref}'])
+
+#
+# Set the upstream so we can push back to it more easily
+#
+subprocess.check_call(['git', 'branch', '--set-upstream-to', f'{user}/{ref}'])
