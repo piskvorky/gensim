5f1309e70816d7a09bbbccd88b29a9dd15d4f06d
samsul <samsul@bahasa.ai>
2022-11-08 22:27:38 +0700

    change to oldest version numpy and scipy that support 3.11

--- setup.py
+++ setup.py
@@ -318,7 +318,7 @@ docs_testenv = core_testenv + distributed_env + visdom_req + [
     'pandas',
 ]
 
-NUMPY_STR = 'numpy >= 1.23.4'
+NUMPY_STR = 'numpy >= 1.18.5'
 #
 # We pin the Cython version for reproducibility.  We expect our extensions
 # to build with any sane version of Cython, so we should update this pin
@@ -331,7 +331,7 @@ CYTHON_STR = os.environ.get('GENSIM_CYTHON_REQUIRES', CYTHON_STR)
 
 install_requires = [
     NUMPY_STR,
-    'scipy >= 1.9.3',
+    'scipy >= 1.9.0',
     'smart_open >= 1.8.1',
 ]
 
