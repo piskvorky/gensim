1a5ee4b403acf0d11d77ee7cad534e5f46f7445b
Michael Penkov <m@penkov.dev>
2022-12-18 00:02:10 +0900

    bumped version to 4.3.0

--- setup.py
+++ setup.py
@@ -352,7 +352,7 @@ if need_cython():
 
 setup(
     name='gensim',
-    version='4.2.1.dev0',
+    version='4.3.0',
     description='Python framework for fast Vector Space Modelling',
     long_description=LONG_DESCRIPTION,
 
