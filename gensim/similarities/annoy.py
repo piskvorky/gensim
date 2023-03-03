63fce182abc7d1e1e64fe7074dc0ed2c0eb90b3e
monash849 <62815453+monash849@users.noreply.github.com>
2023-03-03 14:02:43 +0200

    fix: conversion of cosine distance to cosine similarity is incorrect (#3441)
    Co-authored-by: Mona Shaheen <mona.shaheen@dynamicyield.com>

--- gensim/similarities/annoy.py
+++ gensim/similarities/annoy.py
@@ -185,4 +185,4 @@ class AnnoyIndexer():
         ids, distances = self.index.get_nns_by_vector(
             vector, num_neighbors, include_distances=True)
 
-        return [(self.labels[ids[i]], 1 - distances[i] / 2) for i in range(len(ids))]
+        return [(self.labels[ids[i]], 1 - distances[i] ** 2 / 2) for i in range(len(ids))]
