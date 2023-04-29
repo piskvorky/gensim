piskvorky/gensim

commit a21d9cc768598640f38e4bd03d368f8712a9aa77

Remove wrappers and other cruft (#2972)
* git rm docs/src/simserver.rst

* rm -r gensim/models/wrappers

* get rid of wrappers import

* scrub reference to wrappers from docstring

* git rm -r docs/src/models/wrappers

* git rm gensim/test/test_coherencemodel.py

This file relies on wrappers that aren't there anymore.

* rm -r gensim/models/wrappers

* rm wrapper and sklearn_api docs

* git rm gensim/test/test_sklearn_api.py

* bump Cython version to 0.29.21

* decrease tox verbosity

* get rid of more six remnants

* rm -rf gensim/sklearn_api

setup.py
@@ -275,7 +275,6 @@ core_testenv = [
     'testfixtures',
     'Morfessor==2.0.2a4',
     'python-Levenshtein >= 0.10.2',
-    'scikit-learn',
 ]
 
 # Add additional requirements for testing on Linux that are skipped on Windows.
@@ -307,7 +306,6 @@ docs_testenv = core_testenv + distributed_env + visdom_req + [
     'memory_profiler',
     'annoy',
     'Pyro4',
-    'scikit-learn',
     'nltk',
     'testfixtures',
     'statsmodels',

.travis.yml
@@ -44,7 +44,7 @@ before_script:
   - ulimit -c unlimited -S  # enable core dumps
 
 
-script: tox -vv
+script: tox
 
 
 after_failure:

azure-pipelines.yml
@@ -25,5 +25,5 @@ steps:
   displayName: 'Install tox'
 
 - script: |
-    tox -vv
+    tox
   displayName: 'Testing'

gensim/corpora/_mmreader.pyx
@@ -2,16 +2,12 @@
 # cython: embedsignature=True
 
 """Reader for corpus in the Matrix Market format."""
-
-from __future__ import with_statement
-
-from gensim import utils
-
 import logging
 
 cimport cython
 from libc.stdio cimport sscanf
 
+from gensim import utils
 
 logger = logging.getLogger(__name__)
 

gensim/models/__init__.py
@@ -22,8 +22,6 @@ from .ldaseqmodel import LdaSeqModel  # noqa:F401
 from .fasttext import FastText  # noqa:F401
 from .translation_matrix import TranslationMatrix, BackMappingTranslationMatrix  # noqa:F401
 
-from . import wrappers  # noqa:F401
-
 from gensim import interfaces, utils
 
 

gensim/models/tfidfmodel.py
@@ -69,11 +69,6 @@ def resolve_weights(smartirs):
     ValueError
         If `smartirs` is not a string of length 3 or one of the decomposed value
         doesn't fit the list of permissible values.
-
-    See Also
-    --------
-    ~gensim.sklearn_api.tfidf.TfIdfTransformer, TfidfModel : Classes that also use the SMART scheme.
-
     """
     if isinstance(smartirs, str) and re.match(r"...\....", smartirs):
         match = re.match(r"(?P<ddd>...)\.(?P<qqq>...)", smartirs)

gensim/models/word2vec.py
@@ -21,8 +21,7 @@ Other embeddings
 ================
 
 There are more ways to train word vectors in Gensim than just Word2Vec.
-See also :class:`~gensim.models.doc2vec.Doc2Vec`, :class:`~gensim.models.fasttext.FastText` and
-wrappers for :class:`~gensim.models.wrappers.varembed.VarEmbed` and :class:`~gensim.models.wrappers.wordrank.WordRank`.
+See also :class:`~gensim.models.doc2vec.Doc2Vec`, :class:`~gensim.models.fasttext.FastText`.
 
 The training algorithms were originally ported from the C package https://code.google.com/p/word2vec/
 and extended with additional functionality and

gensim/models/wrappers/__init__.py
@@ -1,9 +1,0 @@
-"""
-This package contains wrappers for other topic modeling programs.
-"""
-
-from .ldamallet import LdaMallet  # noqa:F401
-from .dtmmodel import DtmModel  # noqa:F401
-from .ldavowpalwabbit import LdaVowpalWabbit  # noqa:F401
-from .wordrank import Wordrank  # noqa:F401
-from .varembed import VarEmbed  # noqa:F401

commit 2249d56f7dc9c0d65f322528aca8312a097775f4

Add missing documentation build dependency scikit-learn
The run_compare_lda.py howto and run_word2vec.py tutorial import it.

It was removed from docs_testenv when the scikit-learn wrapper was removed.

Fixes: commit a21d9cc768598640f38e4bd03d368f8712a9aa77

setup.py
@@ -323,6 +323,7 @@ docs_testenv = core_testenv + distributed_env + visdom_req + [
     'memory_profiler',
     'annoy',
     'Pyro4',
+    'scikit-learn',
     'nltk',
     'testfixtures',
     'statsmodels',