d35eb9854aaedb0d341fdaa760f3becd5f299a3a
Michael Penkov <m@penkov.dev>
2023-05-10 21:36:16 +0900

    patch installwheel.py to take processor into account

--- .github/workflows/installwheel.py
+++ .github/workflows/installwheel.py
@@ -1,5 +1,6 @@
 """Install a wheel for the current platform."""
 import os
+import platform
 import subprocess
 import sys
 
@@ -7,10 +8,13 @@ import sys
 def main():
     subdir = sys.argv[1]
     vi = sys.version_info
+    processor = platform.processor()
+
     want = f'-cp{vi.major}{vi.minor}-'
+    suffix = f'_{processor}.whl'
 
     for f in os.listdir(subdir):
-        if want in f:
+        if want in f and f.endswith(suffix):
             command = [sys.executable, '-m', 'pip', 'install', os.path.join(subdir, f)]
             subprocess.check_call(command)
             return 0
