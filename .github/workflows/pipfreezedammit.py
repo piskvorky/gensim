a6cdaa0b109da1a62058ebad2e847a40665c9003
Michael Penkov <m@penkov.dev>
2023-05-15 15:20:17 +0900

    work around cibuildwheel not outputting pip-freeze

--- .github/workflows/pipfreezedammit.py
+++ .github/workflows/pipfreezedammit.py
@@ -2,6 +2,12 @@
 
 Implemented as a Python script because we need it to work on Windows, etc.
 """
+import os
 import subprocess
 import sys
-subprocess.call([sys.executable, '-m', 'pip', 'freeze'], stdout=sys.stderr)
+
+pythonversion = f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
+
+os.makedirs('pip-freeze', exist_ok=True)
+with open(f'pip-freeze/{pythonversion}.txt', 'wt') as fout:
+    subprocess.call([sys.executable, '-m', 'pip', 'freeze'], stdout=fout)
