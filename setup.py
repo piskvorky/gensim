da2f38861ff097d2f9054db273b880b0ec98acdd
Amirhosein Azhdarnezhad <amir.1381.azhdarnzhad@gmail.com>
2024-05-18 16:04:14 +0330

    Add support for python3.12 wheels (#3531)

--- setup.py
+++ setup.py
@@ -372,6 +372,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'Topic :: Scientific/Engineering :: Information Analysis',
