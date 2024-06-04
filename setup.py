712780d3eebf8230fad81b8236e1e7762fbbb896
Gordon Mohr <gojogit@gmail.com>
2024-06-04 11:58:42 -0700

    use legal version id: `4.4.0a1.dev0`

--- setup.py
+++ setup.py
@@ -337,7 +337,7 @@ install_requires = [
 
 setup(
     name='gensim',
-    version='4.3.2.np2test0',
+    version='4.4.0a0.dev0',
     description='Python framework for fast Vector Space Modelling',
     long_description=LONG_DESCRIPTION,
 
