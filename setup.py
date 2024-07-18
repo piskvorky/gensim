79f982b25be815310bc4410758ac6ee83a095e9e
Filip <39336739+filip-komarzyniec@users.noreply.github.com>
2024-07-18 14:03:07 +0200

    removed scipy from build-only dependencies (#3538)
    * removed scipy from build-only dependencies; updated README accordingly
    
    * fixed conditions regarding oldest numpy installation on arm64 Windows machines
    
    * README rephrased regarding NumPy installation
    
    * Revert "fixed conditions regarding oldest numpy installation on arm64 Windows machines"
    
    This reverts commit 68197fb9beb65cf3abdb03f71d7c7a4b8ae36473.
    
    * pin scipy, need old sparsetools for wheel testing
    
    * make scipy pin more strict
    
    * use macos-latest instead of deprecated macos-11
    
    * pin scipy in setup.py
    
    * remove redundant scipy pin from build-wheels.yml
    
    * get rid of remaining macos-11 traces in CI workflow
    
    * use macos-12 instead of macos-latest
    
    ---------
    
    Co-authored-by: Michael Penkov <m@penkov.dev>

--- setup.py
+++ setup.py
@@ -331,7 +331,10 @@ NUMPY_STR = 'numpy >= 1.18.5, < 2.0'
 
 install_requires = [
     NUMPY_STR,
-    'scipy >= 1.7.0',
+    #
+    # scipy 1.14.0 and onwards removes deprecated sparsetools submodule
+    #
+    'scipy >= 1.7.0, <1.14.0',
     'smart_open >= 1.8.1',
 ]
 
